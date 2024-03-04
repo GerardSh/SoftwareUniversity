cbe4efe735230b08ecd89d81c769fb3d44409804
Gerard <gerardshah@gmail.com>
2024-03-04 10:27:00 +0200

    test commit

--- 01 C#/02 Fundamentals/09 Regular Expressions/01 Lab/01 Reverse Strings/Program.cs
+++ 01 C#/02 Fundamentals/09 Regular Expressions/01 Lab/01 Reverse Strings/Program.cs
@@ -1,8 +1,1 @@
-﻿string input;
-
-while ((input = Console.ReadLine()) != "end")
-{
-    string reversedInput = string.Concat(input.Reverse());
-
-    Console.WriteLine(input + " = " + reversedInput);
-}
+﻿
