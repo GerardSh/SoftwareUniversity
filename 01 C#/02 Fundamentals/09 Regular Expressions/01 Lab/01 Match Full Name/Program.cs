45eb554f64b5cbdc546f82813a5807141f320341
Gerard <gerardshah@gmail.com>
2024-03-05 18:10:38 +0200

    Regular Expressions exercise added

---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/01 Lab/01 Match Full Name/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.RegularExpressions;
+string names = Console.ReadLine();
+
+var matches = Regex.Matches(names, @"\b[A-Z][a-z]+ [A-Z][a-z]+");
+
+Console.Write(string.Join(" ", matches));
