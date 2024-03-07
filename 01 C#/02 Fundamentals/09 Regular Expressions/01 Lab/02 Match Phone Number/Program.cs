4963b720fe751fd4f9af33860fc73bb160b00630
Gerard <gerardshah@gmail.com>
2024-03-07 21:36:09 +0200

    Lab exercises added

---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/01 Lab/02 Match Phone Number/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+string numbers = Console.ReadLine();
+
+MatchCollection matches = Regex.Matches(numbers, @"\+359( |-)2\1\d{3}\1\d{4}\b");
+
+Console.WriteLine(string.Join(", ", matches));
---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/01 Lab/03 Match Dates/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+MatchCollection dates = Regex.Matches(input, @"\b(?<day>\d{2})(\/|-|.)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+
+foreach (Match date in dates)
+{
+    Console.WriteLine($"Day: {date.Groups["day"]}, Month: {date.Groups["month"]}, Year: {date.Groups["year"]}");
+}
