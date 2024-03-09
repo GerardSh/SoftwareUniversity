31f2a29365a15bc284619a87646ddf1a514417cb
Gerard <gerardshah@gmail.com>
2024-03-09 19:10:51 +0200

    Regular Expressions exercises added

---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/02 Exercise/02 Race/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string[] names = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+Regex regexName = new Regex(@"[A-Za-z]");
+Regex regexDistance = new Regex(@"\d");
+
+Dictionary<string, double> racers = new Dictionary<string, double>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "end of race")
+{
+
+    StringBuilder name = new StringBuilder();
+    double distance = 0;
+
+    foreach (char c in input)
+    {
+        Match matchName = regexName.Match(c.ToString());
+        Match matchDistance = regexDistance.Match(c.ToString());
+        name = name.Append(regexName.Match(c.ToString()).Value);
+
+        if (matchDistance.Success)
+        {
+            distance += double.Parse(matchDistance.Value);
+        }
+    }
+
+    if (!racers.ContainsKey(name.ToString()))
+    {
+        racers.Add(name.ToString(), 0);
+    }
+
+    racers[name.ToString()] += distance;
+}
+
+foreach (string racer in racers.Keys)
+{
+    if (!names.Contains(racer))
+    {
+        racers.Remove(racer);
+    }
+}
+
+racers = racers
+    .OrderByDescending(x => x.Value)
+    .Take(3)
+    .ToDictionary(x => x.Key, x => x.Value);
+
+int count = 1;
+
+foreach (string racer in racers.Keys)
+{
+    if (count == 1)
+    {
+        Console.WriteLine($"1st place: {racer}");
+        count++;
+    }
+    else if (count == 2)
+    {
+        Console.WriteLine($"2nd place: {racer}");
+        count++;
+    }
+    else
+    {
+        Console.WriteLine($"3rd place: {racer}");
+    }
+}
+
+
+
+
+//2
+using System.Text;
+using System.Text.RegularExpressions;
+
+Dictionary<string, int> distanceByRacer = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .ToDictionary(x => x, x => 0);
+
+Regex lettersRegex = new Regex(@"[A-Za-z]+");
+Regex digitsRegex = new Regex(@"\d");
+
+string input;
+
+while ((input = Console.ReadLine()) != "end of race")
+{
+    MatchCollection names = lettersRegex.Matches(input);
+
+    StringBuilder nameBuilder = new StringBuilder();
+
+    foreach (Match match in names)
+    {
+        nameBuilder = nameBuilder.Append(match.Value);
+    }
+
+    string name = nameBuilder.ToString();
+    int distanceSum = 0;
+
+    if (distanceByRacer.ContainsKey(name))
+    {
+        MatchCollection distance = digitsRegex.Matches(input);
+
+        foreach (Match match in distance)
+        {
+            distanceSum += int.Parse(match.Value);
+        }
+
+        distanceByRacer[name] += distanceSum;
+    }
+}
+
+string[] topRacers = distanceByRacer
+    .OrderByDescending(x => x.Value)
+    .Select(x => x.Key)
+    .Take(3)
+    .ToArray();
+
+Console.WriteLine($"1st place: {topRacers[0]}");
+Console.WriteLine($"2nd place: {topRacers[1]}");
+Console.WriteLine($"3rd place: {topRacers[2]}");
---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/02 Exercise/03 SoftUni Bar Income/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+double sum = 0;
+string input;
+
+while ((input = Console.ReadLine()) != "end of shift")
+{
+    Match matchName = Regex.Match(input, @"%(?<name>[A-Z][a-z]+)%");
+    Match matchProduct = Regex.Match(input, @"<(?<product>[\w]+)>");
+    Match matchCount = Regex.Match(input, @"\|(?<count>[\d]+)\|");
+    Match matchPrice = Regex.Match(input, @"(?<price>[\d]+\.?\d*)\$");
+
+
+    if (!matchCount.Success || !matchPrice.Success || !matchName.Success || !matchProduct.Success)
+    {
+        continue;
+    }
+
+    string name = matchName.Groups["name"].Value;
+    string product = matchProduct.Groups["product"].Value;
+
+    int count = int.Parse(matchCount.Groups["count"].Value);
+    double price = double.Parse(matchPrice.Groups["price"].Value);
+
+    double totalPrice = price * count;
+    sum += totalPrice;
+
+    Console.WriteLine($"{name}: {product} - {totalPrice:f2}");
+}
+
+Console.WriteLine($"Total income: {sum:f2}");
+
+
+
+
+//2
+using System.Text.RegularExpressions;
+
+double sum = 0;
+string input;
+
+while ((input = Console.ReadLine()) != "end of shift")
+{
+    Match match = Regex.Match(input, @"\%(?<name>[A-Z][a-z]+)\%.*<(?<product>\w+)>.*\|(?<count>\d+)\|[^1-9]*(?<price>\d+\.?\d*)\$");
+
+    if (!match.Success)
+    {
+        continue;
+    }
+
+    string name = match.Groups["name"].Value;
+    string product = match.Groups["product"].Value;
+
+    int count = int.Parse(match.Groups["count"].Value);
+    double price = double.Parse(match.Groups["price"].Value);
+
+    double totalPrice = price * count;
+    sum += totalPrice;
+
+    Console.WriteLine($"{name}: {product} - {totalPrice:f2}");
+}
+
+Console.WriteLine($"Total income: {sum:f2}");
