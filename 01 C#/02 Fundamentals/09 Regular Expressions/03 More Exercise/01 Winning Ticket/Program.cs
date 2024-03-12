d953f5c192e59ff5e0c44ad0a186cd6f5e6d4098
Gerard <gerardshah@gmail.com>
2024-03-12 22:26:28 +0200

    Regular Expressions exercises added

---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/03 More Exercise/01 Winning Ticket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+List<string> tickets = Regex.Split(Console.ReadLine(), "[, ]+").ToList();
+
+Regex ticketRegex = new Regex(@"[@]{6,10}|[#]{6,10}|[$]{6,10}|[\^]{6,10}");
+
+foreach (string ticket in tickets)
+{
+    if (ticket.Length < 20 || ticket.Length > 20)
+    {
+        Console.WriteLine("invalid ticket");
+        continue;
+    }
+
+    MatchCollection matchesLeft = ticketRegex.Matches(ticket.Substring(0, 10));
+    MatchCollection matchesRight = ticketRegex.Matches(ticket.Substring(10));
+
+    if (matchesLeft.Count < 1 || matchesRight.Count < 1)
+    {
+        Console.WriteLine($"ticket \"{ticket}\" - no match");
+        continue;
+    }
+
+    int winningSequence = Math.Min(matchesLeft[0].Length, matchesRight[0].Length);
+    char winningSymbol = matchesLeft[0].Value[0];
+
+    if (winningSequence < 10)
+    {
+        Console.WriteLine($"ticket \"{ticket}\" - {winningSequence}{winningSymbol}");
+    }
+    else
+    {
+        Console.WriteLine($"ticket \"{ticket}\" - {winningSequence}{winningSymbol} Jackpot!");
+    }
+}
---
+++ 01 C#/02 Fundamentals/09 Regular Expressions/03 More Exercise/02 Rage Quit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string text = Console.ReadLine().ToUpper();
+
+MatchCollection matches = Regex.Matches(text, @"(?<symbols>[^0-9]+)(?<numbers>\d+)");
+
+StringBuilder rageQuitText = new StringBuilder();
+
+foreach (Match match in matches)
+{
+    int repetitions = int.Parse(match.Groups["numbers"].Value);
+
+    for (int i = 0; i < repetitions; i++)
+    {
+        rageQuitText.Append(match.Groups["symbols"].Value);
+    }
+}
+
+Console.WriteLine($"Unique symbols used: {rageQuitText.ToString().Distinct().Count()}");
+Console.WriteLine(rageQuitText);
