b00ba15e8af5383b9e30a8690927c4f8942b4515
Gerard <gerardshah@gmail.com>
2024-03-14 23:48:32 +0200

    Exam preparation added

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/Programming Fundamentals with C#- януари 2021/01 The Imitation Game/Program.cs
@@ -0,0 +1,39 @@
+﻿string input = Console.ReadLine();
+
+string encryptedMessage = input;
+
+while ((input = Console.ReadLine()) != "Decode")
+{
+    string[] commands = input
+        .Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = commands[0];
+
+    if (command == "Move")
+    {
+        int moves = int.Parse(commands[1]);
+
+        for (int i = 0; i < moves; i++)
+        {
+            encryptedMessage = encryptedMessage + encryptedMessage[0];
+            encryptedMessage = encryptedMessage.Remove(0, 1);
+        }
+    }
+    else if (command == "Insert")
+    {
+        int index = int.Parse(commands[1]);
+        string value = commands[2];
+
+        encryptedMessage = encryptedMessage.Insert(index, value);
+
+    }
+    else if (command == "ChangeAll")
+    {
+        string valueToReplace = commands[1];
+        string replacmentValue = commands[2];
+
+        encryptedMessage = encryptedMessage.Replace(valueToReplace, replacmentValue);
+    }
+}
+
+Console.WriteLine($"The decrypted message is: {encryptedMessage}");
---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/Programming Fundamentals with C#- януари 2021/02 Ad Astra/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+MatchCollection matches = Regex.Matches(Console.ReadLine(), @"([#\|])(?<name>[A-Za-z\s]+)\1(?<expiration>\d{2}/\d{2}/\d{2})\1(?<nutrition>\d+)\1");
+
+int sumNutrition = 0;
+
+List<string> items = new List<string>();
+
+foreach (Match match in matches)
+{
+    sumNutrition += int.Parse(match.Groups["nutrition"].Value);
+
+    items.Add($"Item: {match.Groups["name"]}, Best before: {match.Groups["expiration"]}, Nutrition: {match.Groups["nutrition"].Value}");
+}
+
+Console.WriteLine($"You have food to last you for: {sumNutrition / 2000} days!");
+Console.WriteLine(string.Join("\n", items));
