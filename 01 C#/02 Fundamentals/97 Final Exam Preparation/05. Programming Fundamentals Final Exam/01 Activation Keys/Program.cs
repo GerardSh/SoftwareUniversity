e6d587a0b03ddedcac9628bc82c240ae653eedf6
Gerard <gerardshah@gmail.com>
2024-03-16 00:05:04 +0200

    Final exam preparation exercises added

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/05. Programming Fundamentals Final Exam/01 Activation Keys/Program.cs
@@ -0,0 +1,61 @@
+﻿string rawActivationKey = Console.ReadLine();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Generate")
+{
+    string[] commands = input
+        .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = commands[0];
+
+    if (command == "Contains")
+    {
+        string substring = commands[1];
+
+        if (rawActivationKey.Contains(substring))
+        {
+            Console.WriteLine($"{rawActivationKey} contains {substring}");
+        }
+        else
+        {
+            Console.WriteLine("Substring not found!");
+        }
+    }
+    else if (command == "Flip")
+    {
+        string lowerUpper = commands[1];
+        int startIndx = int.Parse(commands[2]);
+        int endIndx = int.Parse(commands[3]);
+        int count = endIndx - startIndx;
+
+        if (lowerUpper == "Lower")
+        {
+            string temp = rawActivationKey.Substring(0, startIndx);
+            temp += rawActivationKey.Substring(startIndx, count).ToLower();
+            temp += rawActivationKey.Substring(endIndx);
+            rawActivationKey = temp;
+        }
+        else
+        {
+            string temp = rawActivationKey.Substring(0, startIndx);
+            temp += rawActivationKey.Substring(startIndx, count).ToUpper();
+            temp += rawActivationKey.Substring(endIndx);
+            rawActivationKey = temp;
+        }
+
+        Console.WriteLine(rawActivationKey);
+    }
+    else if (command == "Slice")
+    {
+        int startIndx = int.Parse(commands[1]);
+        int endIndx = int.Parse(commands[2]);
+        int count = endIndx - startIndx;
+
+        rawActivationKey = rawActivationKey.Remove(startIndx, count);
+
+        Console.WriteLine(rawActivationKey);
+    }
+}
+
+Console.WriteLine($"Your activation key is: {rawActivationKey}");
---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/05. Programming Fundamentals Final Exam/02 Emoji Detector/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+var digitMatches = Regex.Matches(input, @"[\d]");
+
+ulong coolThreshold = 1;
+Console.WriteLine();
+foreach (Match match in digitMatches)
+{
+    coolThreshold *= ulong.Parse(match.Value);
+}
+
+var emojiMatches = Regex.Matches(input, @"(:{2}|\*{2})([A-Z][a-z]{2,})\1");
+
+Console.WriteLine($"Cool threshold: {coolThreshold}");
+Console.WriteLine($"{emojiMatches.Count} emojis found in the text. The cool ones are:");
+
+foreach (Match match in emojiMatches)
+{
+    string emoji = match.Groups[2].Value;
+    ulong emojiSum = 0;
+
+    foreach (char c in emoji)
+    {
+        emojiSum += c;
+    }
+
+    if (emojiSum > coolThreshold)
+    {
+        Console.WriteLine(match.Value);
+    }
+}
