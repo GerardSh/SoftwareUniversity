1388c16b6566a8df85c97e96a20c43c925a6464b
Gerard <gerardshah@gmail.com>
2024-03-16 17:05:41 +0200

    Final exam preparation exercises added

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/02 Programming Fundamentals Final Exam/02 Destination Mapper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+MatchCollection locationMatches = Regex.Matches(input, @"([=/])([A-Z][A-Za-z]{2,})\1");
+
+int sumLength = locationMatches
+    .Select(x => x.Groups[2].Length)
+    .Sum();
+
+List<string> locations = new List<string>();
+
+foreach (Match match in locationMatches)
+{
+    locations.Add(match.Groups[2].Value);
+}
+
+Console.WriteLine($"Destinations: {string.Join(", ", locations)}");
+Console.WriteLine($"Travel Points: {sumLength}");
---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/02 Programming Fundamentals Final Exam/03 Plant Discovery/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<int>> rarityAndRatingsByPlants = new Dictionary<string, List<int>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] plants = Regex.Split(Console.ReadLine(), @"<->");
+
+    string plant = plants[0];
+    int rarity = int.Parse(plants[1]);
+
+    if (!rarityAndRatingsByPlants.ContainsKey(plant))
+    {
+        rarityAndRatingsByPlants[plant] = new List<int>() { 0 };
+    }
+
+    rarityAndRatingsByPlants[plant][0] = rarity;
+}
+
+string input;
+
+while ((input = Console.ReadLine()) != "Exhibition")
+{
+    string[] commands = Regex.Split(input, @" - | ");
+    string command = commands[0];
+    string plant = commands[1];
+
+    if (!rarityAndRatingsByPlants.ContainsKey(plant))
+    {
+        Console.WriteLine("error");
+        continue;
+    }
+
+    if (command == "Rate:")
+    {
+        int rating = int.Parse(commands[2]);
+
+        rarityAndRatingsByPlants[plant].Add(rating);
+    }
+    else if (command == "Update:")
+    {
+        int rarity = int.Parse(commands[2]);
+
+        rarityAndRatingsByPlants[plant][0] = rarity;
+    }
+    else if (command == "Reset:")
+    {
+        int tempRarity = rarityAndRatingsByPlants[plant][0];
+
+        rarityAndRatingsByPlants[plant].Clear();
+        rarityAndRatingsByPlants[plant].Add(tempRarity);
+    }
+}
+
+Console.WriteLine("Plants for the exhibition:");
+
+foreach (var plant in rarityAndRatingsByPlants)
+{
+    string name = plant.Key;
+    int rarity = plant.Value[0];
+    plant.Value.RemoveAt(0);
+
+    double averageRating = plant.Value.Count > 0 ? plant.Value.Average() : 0;
+
+    Console.WriteLine($"- {name}; Rarity: {rarity}; Rating: {averageRating:f2}");
+}
