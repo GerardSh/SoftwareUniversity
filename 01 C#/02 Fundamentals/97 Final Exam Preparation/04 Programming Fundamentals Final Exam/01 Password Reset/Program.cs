4682bd5315c6f29c1043b33fb53aa8924b321fad
Gerard <gerardshah@gmail.com>
2024-03-17 19:15:05 +0200

    Final exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/04 Programming Fundamentals Final Exam/01 Password Reset/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+string password = Console.ReadLine();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Done")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+
+    if (command == "TakeOdd")
+    {
+        StringBuilder newPassword = new StringBuilder();
+
+        for (int i = 0; i < password.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                newPassword.Append(password[i]);
+            }
+        }
+
+        password = newPassword.ToString();
+
+        Console.WriteLine(password);
+    }
+    else if (command == "Cut")
+    {
+        int index = int.Parse(commands[1]);
+        int length = int.Parse(commands[2]);
+
+        string substring = password.Substring(index, length);
+
+        int firstOccurrenceIndex = password.IndexOf(substring);
+
+        password = password.Remove(firstOccurrenceIndex, substring.Length);
+
+        Console.WriteLine(password);
+    }
+    else if (command == "Substitute")
+    {
+        string substring = commands[1];
+        string substitute = commands[2];
+
+        if (!password.Contains(substring))
+        {
+            Console.WriteLine("Nothing to replace!");
+            continue;
+        }
+
+        while (password.Contains(substring))
+        {
+            password = password.Replace(substring, substitute);
+        }
+
+        Console.WriteLine(password);
+    }
+}
+
+Console.WriteLine($"Your password is: {password}");
