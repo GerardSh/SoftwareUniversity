9ca1cb66041a79cc503bae39fc5a819f82127dfa
Gerard <gerardshah@gmail.com>
2024-03-17 20:34:04 +0200

    Final exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/04 Programming Fundamentals Final Exam/02 Fancy Barcodes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+
+    Match validBarcode = Regex.Match(input, @"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+");
+
+    if (validBarcode.Success == true)
+    {
+        string productGroup = string.Empty;
+
+        MatchCollection digits = Regex.Matches(validBarcode.Value, @"\d");
+
+        if (digits.Count > 0)
+        {
+            foreach (Match match in digits)
+            {
+                productGroup += match.Value;
+            }
+        }
+        else
+        {
+            productGroup = "00";
+        }
+
+        Console.WriteLine($"Product group: {productGroup}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid barcode");
+    }
+}
