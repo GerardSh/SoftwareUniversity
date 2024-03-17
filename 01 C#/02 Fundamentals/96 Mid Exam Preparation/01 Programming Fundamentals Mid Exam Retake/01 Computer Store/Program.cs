77dcf17f6942e987d18d845304921e8944aa5fd6
Gerard <gerardshah@gmail.com>
2024-03-18 01:03:47 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/01 Programming Fundamentals Mid Exam Retake/01 Computer Store/Program.cs
@@ -0,0 +1,37 @@
+﻿string input;
+
+decimal sum = 0;
+
+while ((input = Console.ReadLine()) != "special" && input != "regular")
+{
+    decimal price = decimal.Parse(input);
+
+    if (price <= 0)
+    {
+        Console.WriteLine("Invalid price!");
+        continue;
+    }
+
+    sum += price;
+}
+
+if (sum == 0)
+{
+    Console.WriteLine("Invalid order!");
+}
+else
+{
+    decimal taxes = sum * 0.20m;
+    decimal totalPrice = taxes + sum;
+
+    if (input == "special")
+    {
+        totalPrice *= 0.9m;
+    }
+
+    Console.WriteLine($@"Congratulations you've just bought a new computer!
+Price without taxes: {sum:f2}$
+Taxes: {taxes:f2}$
+-----------
+Total price: {totalPrice:f2}$");
+}
