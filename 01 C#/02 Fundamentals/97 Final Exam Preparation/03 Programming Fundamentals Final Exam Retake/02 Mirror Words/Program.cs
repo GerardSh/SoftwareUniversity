a7a976d04a1b72a7e25b4eef38ffb8dfc1a19a60
Gerard <gerardshah@gmail.com>
2024-03-17 10:09:54 +0200

    Final exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/03 Programming Fundamentals Final Exam Retake/02 Mirror Words/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+MatchCollection matches = Regex.Matches(input, @"([@#])([A-Za-z]{3,})\1{2}([A-Za-z]{3,})\1");
+
+if (matches.Count == 0)
+{
+    Console.WriteLine("No word pairs found!");
+}
+else
+{
+    Console.WriteLine($"{matches.Count} word pairs found!");
+}
+
+List<string> mirrorPairs = new List<string>();
+
+foreach (Match match in matches)
+{
+    string wordOne = match.Groups[2].Value;
+    string wordTwo = match.Groups[3].Value;
+    string wordTwoReversed = string.Concat(match.Groups[3].Value.Reverse());
+
+    if (wordOne == wordTwoReversed)
+    {
+        mirrorPairs.Add($"{wordOne} <=> {wordTwo}");
+    }
+}
+
+if (mirrorPairs.Count == 0)
+{
+    Console.WriteLine("No mirror words!");
+}
+else
+{
+    Console.WriteLine($"The mirror words are:\n{string.Join(", ", mirrorPairs)}");
+}
