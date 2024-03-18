f724766c42cc377efe65d906d155496f14c9b188
Gerard <gerardshah@gmail.com>
2024-03-18 09:58:39 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/02 Programming Fundamentals Mid Exam/02 Array Modifier/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] array = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string input;
+
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+
+    if (command == "swap" || command == "multiply")
+    {
+        int indexOne = int.Parse(commands[1]);
+        int indexTwo = int.Parse(commands[2]);
+
+        if (command == "swap")
+        {
+            int temp = array[indexOne];
+            array[indexOne] = array[indexTwo];
+            array[indexTwo] = temp;
+        }
+        else
+        {
+            array[indexOne] *= array[indexTwo];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] -= 1;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", array));
