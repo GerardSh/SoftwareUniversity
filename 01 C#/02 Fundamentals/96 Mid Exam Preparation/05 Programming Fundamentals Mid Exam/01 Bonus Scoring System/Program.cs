9844a65b15c3b1acaee4da852751dfd977d3b821
Gerard <gerardshah@gmail.com>
2024-03-18 15:47:50 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/05 Programming Fundamentals Mid Exam/01 Bonus Scoring System/Program.cs
@@ -0,0 +1,22 @@
+﻿int students = int.Parse(Console.ReadLine());
+int lectures = int.Parse(Console.ReadLine());
+int additionalBonus = int.Parse(Console.ReadLine());
+
+double maxBonus = 0;
+int bestStudentAttendances = 0;
+
+for (int i = 0; i < students; i++)
+{
+    int attendances = int.Parse(Console.ReadLine());
+
+    double bonus = 1.0 * attendances / lectures * (5 + additionalBonus);
+
+    if (bonus > maxBonus)
+    {
+        maxBonus = bonus;
+        bestStudentAttendances = attendances;
+    }
+}
+
+Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonus)}.");
+Console.WriteLine($"The student has attended {bestStudentAttendances} lectures.");
