5d738e9eb96ce8e189ebffc4457655d91aef922f
Gerard <gerardshah@gmail.com>
2024-03-18 16:08:23 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/05 Programming Fundamentals Mid Exam/03 Inventory/Program.cs
@@ -0,0 +1,48 @@
+﻿List<string> items = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Craft!")
+{
+    string[] commands = input
+        .Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+    string item = commands[1];
+
+    if (command == "Collect")
+    {
+        if (!items.Contains(item))
+        {
+            items.Add(item);
+        }
+    }
+    else if (command == "Drop")
+    {
+        items.Remove(item);
+    }
+    else if (command == "Combine Items")
+    {
+        string[] itemsToCombine = item
+            .Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+        string oldItem = itemsToCombine[0];
+        string newItem = itemsToCombine[1];
+
+        if (items.Contains(oldItem))
+        {
+            items.Insert(items.IndexOf(oldItem) + 1, newItem);
+        }
+    }
+    else
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            items.Add(item);
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", items));
