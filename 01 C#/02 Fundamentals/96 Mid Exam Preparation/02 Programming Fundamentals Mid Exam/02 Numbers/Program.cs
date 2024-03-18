582fd2700c528b7dcf50ed9727ee2010be815977
Gerard <gerardshah@gmail.com>
2024-03-18 09:36:17 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/02 Programming Fundamentals Mid Exam/02 Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+double avrg = Math.Round(GetAverage(array), 2);
+
+Array.Sort(array);
+Array.Reverse(array);
+
+bool greaterThanAverage = false;
+
+for (int i = 0; i < array.Length && i < 5; i++)
+{
+    if (array[i] > avrg)
+    {
+        Console.Write(array[i] + " ");
+        greaterThanAverage = true;
+    }
+}
+
+if (!greaterThanAverage)
+{
+    Console.WriteLine("No");
+}
+
+static double GetAverage(int[] array)
+{
+    double sum = 0;
+
+    foreach (var item in array)
+    {
+        sum += item;
+    }
+
+    return sum / array.Length;
+}
