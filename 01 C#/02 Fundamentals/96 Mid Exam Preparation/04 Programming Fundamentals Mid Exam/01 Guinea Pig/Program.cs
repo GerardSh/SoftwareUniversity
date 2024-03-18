745e61311f2472836716f9a732e626eec099c65b
Gerard <gerardshah@gmail.com>
2024-03-18 13:30:03 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/04 Programming Fundamentals Mid Exam/01 Guinea Pig/Program.cs
@@ -0,0 +1,30 @@
+﻿double quantityFood = double.Parse(Console.ReadLine()) * 1000;
+double quantityHay = double.Parse(Console.ReadLine()) * 1000;
+double quantityCover = double.Parse(Console.ReadLine()) * 1000;
+double weigth = double.Parse(Console.ReadLine()) * 1000;
+
+for (int i = 1; i <= 30; i++)
+{
+    quantityFood -= 300;
+
+    if (i % 2 == 0)
+    {
+        double neededHay = quantityFood * 0.05;
+
+        quantityHay -= neededHay;
+    }
+
+    if (i % 3 == 0)
+    {
+        double neededCover = weigth / 3;
+        quantityCover -= neededCover;
+    }
+
+    if (quantityFood <= 0 || quantityHay <= 0 || quantityCover <= 0)
+    {
+        Console.WriteLine("Merry must go to the pet store!");
+        return;
+    }
+}
+
+Console.WriteLine($"Everything is fine! Puppy is happy! Food: {quantityFood / 1000:f2}, Hay: {quantityHay / 1000:f2}, Cover: {quantityCover / 1000:f2}.");
