a6e09260f17a733f47c336fcd45523a021550a54
Gerard <gerardshah@gmail.com>
2024-03-18 17:25:44 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/05 Programming Fundamentals Mid Exam - Copy/02 Treasure Hunt/Program.cs
@@ -0,0 +1,60 @@
+﻿List<string> items = Console.ReadLine()
+    .Split("|", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Yohoho!")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+
+    if (command == "Loot")
+    {
+        for (int i = 1; i < commands.Length; i++)
+        {
+            if (!items.Contains(commands[i]))
+            {
+                items.Insert(0, commands[i]);
+            }
+        }
+    }
+    else if (command == "Drop")
+    {
+        int index = int.Parse(commands[1]);
+
+        if (index >= 0 && index < items.Count)
+        {
+            string temp = items[index];
+
+            items.RemoveAt(index);
+            items.Add(temp);
+        }
+    }
+    else
+    {
+        int count = int.Parse(commands[1]);
+        List<string> stolenItems = new List<string>();
+
+        if (items.Count < count)
+        {
+            count = items.Count;
+        }
+
+        stolenItems = items.TakeLast(count).ToList();
+        items.RemoveRange(items.Count - count, count);
+
+        Console.WriteLine(string.Join(", ", stolenItems));
+    }
+}
+
+if (items.Count == 0)
+{
+    Console.WriteLine("Failed treasure hunt.");
+}
+else
+{
+    double averageGain = 1.0 * items.Sum(x => x.Length) / items.Count;
+    Console.WriteLine($"Average treasure gain: {averageGain:f2} pirate credits.");
+}
