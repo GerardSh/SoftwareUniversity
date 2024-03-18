b4bf2cf4269c4a57321932b6909d3b1ceb338d84
Gerard <gerardshah@gmail.com>
2024-03-18 15:48:20 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/05 Programming Fundamentals Mid Exam/02 MuOnline/Program.cs
@@ -0,0 +1,63 @@
+﻿int health = 100;
+int bitcoins = 0;
+bool alive = true;
+
+string[] dungeonRooms = Console.ReadLine()
+    .Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+for (int i = 0; i < dungeonRooms.Length; i++)
+{
+    string[] roomData = dungeonRooms[i].Split(" ");
+
+    string name = roomData[0];
+    int number = int.Parse(roomData[1]);
+
+    if (name == "potion")
+    {
+        int amount = 0;
+
+        if (health + number > 100)
+        {
+            amount = 100 - health;
+        }
+        else
+        {
+            amount = number;
+        }
+
+        health += amount;
+
+        Console.WriteLine($"You healed for {amount} hp.");
+        Console.WriteLine($"Current health: {health} hp.");
+    }
+    else if (name == "chest")
+    {
+        bitcoins += number;
+
+        Console.WriteLine($"You found {number} bitcoins.");
+    }
+    else
+    {
+        health -= number;
+
+        if (health <= 0)
+        {
+            Console.WriteLine($"You died! Killed by {name}.");
+            Console.WriteLine($"Best room: {i + 1}");
+
+            alive = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"You slayed {name}.");
+        }
+    }
+}
+
+if (alive)
+{
+    Console.WriteLine("You've made it!");
+    Console.WriteLine($"Bitcoins: {bitcoins}");
+    Console.WriteLine($"Health: {health}");
+}
