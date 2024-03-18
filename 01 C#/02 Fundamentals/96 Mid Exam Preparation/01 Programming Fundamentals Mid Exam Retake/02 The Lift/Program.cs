5ea8862265f58824ea499bfb81aacb053ac9fde0
Gerard <gerardshah@gmail.com>
2024-03-18 09:30:18 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/01 Programming Fundamentals Mid Exam Retake/02 The Lift/Program.cs
@@ -0,0 +1,41 @@
+﻿int people = int.Parse(Console.ReadLine());
+
+int[] lift = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i < lift.Length; i++)
+{
+    int wagon = lift[i];
+
+    if (wagon < 4)
+    {
+        int wagonFreeCapacity = 4 - wagon;
+        if (people >= wagonFreeCapacity)
+        {
+            people -= wagonFreeCapacity;
+            lift[i] += wagonFreeCapacity;
+        }
+        else
+        {
+            lift[i] += people;
+            people = 0;
+        }
+    }
+    if (people == 0 && i == lift.Length - 1 && lift[i] == 4)
+    {
+        Console.WriteLine(string.Join(" ", lift));
+    }
+    else if (people == 0)
+    {
+        Console.WriteLine($"The lift has empty spots!");
+        Console.WriteLine(string.Join(" ", lift));
+        break;
+    }
+    else if (i == lift.Length - 1)
+    {
+        Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+        Console.WriteLine(string.Join(" ", lift));
+    }
+}
