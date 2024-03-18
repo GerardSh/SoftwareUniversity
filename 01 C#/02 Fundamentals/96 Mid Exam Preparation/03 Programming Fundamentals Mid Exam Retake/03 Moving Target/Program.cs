83c1a7a94c7e085d93b5a70347b882592589f110
Gerard <gerardshah@gmail.com>
2024-03-18 11:43:38 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/03 Programming Fundamentals Mid Exam Retake/03 Moving Target/Program.cs
@@ -0,0 +1,59 @@
+﻿List<int> targets = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string input;
+
+while ((input = Console.ReadLine()) != "End")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+
+    int index = int.Parse(commands[1]);
+
+    if (command == "Shoot")
+    {
+        int power = int.Parse(commands[2]);
+
+        if (index < 0 || index >= targets.Count)
+        {
+            continue;
+        }
+
+        targets[index] -= power;
+
+        if (targets[index] <= 0)
+        {
+            targets.RemoveAt(index);
+        }
+    }
+    else if (command == "Add")
+    {
+        int value = int.Parse(commands[2]);
+
+        if (index < 0 || index >= targets.Count)
+        {
+            Console.WriteLine("Invalid placement!");
+        }
+        else
+        {
+            targets.Insert(index, value);
+        }
+    }
+    else
+    {
+        int radius = int.Parse(commands[2]);
+
+        if (index - radius < 0 || index + radius >= targets.Count)
+        {
+            Console.WriteLine("Strike missed!");
+            continue;
+        }
+
+        targets.RemoveRange(index - radius, radius * 2 + 1);
+    }
+}
+
+Console.WriteLine(string.Join("|", targets));
