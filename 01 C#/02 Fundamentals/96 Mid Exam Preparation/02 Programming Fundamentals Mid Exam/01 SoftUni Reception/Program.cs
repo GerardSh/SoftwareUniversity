80c81342d9952da7a8f06fbb05c4ef8f6f428b06
Gerard <gerardshah@gmail.com>
2024-03-18 09:35:14 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/02 Programming Fundamentals Mid Exam/01 SoftUni Reception/Program.cs
@@ -0,0 +1,21 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+int capacityHour = n1 + n2 + n3;
+int students = int.Parse(Console.ReadLine());
+
+int hours = 0;
+
+while (students > 0)
+{
+    hours++;
+
+    if (hours % 4 == 0)
+    {
+        continue;
+    }
+
+    students = students - capacityHour;
+}
+
+Console.WriteLine($"Time needed: {hours}h.");
