f3fb7d39eb142a2124ab59b76182231d5376f5e2
Gerard <gerardshah@gmail.com>
2024-03-18 15:06:41 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/04 Programming Fundamentals Mid Exam/02 Shopping List/Program.cs
@@ -0,0 +1,46 @@
+﻿List<string> shoppingList = Console.ReadLine()
+    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Go Shopping!")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+    string item = commands[1];
+
+    if (command == "Urgent")
+    {
+        if (!shoppingList.Contains(item))
+        {
+            shoppingList.Insert(0, item);
+        }
+    }
+    else if (command == "Unnecessary")
+    {
+        shoppingList.Remove(item);
+    }
+    else if (command == "Correct")
+    {
+        string newItem = commands[2];
+
+        int indexOldItem;
+
+        if ((indexOldItem = shoppingList.IndexOf(item)) != -1)
+        {
+            shoppingList[indexOldItem] = newItem;
+        }
+    }
+    else
+    {
+        if (shoppingList.Contains(item))
+        {
+            shoppingList.Remove(item);
+            shoppingList.Add(item);
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", shoppingList));
---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/04 Programming Fundamentals Mid Exam/03 Heart Delivery/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+
+List<int> neighborhood = Console.ReadLine()
+    .Split("@", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int lastPosition = 0;
+
+string input;
+
+while ((input = Console.ReadLine()) != "Love!")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+    int indx = int.Parse(commands[1]) + lastPosition;
+
+    if (indx >= neighborhood.Count)
+    {
+        indx = 0;
+    }
+
+    lastPosition = indx;
+
+    if (neighborhood[indx] == 0)
+    {
+        Console.WriteLine($"Place {indx} already had Valentine's day.");
+        continue;
+    }
+
+    neighborhood[indx] -= 2;
+
+    if (neighborhood[indx] == 0)
+    {
+        Console.WriteLine($"Place {indx} has Valentine's day.");
+    }
+}
+
+Console.WriteLine($"Cupid's last position was {lastPosition}.");
+
+if (neighborhood.Sum() == 0)
+{
+    Console.WriteLine("Mission was successful.");
+}
+else
+{
+    Console.WriteLine($"Cupid has failed {neighborhood.Where(x => x > 0).Count()} places.");
+}
