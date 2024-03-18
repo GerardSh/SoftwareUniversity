c3eeb519974eda25ddaa739c677e599fb9d93f7c
Gerard <gerardshah@gmail.com>
2024-03-18 10:16:12 +0200

    Mid exam preparation exercise added

---
+++ 01 C#/02 Fundamentals/96 Mid Exam Preparation/03 Programming Fundamentals Mid Exam Retake/01 Counter-Strike/Program.cs
@@ -0,0 +1,30 @@
+﻿int energy = int.Parse(Console.ReadLine());
+int wonGamesCount = 0;
+int gamesCount = 0;
+
+string input;
+
+while ((input = Console.ReadLine()) != "End of battle")
+{
+    gamesCount++;
+
+    int distance = int.Parse(input);
+
+    if (energy >= distance)
+    {
+        energy -= distance;
+        wonGamesCount++;
+    }
+    else
+    {
+        Console.WriteLine($"Not enough energy! Game ends with {wonGamesCount} won battles and {energy} energy");
+        return;
+    }
+
+    if (gamesCount % 3 == 0)
+    {
+        energy += wonGamesCount;
+    }
+}
+
+Console.WriteLine($"Won battles: {wonGamesCount}. Energy left: {energy}");
