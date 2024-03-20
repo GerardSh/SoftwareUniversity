0fb107c3a0e1500a4e27e63bf6ebf6ca5e08990d
Gerard <gerardshah@gmail.com>
2024-03-20 23:59:24 +0200

    Advanced Stacks and Queues Lab exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/01 Lab/02 Stack Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Stack<int> stack = new Stack<int>();
+
+foreach (int number in numbers)
+{
+    stack.Push(number);
+}
+
+string input;
+
+while ((input = Console.ReadLine().ToLower()) != "end")
+{
+    string[] commands = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string command = commands[0];
+
+    if (command == "add")
+    {
+        stack.Push(int.Parse(commands[1]));
+        stack.Push(int.Parse(commands[2]));
+    }
+    else
+    {
+        int number = int.Parse(commands[1]);
+
+        if (stack.Count > number)
+        {
+            while (number > 0)
+            {
+                number--;
+                stack.Pop();
+            }
+        }
+    }
+}
+
+Console.WriteLine("Sum: " + stack.Sum());
