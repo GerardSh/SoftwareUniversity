3c42105c1c4e00a7ce3855ebd9de526630a2707c
Gerard <gerardshah@gmail.com>
2024-03-21 08:24:53 +0200

    Advanced Stacks and Queues Lab exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/01 Lab/04 Matching Brackets/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+
+    if (input[i] == '(')
+    {
+        stack.Push(i);
+    }
+
+    if (input[i] == ')')
+    {
+        int startIndx = stack.Pop();
+        int count = i - startIndx + 1;
+
+        Console.WriteLine(input.Substring(startIndx, count));
+    }
+}
