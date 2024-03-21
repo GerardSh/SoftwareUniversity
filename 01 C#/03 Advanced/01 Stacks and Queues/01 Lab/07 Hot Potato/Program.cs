c1308d638ea829a578cf54f8e9bfd39fababdf58
Gerard <gerardshah@gmail.com>
2024-03-21 16:09:45 +0200

    Advanced Stacks and Queues Lab exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/01 Lab/07 Hot Potato/Program.cs
@@ -0,0 +1,53 @@
+﻿Queue<string> names = new Queue<string>(Console.ReadLine().Split());
+int rotations = int.Parse(Console.ReadLine());
+
+while (names.Count > 1)
+{
+    for (int i = 1; i <= rotations; i++)
+    {
+        if (i == rotations)
+        {
+            Console.WriteLine($"Removed {names.Dequeue()}");
+        }
+        else
+        {
+            names.Enqueue(names.Dequeue());
+        }
+    }
+}
+
+Console.WriteLine("Last is " + names.Dequeue());
+
+
+
+
+//2 with List
+List<string> names = Console.ReadLine().Split().ToList();
+int n = int.Parse(Console.ReadLine());
+int index = 0;
+
+while (names.Count > 1)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (index >= names.Count - 1)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+    }
+
+    Console.WriteLine("Removed " + names[index]);
+
+    names.Remove(names[index]);
+
+    if (index == names.Count)
+    {
+        index = 0;
+    }
+}
+
+Console.WriteLine("Last is " + names[0]);
