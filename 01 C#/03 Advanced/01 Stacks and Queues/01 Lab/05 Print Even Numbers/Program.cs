270f2a757791863b43e4c95e7235200093985c09
Gerard <gerardshah@gmail.com>
2024-03-21 14:02:04 +0200

    Advanced Stacks and Queues Lab exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/01 Lab/05 Print Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+Queue<int> numbers = new Queue<int>(Console.ReadLine()
+    .Split()
+    .Select(int.Parse));
+
+while (numbers.Count > 0)
+{
+    if (numbers.Peek() % 2 == 0)
+    {
+        if (numbers.Count > 1)
+        {
+            Console.Write(numbers.Dequeue() + ", ");
+        }
+        else
+        {
+            Console.Write(numbers.Dequeue());
+        }
+    }
+    else
+    {
+        numbers.Dequeue();
+    }
+}
