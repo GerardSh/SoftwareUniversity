caa381ea77fbb3a4d0c315127ea88b038085e7ca
Gerard <gerardshah@gmail.com>
2024-03-21 14:08:46 +0200

    Advanced Stacks and Queues Lab exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/01 Lab/06 Supermarket/Program.cs
@@ -0,0 +1,19 @@
+﻿string input;
+Queue<string> customers = new Queue<string>();
+
+while ((input = Console.ReadLine()) != "End")
+{
+    if (input == "Paid")
+    {
+        while (customers.Count > 0)
+        {
+            Console.WriteLine(customers.Dequeue());
+        }
+    }
+    else
+    {
+        customers.Enqueue(input);
+    }
+}
+
+Console.WriteLine($"{customers.Count()} people remaining.");
