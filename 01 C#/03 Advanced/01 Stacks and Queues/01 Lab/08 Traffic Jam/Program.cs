93d0473c70981b12c8eb1b20c03460c1359064db
Gerard <gerardshah@gmail.com>
2024-03-21 16:22:19 +0200

    Advanced Stacks and Queues Lab exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/01 Lab/08 Traffic Jam/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+int carsCount = 0;
+
+string input = "";
+
+while ((input = Console.ReadLine()) != "end")
+{
+    if (input == "green")
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (cars.Count > 0)
+            {
+                Console.WriteLine(cars.Dequeue() + " passed!");
+                carsCount++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        cars.Enqueue(input);
+    }
+}
+
+Console.WriteLine($"{carsCount} cars passed the crossroads.");
