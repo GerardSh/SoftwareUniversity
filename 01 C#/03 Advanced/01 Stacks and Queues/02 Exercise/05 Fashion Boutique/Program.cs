1aabdf1918dc5bab3e8ee86206ce9ce991b601fd
Gerard <gerardshah@gmail.com>
2024-03-22 20:44:45 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/05 Fashion Boutique/Program.cs
@@ -0,0 +1,45 @@
+﻿Stack<int> clothBox = new Stack<int>(Console.ReadLine()
+    .Split()
+    .Select(int.Parse));
+
+int rackCapacity = int.Parse(Console.ReadLine());
+int currentRackSpace = 0;
+int racksUsed = 0;
+
+if (rackCapacity == 0)
+{
+    racksUsed = 0;
+    return;
+}
+while (clothBox.Count > 0)
+{
+    currentRackSpace += clothBox.Peek();
+
+    if (currentRackSpace > rackCapacity)
+    {
+        racksUsed++;
+        currentRackSpace = clothBox.Pop();
+
+        if (clothBox.Count == 0)
+        {
+            racksUsed++;
+        }
+    }
+    else if (currentRackSpace == rackCapacity)
+    {
+        racksUsed++;
+        currentRackSpace = 0;
+        clothBox.Pop();
+    }
+    else
+    {
+        clothBox.Pop();
+
+        if (clothBox.Count == 0)
+        {
+            racksUsed++;
+        }
+    }
+}
+
+Console.WriteLine(racksUsed);
