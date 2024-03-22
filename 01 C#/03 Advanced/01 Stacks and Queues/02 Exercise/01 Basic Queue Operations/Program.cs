9c8724b12ec8fae915566adc3bcfad0ea47edb8f
Gerard <gerardshah@gmail.com>
2024-03-22 15:22:42 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/01 Basic Queue Operations/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] initialNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int n = initialNumbers[0];
+int s = initialNumbers[1];
+int x = initialNumbers[2];
+
+int[] numbersToAdd = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> numbers = new Queue<int>(numbersToAdd.Take(n));
+
+for (int i = 0; i < s; i++)
+{
+    numbers.Dequeue();
+}
+
+if (numbers.Count > 0)
+{
+    if (numbers.Contains(x))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine(numbers.Min());
+    }
+}
+else
+{
+    Console.WriteLine(0);
+}
