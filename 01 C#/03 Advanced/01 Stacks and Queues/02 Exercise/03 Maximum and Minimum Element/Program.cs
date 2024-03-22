66e070a70cc5262134a23d0d76da26a849529bd9
Gerard <gerardshah@gmail.com>
2024-03-22 17:45:56 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/03 Maximum and Minimum Element/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Stack<int> stack = new Stack<int>(n);
+
+for (int i = 0; i < n; i++)
+{
+    int[] elements = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    int command = elements[0];
+
+    if (command == 1)
+    {
+        int numberToAdd = elements[1];
+
+        stack.Push(numberToAdd);
+    }
+    else if (command == 2)
+    {
+        if (stack.Count > 0)
+        {
+            stack.Pop();
+        }
+    }
+    else
+    {
+        if (stack.Count > 0)
+        {
+            Console.WriteLine(command == 3 ? stack.Max() : stack.Min());
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", stack));
