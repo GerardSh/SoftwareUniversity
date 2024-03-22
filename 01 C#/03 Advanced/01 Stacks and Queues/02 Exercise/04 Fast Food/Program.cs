d744f29070810b721e89e8335781128c0decc5b2
Gerard <gerardshah@gmail.com>
2024-03-22 18:05:07 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/04 Fast Food/Program.cs
@@ -0,0 +1,28 @@
+﻿int amountOfFood = int.Parse(Console.ReadLine());
+
+Queue<int> orders = new Queue<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Console.WriteLine(orders.Max());
+
+while (orders.Count > 0 && amountOfFood > 0)
+{
+    if (orders.Peek() <= amountOfFood)
+    {
+        amountOfFood -= orders.Dequeue();
+    }
+    else
+    {
+        break;
+    }
+}
+
+if (orders.Count > 0)
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
