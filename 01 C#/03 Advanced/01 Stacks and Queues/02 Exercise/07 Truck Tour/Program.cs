b24908cbdf3e25cb19ea95bf81d8cd1e0b4fd95a
Gerard <gerardshah@gmail.com>
2024-03-23 16:51:24 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/07 Truck Tour/Program.cs
@@ -0,0 +1,63 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Queue<int[]> pumps = new Queue<int[]>();
+
+for (int i = 0; i < n; i++)
+{
+    int[] elements = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+
+    pumps.Enqueue(new int[] { elements[0], elements[1] });
+}
+
+Queue<int[]> pumpsOriginal = new Queue<int[]>(pumps);
+int index = 0;
+for (int i = 0; i < n; i++)
+{
+    int remainingFuel = pumps.Peek()[0] - pumps.Peek()[1];
+
+    if (remainingFuel >= 0)
+    {
+        pumps.Dequeue();
+
+        index = i;
+
+        while (pumps.Count > 0)
+        {
+            remainingFuel += pumps.Peek()[0] - pumps.Peek()[1];
+
+            if (remainingFuel >= 0)
+            {
+                pumps.Dequeue();
+            }
+            else
+            {
+                pumps = new Queue<int[]>(pumpsOriginal);
+
+                for (int j = 0; j <= i; j++)
+                {
+                    pumps.Enqueue(pumps.Dequeue());
+                }
+                break;
+            }
+        }
+
+        if (pumps.Count == 0)
+        {
+            break;
+        }
+
+        if (i == n - 1)
+        {
+            index = -1;
+        }
+    }
+    else
+    {
+        pumps.Enqueue(pumps.Dequeue());
+    }
+}
+
+Console.WriteLine(index);
