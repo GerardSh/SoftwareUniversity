2c8f2b310fe183fe4f62e50f4e1a5d7a4f008be9
Gerard <gerardshah@gmail.com>
2024-03-24 23:55:19 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/08 Balanced Parenthesis/Program.cs
@@ -0,0 +1,36 @@
+﻿string input = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>(input);
+Queue<char> queue = new Queue<char>(input);
+
+bool isEqual = true;
+
+if (input.Length % 2 != 0)
+{
+    Console.WriteLine("NO");
+    return;
+}
+
+for (int i = 0; i < input.Length / 2; i++)
+{
+    char firstChar = queue.Dequeue();
+    char secondChar = stack.Pop();
+
+    if (firstChar == '{' && secondChar != '}'
+        || firstChar == '[' && secondChar != ']'
+        || firstChar == '(' && secondChar != ')')
+    {
+        isEqual = false;
+        break;
+    }
+}
+
+
+if (isEqual)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
