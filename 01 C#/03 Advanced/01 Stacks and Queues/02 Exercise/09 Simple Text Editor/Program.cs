ce4c23878a0256ce77dab08f35efaf1b03a524a6
Gerard <gerardshah@gmail.com>
2024-03-25 12:32:28 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/09 Simple Text Editor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+int numberOfOperations = int.Parse(Console.ReadLine());
+StringBuilder text = new StringBuilder("");
+Stack<string> textVersions = new Stack<string>();
+
+for (int i = 0; i < numberOfOperations; i++)
+{
+    string[] elements = Regex.Split(Console.ReadLine(), @" ");
+
+    int command = int.Parse(elements[0]);
+    string arg = elements.Length > 1 ? elements[1] : null;
+
+    if (command == 1)
+    {
+        string textToAdd = arg;
+
+        textVersions.Push(text.ToString());
+        text.Append(textToAdd);
+    }
+    else if (command == 2)
+    {
+        int elementsToDelete = int.Parse(arg);
+
+        textVersions.Push(text.ToString());
+        text.Remove(text.Length - elementsToDelete, elementsToDelete);
+    }
+    else if (command == 3)
+    {
+        int index = int.Parse(arg);
+
+        if (index <= text.Length)
+        {
+            Console.WriteLine(text[index - 1]);
+        }
+    }
+    else if (command == 4)
+    {
+        text = new StringBuilder(textVersions.Pop());
+    }
+}
