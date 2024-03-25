473cd88f6e4393f3eafcb48b44ecbcd7c5fdd97c
Gerard <gerardshah@gmail.com>
2024-03-25 17:28:58 +0200

    Advanced Stacks and Queues Exercise added

---
+++ 01 C#/03 Advanced/01 Stacks and Queues/02 Exercise/10 Crossroads/Program.cs
@@ -0,0 +1,37 @@
+﻿int greenLight = int.Parse(Console.ReadLine());
+int freeWindow = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+int passedCars = 0;
+
+string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string currentCar = input;
+
+    if (currentCar != "green")
+    {
+        cars.Enqueue(currentCar);
+    }
+    else
+    {
+        int remainingSeconds = greenLight;
+        while (remainingSeconds > 0 && cars.Count > 0)
+        {
+            string passingCar = cars.Dequeue();
+            remainingSeconds -= passingCar.Length;
+            passedCars++;
+
+            if (remainingSeconds + freeWindow < 0)
+            {
+                Console.WriteLine("A crash happened!");
+                Console.WriteLine($"{passingCar} was hit at {passingCar[passingCar.Length - (remainingSeconds + freeWindow) * -1]}.");
+                return;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{passedCars} total cars passed the crossroads.");
