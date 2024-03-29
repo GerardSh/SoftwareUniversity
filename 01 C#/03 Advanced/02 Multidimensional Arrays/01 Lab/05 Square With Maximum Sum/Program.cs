8eccbcdf06833401ab861229b7399cf828aa3d79
Gerard <gerardshah@gmail.com>
2024-03-29 17:07:26 +0200

    Update Program.cs

--- 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/05 Square With Maximum Sum/Program.cs
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/05 Square With Maximum Sum/Program.cs
@@ -34,3 +34,68 @@ for (int row = 0; row < rows - 1; row++)
 
 Console.WriteLine(subMatrix);
 Console.WriteLine(biggestSum);
+
+
+
+
+//Performing the same exercise but with the addition of defining the sub-matrix size.
+string[] elements = Console.ReadLine().Split(", ");
+
+int rows = int.Parse(elements[0]);
+int cols = int.Parse(elements[1]);
+
+elements = Console.ReadLine().Split(", ");
+
+int subMatrixRows = int.Parse(elements[0]);
+int subMatrixCols = int.Parse(elements[1]);
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    elements = Console.ReadLine().Split(", ");
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = int.Parse(elements[col]);
+    }
+}
+
+int biggestSum = 0;
+int biggestRow = 0;
+int biggestCol = 0;
+
+for (int row = 0; row < rows - subMatrixRows + 1; row++)
+{
+    for (int col = 0; col < cols - subMatrixCols + 1; col++)
+    {
+        int subMatrixSum = 0;
+
+        for (int subRow = 0; subRow < subMatrixRows; subRow++)
+        {
+            for (int subCol = 0; subCol < subMatrixCols; subCol++)
+            {
+                subMatrixSum += matrix[row + subRow, col + subCol];
+            }
+        }
+
+        if (biggestSum < subMatrixSum)
+        {
+            biggestSum = subMatrixSum;
+            biggestRow = row;
+            biggestCol = col;
+        }
+    }
+}
+
+for (int row = biggestRow; row < biggestRow + subMatrixRows; row++)
+{
+    for (int col = biggestCol; col < biggestCol + subMatrixCols; col++)
+    {
+        Console.Write(matrix[row, col] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine(biggestSum);
