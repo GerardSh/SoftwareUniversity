e46c4954962c55708dea83880b16daaa4eeecde2
Gerard <gerardshah@gmail.com>
2024-03-29 18:40:57 +0200

    Advanced Multidimensional Arrays Lab exercise added

---
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/99 Jagged array/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[][] jaggedArray = new int[n][];
+
+for (int i = 0; i < n; i++)
+{
+    string[] elements = Console.ReadLine().Split();
+
+    jaggedArray[i] = new int[elements.Length];
+
+    for (int j = 0; j < elements.Length; j++)
+    {
+        jaggedArray[i][j] = int.Parse(elements[j]);
+    }
+}
+
+for (int i = 0; i < jaggedArray.Length; ++i)
+{
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        Console.Write(jaggedArray[i][j] + " ");
+    }
+
+    Console.WriteLine();
+}
