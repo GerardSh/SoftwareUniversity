4ac1bbd037e6254e66f84d1132e4d9d182d3336a
Gerard <gerardshah@gmail.com>
2024-03-29 11:11:04 +0200

    Advanced Multidimensional Arrays Lab exercise added

---
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/02 Sum Matrix Columns/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] elements = Console.ReadLine().Split(", ");
+
+int rows = int.Parse(elements[0]);
+int cols = int.Parse(elements[1]);
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    elements = Console.ReadLine().Split();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = int.Parse(elements[col]);
+    }
+}
+
+for (int col = 0; col < cols; col++)
+{
+    int sum = 0;
+
+    for (int row = 0; row < rows; row++)
+    {
+        sum += matrix[row, col];
+    }
+
+    Console.WriteLine(sum);
+}
