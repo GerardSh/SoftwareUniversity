cf02ccb864221bdeb0daa816ed8aaf63b47ada6f
Gerard <gerardshah@gmail.com>
2024-03-29 13:49:04 +0200

    Advanced Multidimensional Arrays Lab exercise added

---
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/04 Symbol in Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int rows = n;
+int cols = n;
+
+char[,] matrix = new char[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    char[] chars = Console.ReadLine()
+        .ToCharArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = chars[col];
+    }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+string position = null;
+bool isFound = false;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        if (matrix[row, col] == symbol)
+        {
+            position = $"({row}, {col})";
+            isFound = true;
+            break;
+        }
+    }
+
+    if (isFound)
+    {
+        break;
+    }
+}
+
+if (position != null)
+{
+    Console.WriteLine(position);
+}
+else
+{
+    Console.WriteLine($"{symbol} does not occur in the matrix");
+}
