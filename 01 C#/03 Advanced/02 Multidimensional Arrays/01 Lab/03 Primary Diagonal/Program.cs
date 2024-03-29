eb19a0b4b32e5c7298fb49b128d69cb0bf9ea2e8
Gerard <gerardshah@gmail.com>
2024-03-29 13:28:01 +0200

    Advanced Multidimensional Arrays Lab exercise added

---
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/03 Primary Diagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int rows = n;
+int cols = n;
+
+int count = 0;
+int diagonalSum = 0;
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    string[] elements = Console.ReadLine().Split();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = int.Parse(elements[col]);
+
+        if (col == count)
+        {
+            diagonalSum += matrix[row, col];
+        }
+    }
+
+    count++;
+}
+
+Console.WriteLine(diagonalSum);
