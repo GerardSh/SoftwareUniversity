c4c41c2d10cb466800f9ac0088b5c91e1fff6ccb
Gerard <gerardshah@gmail.com>
2024-03-30 11:28:40 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/02 Programming Fundamentals Final Exam/01 World Tour/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/02 Programming Fundamentals Final Exam/01 World Tour/Program.cs
@@ -51,3 +51,57 @@ while ((input = Console.ReadLine()) != "Travel")
 }
 
 Console.WriteLine($"Ready for world tour! Planned stops: {text}");
+
+
+
+
+//2
+using System.Text;
+
+StringBuilder destinations = new StringBuilder(Console.ReadLine());
+
+string input;
+
+while ((input = Console.ReadLine()) != "Travel")
+{
+    string[] elements = input
+        .Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+
+    if (command == "Add Stop")
+    {
+        int index = int.Parse(elements[1]);
+        string newStop = elements[2];
+
+        if (index >= 0 && index < destinations.Length)
+        {
+            destinations.Insert(index, newStop);
+        }
+
+        Console.WriteLine(destinations);
+    }
+    else if (command == "Remove Stop")
+    {
+        int startIndex = int.Parse(elements[1]);
+        int endIndex = int.Parse(elements[2]);
+
+        if (startIndex <= endIndex && startIndex >= 0 && endIndex < destinations.Length)
+        {
+            destinations.Remove(startIndex, endIndex - startIndex + 1);
+        }
+
+        Console.WriteLine(destinations);
+    }
+    else if (command == "Switch")
+    {
+        string oldValue = elements[1];
+        string newValue = elements[2];
+
+        destinations.Replace(oldValue, newValue);
+
+        Console.WriteLine(destinations);
+    }
+}
+
+Console.WriteLine($"Ready for world tour! Planned stops: {destinations}");
