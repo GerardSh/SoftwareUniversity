34d08e22e3c4b00a9c63a76ddd69cd47dca76113
Gerard <gerardshah@gmail.com>
2024-03-30 11:45:58 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/02 Programming Fundamentals Final Exam/02 Destination Mapper/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/02 Programming Fundamentals Final Exam/02 Destination Mapper/Program.cs
@@ -17,3 +17,19 @@ foreach (Match match in locationMatches)
 
 Console.WriteLine($"Destinations: {string.Join(", ", locations)}");
 Console.WriteLine($"Travel Points: {sumLength}");
+
+
+
+
+//2
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+MatchCollection locations = Regex.Matches(Console.ReadLine(), @"(=|\/)([A-Z][A-Za-z]{2,})\1");
+
+int travelPoints = locations.Sum(p => p.Groups[2].Length);
+
+List<string> cities = locations.Select(x => x.Groups[2].Value).ToList();
+
+Console.WriteLine($"Destinations: {string.Join(", ", cities)}");
+Console.WriteLine($"Travel Points: {travelPoints}");
