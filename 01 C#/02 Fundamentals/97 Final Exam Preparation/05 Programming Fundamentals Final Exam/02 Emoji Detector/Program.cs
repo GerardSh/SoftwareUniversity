89dd8916fffd432d0eae1ee489f20db6ba1ddcb6
Gerard <gerardshah@gmail.com>
2024-03-30 20:50:54 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/05 Programming Fundamentals Final Exam/02 Emoji Detector/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/05 Programming Fundamentals Final Exam/02 Emoji Detector/Program.cs
@@ -4,7 +4,7 @@ string input = Console.ReadLine();
 var digitMatches = Regex.Matches(input, @"[\d]");
 
 ulong coolThreshold = 1;
-Console.WriteLine();
+
 foreach (Match match in digitMatches)
 {
     coolThreshold *= ulong.Parse(match.Value);
@@ -28,5 +28,41 @@ foreach (Match match in emojiMatches)
     if (emojiSum > coolThreshold)
     {
         Console.WriteLine(match.Value);
+    }
+}
+
+
+
+
+//2
+using System.Text.RegularExpressions;
+string text = Console.ReadLine();
+
+MatchCollection digits = Regex.Matches(text, @"\d");
+
+MatchCollection emojis = Regex.Matches(text, @"(::|\*\*)([A-Z][a-z]{2,})\1");
+
+ulong coolThresholdSum = 1;
+
+foreach (Match match in digits)
+{
+    coolThresholdSum *= ulong.Parse(match.Value);
+}
+
+Console.WriteLine($"Cool threshold: {coolThresholdSum}");
+Console.WriteLine($"{emojis.Count} emojis found in the text. The cool ones are:");
+
+foreach (Match emoji in emojis)
+{
+    ulong sum = 0;
+
+    foreach (char c in emoji.Groups[2].Value)
+    {
+        sum += c;
+    }
+
+    if (sum >= coolThresholdSum)
+    {
+        Console.WriteLine(emoji.Value);
     }
 }
