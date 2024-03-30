25b4f8882b3c987e4fac6f2c57477475bdda8080
Gerard <gerardshah@gmail.com>
2024-03-30 14:47:51 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/03 Programming Fundamentals Final Exam Retake/02 Mirror Words/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/03 Programming Fundamentals Final Exam Retake/02 Mirror Words/Program.cs
@@ -35,3 +35,40 @@ else
 {
     Console.WriteLine($"The mirror words are:\n{string.Join(", ", mirrorPairs)}");
 }
+
+
+
+
+//2
+using System.Text.RegularExpressions;
+
+MatchCollection matches = Regex.Matches(Console.ReadLine(), @"(@|#)([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1");
+
+List<string> mirrorWords = new List<string>();
+
+foreach (Match match in matches)
+{
+    if (match.Groups[2].Value == string.Concat(match.Groups[3].Value.Reverse()))
+    {
+        mirrorWords.Add($"{match.Groups[2].Value} <=> {match.Groups[3].Value}");
+    }
+}
+
+if (matches.Count == 0)
+{
+    Console.WriteLine("No word pairs found!");
+}
+else
+{
+    Console.WriteLine($"{matches.Count} word pairs found!");
+}
+
+if (mirrorWords.Count > 0)
+{
+    Console.WriteLine("The mirror words are:");
+    Console.WriteLine(string.Join(", ", mirrorWords));
+}
+else
+{
+    Console.WriteLine("No mirror words!");
+}
