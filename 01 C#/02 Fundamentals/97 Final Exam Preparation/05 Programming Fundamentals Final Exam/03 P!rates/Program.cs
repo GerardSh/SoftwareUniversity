1da07db646c528124111259f01bfa410da5ca070
Gerard <gerardshah@gmail.com>
2024-03-30 21:20:45 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/05 Programming Fundamentals Final Exam/03 P!rates/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/05 Programming Fundamentals Final Exam/03 P!rates/Program.cs
@@ -75,3 +75,101 @@ else
 {
     Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
 }
+
+
+
+
+//2
+class Town
+{
+    public int Population { get; set; }
+    public int Gold { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+
+        Dictionary<string, Town> towns = new Dictionary<string, Town>();
+
+        while ((input = Console.ReadLine()) != "Sail")
+        {
+            string[] elements = input
+                .Split("||", StringSplitOptions.RemoveEmptyEntries);
+
+            string town = elements[0];
+            int population = int.Parse(elements[1]);
+            int gold = int.Parse(elements[2]);
+
+            if (!towns.ContainsKey(town))
+            {
+                towns.Add(town, new Town()
+                {
+                    Gold = 0,
+                    Population = 0
+                });
+            }
+
+            towns[town].Population += population;
+            towns[town].Gold += gold;
+        }
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] elements = input
+                .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = elements[0];
+            string town = elements[1];
+
+            Town currentTown = towns[town];
+
+            if (command == "Plunder")
+            {
+                int population = int.Parse(elements[2]);
+                int gold = int.Parse(elements[3]);
+
+                Console.WriteLine($"{town} plundered! {gold} gold stolen, {population} citizens killed.");
+
+                currentTown.Population -= population;
+                currentTown.Gold -= gold;
+
+                if (currentTown.Gold == 0 || currentTown.Population == 0)
+                {
+                    towns.Remove(town);
+                    Console.WriteLine($"{town} has been wiped off the map!");
+                }
+            }
+            else if (command == "Prosper")
+            {
+                int gold = int.Parse(elements[2]);
+
+                if (gold < 0)
+                {
+                    Console.WriteLine($"Gold added cannot be a negative number!");
+                    continue;
+                }
+
+                currentTown.Gold += gold;
+
+                Console.WriteLine($"{gold} gold added to the city treasury. {town} now has {currentTown.Gold} gold.");
+            }
+        }
+
+        if (towns.Count > 0)
+        {
+            Console.WriteLine($"Ahoy, Captain! There are {towns.Count} wealthy settlements to go to:");
+        }
+        else
+        {
+            Console.WriteLine($"Ahoy, Captain! All targets have been plundered and destroyed!");
+        }
+
+        foreach (var town in towns)
+        {
+            Console.WriteLine($"{town.Key} -> Population: {town.Value.Population} citizens, Gold: {town.Value.Gold} kg");
+        }
+    }
+}
