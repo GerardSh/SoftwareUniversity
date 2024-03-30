01c5a291c54fbf8485094cdf3a538ad3dafe87e3
Gerard <gerardshah@gmail.com>
2024-03-30 07:46:17 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/01 Programming Fundamentals Final Exam Retake/01 The Imitation Game/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/01 Programming Fundamentals Final Exam Retake/01 The Imitation Game/Program.cs
@@ -37,3 +37,48 @@ while ((input = Console.ReadLine()) != "Decode")
 }
 
 Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+
+
+
+
+//2
+using System.Text;
+
+StringBuilder message = new StringBuilder(Console.ReadLine());
+
+string input;
+
+while ((input = Console.ReadLine()) != "Decode")
+{
+    string[] elements = input
+        .Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+
+    if (command == "Move")
+    {
+        int lettersToMove = int.Parse(elements[1]);
+
+        message.Append(message.ToString().Substring(0, lettersToMove));
+        message.Remove(0, lettersToMove);
+    }
+    else if (command == "Insert")
+    {
+        int index = int.Parse(elements[1]);
+        string value = elements[2];
+
+        message.Insert(index, value);
+    }
+    else
+    {
+        string substring = elements[1];
+        string replacment = elements[2];
+
+        while (message.ToString().Contains(substring))
+        {
+            message.Replace(substring, replacment);
+        }
+    }
+}
+
+Console.WriteLine($"The decrypted message is: {message}");
