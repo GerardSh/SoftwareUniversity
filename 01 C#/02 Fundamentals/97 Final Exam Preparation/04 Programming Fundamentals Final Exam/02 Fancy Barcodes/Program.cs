1d987244bf47e4a5e70757b9fb69f9efcb3a7b26
Gerard <gerardshah@gmail.com>
2024-03-30 17:24:02 +0200

    Fundamentals final exam preparation

--- 01 C#/02 Fundamentals/97 Final Exam Preparation/04 Programming Fundamentals Final Exam/02 Fancy Barcodes/Program.cs
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/04 Programming Fundamentals Final Exam/02 Fancy Barcodes/Program.cs
@@ -33,3 +33,80 @@ for (int i = 0; i < n; i++)
         Console.WriteLine("Invalid barcode");
     }
 }
+
+
+
+
+//2
+using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string barCode = Console.ReadLine();
+
+    Match match = Regex.Match(barCode, @"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+");
+
+    if (!match.Success)
+    {
+        Console.WriteLine("Invalid barcode");
+    }
+    else
+    {
+        MatchCollection matches = Regex.Matches(barCode, @"[\d]");
+
+        string productGroup = "00";
+
+        if (matches.Count > 0)
+        {
+            productGroup = "";
+
+            foreach (Match digit in matches)
+            {
+                productGroup += digit.Value;
+            }
+        }
+
+        Console.WriteLine("Product group: " + productGroup);
+    }
+}
+
+
+
+
+//3
+using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string barCode = Console.ReadLine();
+
+    Match match = Regex.Match(barCode, @"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+");
+
+    if (!match.Success)
+    {
+        Console.WriteLine("Invalid barcode");
+    }
+    else
+    {
+        string productGroup = "";
+
+        foreach (char c in match.Value)
+        {
+            if (char.IsDigit(c))
+            {
+                productGroup += c;
+            }
+        }
+
+        if (productGroup.Length == 0)
+        {
+            productGroup = "00";
+        }
+
+        Console.WriteLine("Product group: " + productGroup);
+    }
+}
