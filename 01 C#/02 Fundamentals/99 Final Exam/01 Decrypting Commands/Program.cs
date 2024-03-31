be06dd93968e59bca0310b690e5dc4202d504fa5
Gerard <gerardshah@gmail.com>
2024-03-31 11:30:02 +0300

    Fundamentals final exam

---
+++ 01 C#/02 Fundamentals/99 Final Exam/01 Decrypting Commands/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+
+StringBuilder message = new StringBuilder(Console.ReadLine());
+
+string input;
+
+while ((input = Console.ReadLine()) != "Finish")
+{
+    string[] elements = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+
+    if (command == "Replace")
+    {
+        string currentChar = elements[1];
+        string newChar = elements[2];
+
+        message.Replace(currentChar, newChar);
+
+        Console.WriteLine(message);
+    }
+    else if (command == "Cut")
+    {
+        int startIndex = int.Parse(elements[1]);
+        int endIndex = int.Parse(elements[2]);
+        int count = endIndex - startIndex + 1;
+
+        if (startIndex < 0 || endIndex < startIndex || endIndex >= message.Length)
+        {
+            Console.WriteLine("Invalid indices!");
+            continue;
+        }
+
+        message.Remove(startIndex, count);
+
+        Console.WriteLine(message);
+    }
+    else if (command == "Make")
+    {
+        string casing = elements[1];
+
+        string temp = message.ToString().ToUpper();
+
+
+        if (casing == "Lower")
+        {
+            temp = temp.ToLower();
+        }
+
+        message.Clear();
+        message.Append(temp);
+
+        Console.WriteLine(message);
+    }
+    else if (command == "Check")
+    {
+        string text = elements[1];
+
+        if (message.ToString().Contains(text))
+        {
+            Console.WriteLine($"Message contains {text}");
+        }
+        else
+        {
+            Console.WriteLine($"Message doesn't contain {text}");
+        }
+    }
+    else if (command == "Sum")
+    {
+        int startIndex = int.Parse(elements[1]);
+        int endIndex = int.Parse(elements[2]);
+        int count = endIndex - startIndex + 1;
+
+        if (startIndex < 0 || endIndex < startIndex || endIndex >= message.Length)
+        {
+            Console.WriteLine("Invalid indices!");
+            continue;
+        }
+
+        string subString = message.ToString().Substring(startIndex, count);
+
+        int sum = subString.Sum(x => x);
+
+        Console.WriteLine(sum);
+    }
+}
