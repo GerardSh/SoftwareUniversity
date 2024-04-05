f183f1535375a47e4f6bbe32b954516d97190dca
Gerard <gerardshah@gmail.com>
2024-04-05 23:23:07 +0300

    Fundamentals final exam preparation

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/06 Exam Preparation - 20 March 2024/01 Registration/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+StringBuilder username = new StringBuilder(Console.ReadLine());
+
+string input;
+
+while ((input = Console.ReadLine()) != "Registration")
+{
+    string[] elements = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+
+    if (command == "Letters")
+    {
+        string casing = elements[1];
+
+        username = new StringBuilder(username.ToString().ToUpper());
+
+        if (casing == "Lower")
+        {
+            username = new StringBuilder(username.ToString().ToLower());
+        }
+
+        Console.WriteLine(username);
+    }
+    else if (command == "Reverse")
+    {
+        int startIndex = int.Parse(elements[1]);
+        int endIndex = int.Parse(elements[2]);
+        int count = endIndex - startIndex + 1;
+
+        if (startIndex >= 0 && endIndex >= startIndex && endIndex < username.Length)
+        {
+            string substring = username.ToString().Substring(startIndex, count);
+
+            Console.WriteLine(string.Concat(substring.Reverse()));
+        }
+    }
+    else if (command == "Substring")
+    {
+        string substring = elements[1];
+
+        if (username.ToString().Contains(substring))
+        {
+            username.Replace(substring, string.Empty);
+
+            Console.WriteLine(username);
+        }
+        else
+        {
+            Console.WriteLine($"The username {username} doesn't contain {substring}.");
+        }
+    }
+    else if (command == "Replace")
+    {
+        char symbol = char.Parse(elements[1]);
+
+        if (username.ToString().Contains(symbol))
+        {
+            username.Replace(symbol, '-');
+        }
+
+        Console.WriteLine(username);
+    }
+    else if (command == "IsValid")
+    {
+        char symbol = char.Parse(elements[1]);
+
+        if (username.ToString().Contains(symbol))
+        {
+            Console.WriteLine("Valid username.");
+        }
+        else
+        {
+            Console.WriteLine($"{symbol} must be contained in your username.");
+        }
+    }
+}
---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/06 Exam Preparation - 20 March 2024/02 Destination Mapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+MatchCollection locationMatches = Regex.Matches(input, @"([=/])([A-Z][A-Za-z]{2,})\1");
+
+int sumLength = locationMatches
+    .Select(x => x.Groups[2].Length)
+    .Sum();
+
+List<string> locations = new List<string>();
+
+foreach (Match match in locationMatches)
+{
+    locations.Add(match.Groups[2].Value);
+}
+
+Console.WriteLine($"Destinations: {string.Join(", ", locations)}");
+Console.WriteLine($"Travel Points: {sumLength}");
+
+
+
+
+//2
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+MatchCollection locations = Regex.Matches(Console.ReadLine(), @"(=|\/)([A-Z][A-Za-z]{2,})\1");
+
+int travelPoints = locations.Sum(p => p.Groups[2].Length);
+
+List<string> cities = locations.Select(x => x.Groups[2].Value).ToList();
+
+Console.WriteLine($"Destinations: {string.Join(", ", cities)}");
+Console.WriteLine($"Travel Points: {travelPoints}");
