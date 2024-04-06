696362f86344c37b0c90278f6f222c1d5177c10c
Gerard <gerardshah@gmail.com>
2024-04-06 10:06:43 +0300

    Fundamentals final exam preparation

---
+++ 01 C#/02 Fundamentals/97 Final Exam Preparation/06 Exam Preparation - 20 March 2024/03 Bakery Shop/Program.cs
@@ -0,0 +1,61 @@
+﻿Dictionary<string, int> foods = new Dictionary<string, int>();
+int soldFoods = 0;
+
+string input;
+
+while ((input = Console.ReadLine()) != "Complete")
+{
+    string[] elements = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+    int quantity = int.Parse(elements[1]);
+    string food = elements[2];
+
+    if (command == "Receive")
+    {
+        if (quantity <= 0)
+        {
+            continue;
+        }
+
+        if (!foods.ContainsKey(food))
+        {
+            foods.Add(food, 0);
+        }
+
+        foods[food] += quantity;
+    }
+    else if (command == "Sell")
+    {
+        if (!foods.ContainsKey(food))
+        {
+            Console.WriteLine($"You do not have any {food}.");
+            continue;
+        }
+        else if (foods[food] < quantity)
+        {
+            Console.WriteLine($"There aren't enough {food}. You sold the last {foods[food]} of them.");
+            soldFoods += foods[food];
+            foods[food] = 0;
+        }
+        else
+        {
+            foods[food] -= quantity;
+            soldFoods += quantity;
+            Console.WriteLine($"You sold {quantity} {food}.");
+        }
+
+        if (foods[food] == 0)
+        {
+            foods.Remove(food);
+        }
+    }
+}
+
+foreach (var food in foods)
+{
+    Console.WriteLine($"{food.Key}: {food.Value}");
+}
+
+Console.WriteLine($"All sold: {soldFoods} goods");
