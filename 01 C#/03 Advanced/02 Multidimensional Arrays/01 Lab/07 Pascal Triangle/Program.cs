6f28ef1c80d674abb1d5b5df64691361fe219580
Gerard <gerardshah@gmail.com>
2024-04-07 13:50:25 +0300

    Advanced Multidimensional Arrays Lab exercise added

---
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/01 Lab/07 Pascal Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+ulong[][] jaggedArray = new ulong[n][];
+
+for (int row = 0; row < jaggedArray.Length; row++)
+{
+    jaggedArray[row] = new ulong[row + 1];
+
+    for (int col = 0; col < jaggedArray[row].Length; col++)
+    {
+        if (col == 0 || col == jaggedArray[row].Length - 1)
+        {
+            jaggedArray[row][col] = 1;
+        }
+        else
+        {
+            jaggedArray[row][col] = jaggedArray[row - 1][col] + jaggedArray[row - 1][col - 1];
+        }
+    }
+}
+
+for (int row = 0; row < jaggedArray.Length; row++)
+{
+    for (int col = 0; col < jaggedArray[row].Length; col++)
+    {
+        Console.Write(jaggedArray[row][col] + " ");
+    }
+
+    Console.WriteLine();
+}
