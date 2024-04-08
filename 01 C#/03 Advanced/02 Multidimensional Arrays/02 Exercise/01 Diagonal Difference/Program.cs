37fe4ad17eef60e70bf0d7f1945e87f601400b18
Gerard <gerardshah@gmail.com>
2024-04-08 20:37:24 +0300

    Advanced Multidimensional Arrays Exercise added

---
+++ 01 C#/03 Advanced/02 Multidimensional Arrays/02 Exercise/01 Diagonal Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+int count = 0;
+int primaryDiagonal = 0;
+int secondaryDiagonal = 0;
+
+for (int row = 0; row < n; row++)
+{
+    int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    primaryDiagonal += array[count];
+    secondaryDiagonal += array[array.Length - 1 - count];
+
+    //int[] arrayReversed = array;
+    //Array.Reverse(arrayReversed);
+    //secondaryDiagonal += arrayReversed[count];
+
+    count++;
+
+    for (int col = 0; col < n; col++)
+    {
+        matrix[row, col] = array[col];
+    }
+}
+
+Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
