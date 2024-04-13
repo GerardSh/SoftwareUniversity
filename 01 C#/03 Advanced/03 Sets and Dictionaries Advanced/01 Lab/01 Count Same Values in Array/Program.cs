c4e05311fe2544848f88fb080cce442e89ba073f
Gerard <gerardshah@gmail.com>
2024-04-13 20:07:10 +0300

    Advanced - Sets and Dictionaries Advanced Lab exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/01 Lab/01 Count Same Values in Array/Program.cs
@@ -0,0 +1,21 @@
+﻿double[] numbers = Console.ReadLine()
+    .Split()
+    .Select(double.Parse)
+    .ToArray();
+
+var countByNumbers = new Dictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (!countByNumbers.ContainsKey(number))
+    {
+        countByNumbers.Add(number, 0);
+    }
+
+    countByNumbers[number]++;
+}
+
+foreach (var kvp in countByNumbers)
+{
+    Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+}
