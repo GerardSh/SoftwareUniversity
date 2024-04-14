1c3b59e09082b461b2c9690414b5dc70d83aa351
Gerard <gerardshah@gmail.com>
2024-04-14 23:35:36 +0300

    Advanced - Sets and Dictionaries Advanced Lab exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/01 Lab/02 Average Student Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var students = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] student = Console.ReadLine().Split();
+
+    string name = student[0];
+    decimal grade = decimal.Parse(student[1]);
+
+    if (!students.ContainsKey(name))
+    {
+        students.Add(name, new List<decimal>());
+    }
+
+    students[name].Add(grade);
+}
+
+foreach (var student in students)
+{
+    Console.Write($"{student.Key} -> ");
+
+    foreach (var grade in student.Value)
+    {
+        Console.Write($"{grade:f2} ");
+    }
+
+    Console.WriteLine($"(avg: {student.Value.Average():f2})");
+}
