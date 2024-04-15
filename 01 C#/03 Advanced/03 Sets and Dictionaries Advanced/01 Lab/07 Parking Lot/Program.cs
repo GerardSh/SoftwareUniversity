e0f8e12bebc4f7bbc20f73ba7a14dc835113b722
Gerard <gerardshah@gmail.com>
2024-04-15 22:33:46 +0300

    Advanced - Sets and Dictionaries Advanced Lab exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/01 Lab/07 Parking Lot/Program.cs
@@ -0,0 +1,30 @@
+﻿HashSet<string> parkingLot = new HashSet<string>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] elements = input
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+    string carNumber = elements[1];
+
+    if (command == "IN")
+    {
+        parkingLot.Add(carNumber);
+    }
+    else
+    {
+        parkingLot.Remove(carNumber);
+    }
+}
+
+if (parkingLot.Count > 0)
+{
+    Console.WriteLine(string.Join("\n", parkingLot));
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
