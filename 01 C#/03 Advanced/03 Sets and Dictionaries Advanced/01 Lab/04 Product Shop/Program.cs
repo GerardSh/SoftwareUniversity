c9fae7603cdfaced0765b78878bdf2519cf8481a
Gerard <gerardshah@gmail.com>
2024-04-15 17:31:25 +0300

    Advanced - Sets and Dictionaries Advanced Lab exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/01 Lab/04 Product Shop/Program.cs
@@ -0,0 +1,35 @@
+﻿string input;
+
+var shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+while ((input = Console.ReadLine()) != "Revision")
+{
+    string[] elements = input
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    string shop = elements[0];
+    string product = elements[1];
+    double price = double.Parse(elements[2]);
+
+    if (!shops.ContainsKey(shop))
+    {
+        shops.Add(shop, new Dictionary<string, double>());
+    }
+
+    if (!shops[shop].ContainsKey(product))
+    {
+        shops[shop][product] = 0;
+    }
+
+    shops[shop][product] = price;
+}
+
+foreach (var shop in shops)
+{
+    Console.WriteLine($"{shop.Key}->");
+
+    foreach (var product in shop.Value)
+    {
+        Console.WriteLine($"Product: {product.Key}, Price: {product.Value} ");
+    }
+}
