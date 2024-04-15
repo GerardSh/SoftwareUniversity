96bc7c1f7268553ac9d8ee3cc0781614c4d5ab9c
Gerard <gerardshah@gmail.com>
2024-04-15 22:58:43 +0300

    Advanced - Sets and Dictionaries Advanced Lab exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/01 Lab/08 SoftUni Party/Program.cs
@@ -0,0 +1,49 @@
+﻿var guestListVIP = new HashSet<string>();
+var guestList = new HashSet<string>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "PARTY")
+{
+    if (input.Length != 8)
+    {
+        continue;
+    }
+
+    string guestNumber = input;
+
+    if (char.IsDigit(input[0]))
+    {
+        guestListVIP.Add(guestNumber);
+    }
+    else
+    {
+        guestList.Add(guestNumber);
+    }
+}
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string guestNumber = input;
+
+    if (char.IsDigit(input[0]))
+    {
+        guestListVIP.Remove(guestNumber);
+    }
+    else
+    {
+        guestList.Remove(guestNumber);
+    }
+}
+
+Console.WriteLine($"{guestList.Count + guestListVIP.Count}");
+
+if (guestListVIP.Count > 0)
+{
+    Console.WriteLine(string.Join("\n", guestListVIP));
+}
+
+if (guestList.Count > 0)
+{
+    Console.WriteLine(string.Join("\n", guestList));
+}
