79e7a8c1d91e8261ef6a03ed3be63c6333a510d2
Gerard <gerardshah@gmail.com>
2024-04-15 17:53:31 +0300

    Advanced - Sets and Dictionaries Advanced Lab exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/01 Lab/05 Cities by Continent and Country/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var citiesAndCountriesByContinents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] elements = Console.ReadLine().Split();
+
+    string continent = elements[0] + ":";
+    string country = elements[1] + " ->";
+    string city = elements[2];
+
+    if (!citiesAndCountriesByContinents.ContainsKey(continent))
+    {
+        citiesAndCountriesByContinents[continent] = new Dictionary<string, List<string>>();
+    }
+
+    if (!citiesAndCountriesByContinents[continent].ContainsKey(country))
+    {
+        citiesAndCountriesByContinents[continent][country] = new List<string>();
+    }
+
+    citiesAndCountriesByContinents[continent][country].Add(city);
+}
+
+foreach (var continent in citiesAndCountriesByContinents)
+{
+    Console.WriteLine(continent.Key);
+
+    foreach (var country in continent.Value)
+    {
+        Console.WriteLine($"  {country.Key} {string.Join(", ", country.Value)}");
+    }
+}
