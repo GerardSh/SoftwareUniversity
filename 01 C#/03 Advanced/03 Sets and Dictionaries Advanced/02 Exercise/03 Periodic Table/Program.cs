6838e0bac28df14f8a140bf0b91838bac4d974c7
Gerard <gerardshah@gmail.com>
2024-04-16 12:06:28 +0300

    Advanced - Sets and Dictionaries Advanced Exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/03 Periodic Table/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var periodicTable = new SortedSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] elements = Console.ReadLine().Split();
+
+    for (int j = 0; j < elements.Length; j++)
+    {
+        periodicTable.Add(elements[j]);
+    }
+}
+
+Console.WriteLine(string.Join(" ", periodicTable));
