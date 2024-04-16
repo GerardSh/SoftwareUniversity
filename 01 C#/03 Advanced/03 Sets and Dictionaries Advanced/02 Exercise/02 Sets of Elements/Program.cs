65b467552d280d0f70c67086161c61863163f855
Gerard <gerardshah@gmail.com>
2024-04-16 11:20:20 +0300

    Advanced - Sets and Dictionaries Advanced Exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/02 Sets of Elements/Program.cs
@@ -0,0 +1,30 @@
+﻿var elements = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var nNumbers = new HashSet<int>(elements[0]);
+var mNumbers = new HashSet<int>(elements[1]);
+
+for (int i = 0; i < elements[0]; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    nNumbers.Add(number);
+}
+
+for (int i = 0; i < elements[1]; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    mNumbers.Add(number);
+}
+
+foreach (var nNumber in nNumbers)
+{
+    foreach (var mNumber in mNumbers)
+    {
+        if (nNumber == mNumber)
+        {
+            Console.Write(nNumber + " ");
+        }
+    }
+}
