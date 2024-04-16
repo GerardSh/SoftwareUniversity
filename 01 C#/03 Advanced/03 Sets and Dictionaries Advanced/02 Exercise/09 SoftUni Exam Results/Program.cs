e16351dc2ee39e5734a9f9ea58bc2c15261b01ff
Gerard <gerardshah@gmail.com>
2024-04-17 00:00:40 +0300

    Update Program.cs

--- 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/09 SoftUni Exam Results/Program.cs
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/09 SoftUni Exam Results/Program.cs
@@ -1,4 +1,79 @@
-﻿string input;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Test
+    {
+        static void Main()
+        {
+            var countByLanguages = new Dictionary<string, int>();
+            var scoresByStudents = new Dictionary<string, int>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "exam finished")
+            {
+                string[] commands = input
+                    .Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = commands[0];
+
+                if (commands.Length > 2)
+                {
+                    string language = commands[1];
+                    int scores = int.Parse(commands[2]);
+
+                    if (!scoresByStudents.ContainsKey(name))
+                    {
+                        scoresByStudents.Add(name, 0);
+                    }
+
+                    if (scoresByStudents[name] < scores)
+                    {
+                        scoresByStudents[name] = scores;
+                    }
+
+                    if (!countByLanguages.ContainsKey(language))
+                    {
+                        countByLanguages[language] = 0;
+                    }
+
+                    countByLanguages[language]++;
+                }
+                else
+                {
+                    scoresByStudents.Remove(name);
+                }
+            }
+
+            var sortedStudents = scoresByStudents.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+            Console.WriteLine("Results:");
+
+            foreach (var kvp in sortedStudents)
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value}");
+            }
+
+            var sortedLanguages = countByLanguages.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+            Console.WriteLine("Submissions:");
+
+            foreach (var kvp in sortedLanguages)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+    }
+}
+
+
+
+
+//2
+string input;
 
 var languagesByStudent = new Dictionary<string, Dictionary<string, int>>();
 var languages = new Dictionary<string, int>();
