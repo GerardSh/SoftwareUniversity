48834cab04b06c83cafc686dd55a0403454d3c34
Gerard <gerardshah@gmail.com>
2024-04-16 11:18:53 +0300

    Advanced - Sets and Dictionaries Advanced Exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/01 Unique Usernames/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var usernames = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+    usernames.Add(name);
+}
+
+Console.WriteLine(string.Join("\n", usernames));
