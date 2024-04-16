c6fda892f9abb2e789d158bd10174861520ef95a
Gerard <gerardshah@gmail.com>
2024-04-16 17:43:24 +0300

    Advanced - Sets and Dictionaries Advanced Exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/08 Ranking/Program.cs
@@ -0,0 +1,67 @@
+﻿string input;
+
+var passwordsByContest = new Dictionary<string, string>();
+
+while ((input = Console.ReadLine()) != "end of contests")
+{
+    string[] elements = input
+        .Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+    string contest = elements[0];
+    string password = elements[1];
+
+    if (!passwordsByContest.ContainsKey(contest))
+    {
+        passwordsByContest[contest] = password;
+    }
+}
+
+var contestResultsByUsername = new Dictionary<string, Dictionary<string, int>>();
+
+while ((input = Console.ReadLine()) != "end of submissions")
+{
+    string[] elements = input
+        .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+    string contest = elements[0];
+    string password = elements[1];
+    string username = elements[2];
+    int points = int.Parse(elements[3]);
+
+    if (!passwordsByContest.ContainsKey(contest) || passwordsByContest[contest] != password)
+    {
+        continue;
+    }
+
+    if (!contestResultsByUsername.ContainsKey(username))
+    {
+        contestResultsByUsername[username] = new Dictionary<string, int>();
+    }
+
+    if (!contestResultsByUsername[username].ContainsKey(contest))
+    {
+        contestResultsByUsername[username][contest] = 0;
+    }
+
+    if (contestResultsByUsername[username][contest] < points)
+    {
+        contestResultsByUsername[username][contest] = points;
+    }
+}
+
+var bestUser = contestResultsByUsername.OrderByDescending(x => x.Value.Values.Sum()).First();
+
+Console.WriteLine($"Best candidate is {bestUser.Key} with total {bestUser.Value.Values.Sum()} points.");
+Console.WriteLine("Ranking:");
+
+var sortedUsers = contestResultsByUsername.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+foreach (var user in sortedUsers)
+{
+    Console.WriteLine(user.Key);
+
+    foreach (var contest in user.Value.OrderByDescending(x => x.Value))
+    {
+        Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+    }
+}
