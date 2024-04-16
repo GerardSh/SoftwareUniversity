0df7080202c0812fb0c230626f81083e5c80c2b1
Gerard <gerardshah@gmail.com>
2024-04-16 12:49:23 +0300

    Advanced - Sets and Dictionaries Advanced Exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/05 Count Symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+
+var charsCount = new SortedDictionary<char, int>();
+
+foreach (char c in input)
+{
+    if (!charsCount.ContainsKey(c))
+    {
+        charsCount[c] = 0;
+    }
+
+    charsCount[c]++;
+}
+
+foreach (var kvp in charsCount)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+}
