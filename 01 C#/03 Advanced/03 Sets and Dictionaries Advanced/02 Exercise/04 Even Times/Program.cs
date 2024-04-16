36cf0a0dff6d2708cf7b4c28623fbdebcd09d9df
Gerard <gerardshah@gmail.com>
2024-04-16 12:15:29 +0300

    Advanced - Sets and Dictionaries Advanced Exercise added

---
+++ 01 C#/03 Advanced/03 Sets and Dictionaries Advanced/02 Exercise/04 Even Times/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var evenNumbers = new Dictionary<double, int>();
+
+for (int i = 0; i < n; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+
+    if (!evenNumbers.ContainsKey(number))
+    {
+        evenNumbers[number] = 0;
+    }
+
+    evenNumbers[number]++;
+}
+
+foreach (var number in evenNumbers)
+{
+    if (number.Value % 2 == 0)
+    {
+        Console.WriteLine(number.Key + " ");
+    }
+}
