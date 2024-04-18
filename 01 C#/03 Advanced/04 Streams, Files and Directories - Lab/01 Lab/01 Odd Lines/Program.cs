3310b99f9b11f7b2c95156a04d53e0ed5f771a6c
Gerard <gerardshah@gmail.com>
2024-04-18 19:20:16 +0300

    Advanced - Streams, Files and Directories Lab exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/01 Odd Lines/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"temp\01. Odd Lines\Input.txt";
+            string outputFilePath = @"temp\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader sr = new StreamReader(inputFilePath);
+            using StreamWriter sw = new StreamWriter(outputFilePath);
+
+            int rowCounter = 0;
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                if (rowCounter++ % 2 == 1)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+        }
+    }
+}
