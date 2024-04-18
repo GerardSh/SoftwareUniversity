bf05410454114d80217eb903630f25f4947211d1
Gerard <gerardshah@gmail.com>
2024-04-18 20:52:47 +0300

    Advanced - Streams, Files and Directories Lab exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/02 Line Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"temp\LineNumbers\Input.txt";
+            string outputPath = @"temp\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using var sr = new StreamReader(inputFilePath);
+            using var sw = new StreamWriter(outputFilePath);
+
+            int rowCount = 1;
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                sw.WriteLine($" {rowCount++}.{line}");
+            }
+        }
+    }
+}
