43659043301a3f0f5d73fb9127c1d922c4f54f0b
Gerard <gerardshah@gmail.com>
2024-04-19 21:12:11 +0300

    Update Program.cs

--- 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/04 Merge Text Files/Program.cs
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/04 Merge Text Files/Program.cs
@@ -64,6 +64,8 @@
                 }
             }
 
+            return;
+
             //Option 2
             int lineCount = 0;
 
@@ -81,51 +83,6 @@
 
                 lineCount++;
             }
-        }
-    }
-}
-
-
-
-
-//2
-namespace MergeFiles
-{
-    using System;
-    using System.IO;
-    public class MergeFiles
-    {
-        static void Main(string[] args)
-        {
-            var firstInputFilePath = @"temp\input1.txt";
-            var secondInputFilePath = @"temp\input2.txt";
-            var outputFilePath = @"temp\output.txt";
-
-            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
-        }
-
-        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
-        {
-            using var readerOne = new StreamReader(firstInputFilePath);
-            using var readerTwo = new StreamReader(secondInputFilePath);
-            using var sw = new StreamWriter(outputFilePath);
-
-            List<string> inputOne = new List<string>();
-            List<string> inputTwo = new List<string>();
-
-            while (!readerOne.EndOfStream)
-            {
-                string line = readerOne.ReadLine();
-                inputOne.Add(line);
-            }
-
-            while (!readerTwo.EndOfStream)
-            {
-                string line = readerTwo.ReadLine();
-                inputTwo.Add(line);
-            }
-
-           
         }
     }
 }
