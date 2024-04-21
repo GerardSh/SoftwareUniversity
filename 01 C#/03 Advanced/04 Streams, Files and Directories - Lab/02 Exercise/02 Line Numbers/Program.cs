e62a119c037ea96f1f8c8531fc57c230a67e2768
Gerard <gerardshah@gmail.com>
2024-04-21 22:23:30 +0300

    Advanced - Streams, Files and Directories Exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/02 Line Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"temp\text.txt";
+            string outputPath = @"temp\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var textLines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < textLines.Length; i++)
+            {
+                int punctuationMarks = Regex.Matches(textLines[i], @"[.,!?\-']").Count();
+                int letters = Regex.Matches(textLines[i], @"[a-zA-Z]").Count();
+
+                textLines[i] = $"Line {i + 1} {textLines[i]}({letters})({punctuationMarks})";
+            }
+
+            File.WriteAllLines(outputFilePath, textLines);
+        }
+    }
+}
