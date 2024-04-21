2e7123ada0db3286badb0c04ade75023da8e53d2
Gerard <gerardshah@gmail.com>
2024-04-21 16:17:06 +0300

    Advanced - Streams, Files and Directories Lab exercise added

--- 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/07 Folder Size/Program.cs
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/07 Folder Size/Program.cs
@@ -32,7 +32,7 @@
             using var writer = new StreamWriter(outputFilePath);
 
             writer.Write(sizeInKiloBytes + " KB");
-        
+
             static int GetFoldersSize(string[] files)
             {
                 int sizeInBytes = 0;
@@ -41,6 +41,60 @@
                 {
                     using var reader = new FileStream(file, FileMode.Open);
                     sizeInBytes += (int)reader.Length;
+                }
+
+                return sizeInBytes;
+            }
+        }
+    }
+}
+
+
+
+
+//2 With recursion for all levels deep subfolders
+namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"temp\TestFolder";
+            string outputPath = @"temp\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            long sizeInBytes = 0;
+
+            sizeInBytes += GetFoldersSize(@"temp\TestFolder");
+
+            double sizeInKiloBytes = (sizeInBytes / 1024.0);
+
+            using var writer = new StreamWriter(outputFilePath);
+
+            writer.Write(sizeInKiloBytes + " KB");
+
+            static long GetFoldersSize(string directory)
+            {
+                var filesInDirectory = Directory.GetFiles(directory);
+
+                long sizeInBytes = 0;
+
+                foreach (var file in filesInDirectory)
+                {
+                    sizeInBytes += new FileInfo(file).Length;
+                }
+
+                var subDirectories = Directory.GetDirectories(directory);
+
+                foreach (var subDirectory in subDirectories)
+                {
+                    sizeInBytes += GetFoldersSize(subDirectory);
                 }
 
                 return sizeInBytes;
