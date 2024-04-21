287c7656a7ee980338b1fb15deb8abba2dfa9dff
Gerard <gerardshah@gmail.com>
2024-04-21 12:01:35 +0300

    Advanced - Streams, Files and Directories Lab exercise added

--- 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/06 Split, Merge Binary Files/Program.cs
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/01 Lab/06 Split, Merge Binary Files/Program.cs
@@ -14,6 +14,72 @@
             string partTwoPath = @"temp\part-2.bin";
 
             SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using var fileReader = new FileStream(sourceFilePath, FileMode.Open);
+
+            int fileParts = 2;
+
+            long partSize = (long)Math.Ceiling(fileReader.Length / (double)fileParts);
+
+            for (int i = 1; i <= fileParts; i++)
+            {
+                byte[] buffer = new byte[partSize];
+
+                if (fileReader.Length % 2 == 1 && i == fileParts)
+                {
+                    buffer = new byte[partSize - fileParts + 1];
+                }
+
+                fileReader.Read(buffer);
+
+                using var writer = new FileStream($@"temp\part-{i}.bin", FileMode.Create);
+
+                writer.Write(buffer);
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            var readers = new FileStream[] { new FileStream(partOneFilePath, FileMode.Open), new FileStream(partTwoFilePath, FileMode.Open) };
+
+            for (int i = 0; i < 2; i++)
+            {
+                using var writer = new FileStream(joinedFilePath, FileMode.Append);
+
+                byte[] buffer = new byte[readers[i].Length];
+
+                readers[i].Read(buffer);
+
+                writer.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
+
+
+
+
+//2
+namespace SplitMergeBinaryFile
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class SplitMergeBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = @"temp\example.png";
+            string joinedFilePath = @"temp\example-joined.png";
+            string partOnePath = @"temp\part-1.bin";
+            string partTwoPath = @"temp\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
             MergeBinaryFiles(sourceFilePath, partOnePath, partTwoPath, joinedFilePath);
         }
 
@@ -42,7 +108,7 @@
             }
         }
 
-        public static void MergeBinaryFiles(string sourceFilePath, string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
         {
             using var fileReaderOne = new FileStream(partOneFilePath, FileMode.Open);
             using var fileReaderTwo = new FileStream(partTwoFilePath, FileMode.Open);
