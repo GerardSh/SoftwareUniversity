726b17439d90f7b8743fd4ff7cc98404973e5917
Gerard <gerardshah@gmail.com>
2024-04-21 21:23:19 +0300

    Advanced - Streams, Files and Directories Exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/01 Even Lines/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace EvenLines
+{
+    using System;
+    public class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"temp\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using var reader = new StreamReader(inputFilePath);
+
+            int lineRow = 0;
+
+            string allText = null;
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                if (lineRow++ % 2 == 1)
+                {
+                    continue;
+                }
+
+                line = ReplaceSymbols(line);
+
+                allText += ReverseWords(line) + "\n";
+            }
+
+            return allText;
+        }
+        private static string ReverseWords(string replacedSymbols)
+        {
+            string[] reversedText = replacedSymbols.Split(' ');
+
+            Array.Reverse(reversedText);
+
+            return string.Join(" ", reversedText);
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            line = line.Replace('-', '@');
+            line = line.Replace(',', '@');
+            line = line.Replace('.', '@');
+            line = line.Replace('!', '@');
+            line = line.Replace('?', '@');
+
+            return line;
+        }
+    }
+}
