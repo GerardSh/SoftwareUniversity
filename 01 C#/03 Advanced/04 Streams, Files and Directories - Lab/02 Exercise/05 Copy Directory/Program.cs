936c236ebf9b1c257cf07340b662b3e63da3f67f
Gerard <gerardshah@gmail.com>
2024-04-22 19:26:28 +0300

    Advanced - Streams, Files and Directories Exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/05 Copy Directory/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class CopyDirectory
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = Console.ReadLine();
+            string outputPath = Console.ReadLine();
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            var files = Directory.GetFiles(inputPath);
+
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            foreach (var file in files)
+            {
+                using var fileReader = new FileStream(file, FileMode.Open);
+
+                byte[] buffer = new byte[fileReader.Length];
+
+                //Option1
+                var newFilePath = file.Replace(inputPath, outputPath);
+
+                //Option2
+                //var fileToCopy = new FileInfo(file);
+                //var newFilePath = outputPath + "\\" + fileToCopy.Name;
+
+                //Option3
+                //var newFilePath = Regex.Replace(file,inputPath.Replace("\\","\\\\"), outputPath);
+
+                using var fileWriter = new FileStream(newFilePath, FileMode.Create);
+                fileWriter.Write(buffer);
+            }
+        }
+    }
+}
