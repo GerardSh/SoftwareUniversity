c39fbc0e1204cfa2652b9413dd6e7780ec8095a1
Gerard <gerardshah@gmail.com>
2024-04-22 17:07:33 +0300

    Advanced - Streams, Files and Directories Exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/03 Copy Binary File/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"temp\copyMe.png";
+            string outputPath = @"temp\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using var fileReader = new FileStream(inputFilePath, FileMode.Open);
+
+            byte[] buffer = new byte[fileReader.Length];
+
+            fileReader.Read(buffer);
+
+            using var fileWriter = new FileStream(outputFilePath, FileMode.Create);
+
+            fileWriter.Write(buffer);
+        }
+    }
+}
