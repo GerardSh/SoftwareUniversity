d48dabfafc909d488f2ecd4b7ed6a06e7a055360
Gerard <gerardshah@gmail.com>
2024-04-22 23:01:30 +0300

    Advanced - Streams, Files and Directories Exercise added

---
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/06 Zip and Extracts/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = @"temp\copyMe.png";
+            string zipArchiveFile = @"temp\archive.zip";
+            string extractedFile = @"temp\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            var tempDirectory = Directory.CreateDirectory("temp\\" + Path.GetFileNameWithoutExtension(zipArchiveFilePath));
+
+            File.Copy(inputFilePath, Path.Combine(tempDirectory.FullName, $"file{Path.GetExtension(inputFilePath)}"), true);
+
+            ZipFile.CreateFromDirectory(tempDirectory.FullName, tempDirectory.FullName + ".zip");
+
+            Directory.Delete(tempDirectory.FullName, true);
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            ZipFile.ExtractToDirectory(zipArchiveFilePath, Path.GetDirectoryName(outputFilePath));
+
+            var extension = Path.GetExtension(outputFilePath);
+
+            File.Move(Path.GetDirectoryName(outputFilePath) + $"\\file{extension}", outputFilePath);
+        }
+    }
+}
