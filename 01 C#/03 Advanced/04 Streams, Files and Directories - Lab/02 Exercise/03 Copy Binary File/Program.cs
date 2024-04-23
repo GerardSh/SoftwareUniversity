c4e681d40404f5c056feb147a6c8abf84fb77883
Gerard <gerardshah@gmail.com>
2024-04-23 14:23:52 +0300

    Update Program.cs

--- 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/03 Copy Binary File/Program.cs
+++ 01 C#/03 Advanced/04 Streams, Files and Directories - Lab/02 Exercise/03 Copy Binary File/Program.cs
@@ -26,3 +26,45 @@ namespace CopyBinaryFile
         }
     }
 }
+
+
+
+
+//2
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"temp\copyMe.png";
+            string outputPath = @"temp\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using var fileReader = new FileStream(inputFilePath, FileMode.Open);
+            using var fileWriter = new FileStream(outputFilePath, FileMode.Create);
+
+            byte[] buffer = new byte[4096];
+
+            int currentBytesRead = 0;
+
+            while ((currentBytesRead = fileReader.Read(buffer)) != 0)
+            {
+                if (currentBytesRead < buffer.Length)
+                {
+                    fileWriter.Write(buffer, 0, currentBytesRead);
+                }
+                else
+                {
+                    fileWriter.Write(buffer);
+                }
+            }
+        }
+    }
+}
