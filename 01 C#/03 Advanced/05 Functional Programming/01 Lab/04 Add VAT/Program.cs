8526c0c928f8453b1877c3cb6e3e78933692faa3
Gerard <gerardshah@gmail.com>
2024-04-25 19:16:35 +0300

    Advanced - Functional Programming Lab exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/01 Lab/04 Add VAT/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] prices = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => $"{double.Parse(x) * 1.20:f2}")
+    .ToArray();
+
+Console.WriteLine(string.Join("\n", prices));
