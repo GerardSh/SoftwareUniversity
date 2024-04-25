a801b3d3c18f93ca12be74f91f15311ceec1e73a
Gerard <gerardshah@gmail.com>
2024-04-25 20:22:33 +0300

    Advanced - Functional Programming Lab exercise added

--- 01 C#/03 Advanced/05 Functional Programming/01 Lab/05 Filter By Age/Program.cs
+++ 01 C#/03 Advanced/05 Functional Programming/01 Lab/05 Filter By Age/Program.cs
@@ -16,7 +16,6 @@ string commandFilter = Console.ReadLine();
 int ageFilter = int.Parse(Console.ReadLine());
 string[] filter = Console.ReadLine().Split();
 
-
 if (commandFilter == "older")
 {
     students = students.Where(x => x.Value >= ageFilter).ToDictionary(x => x.Key, x => x.Value);
@@ -45,5 +44,75 @@ else
     foreach (var kvp in students)
     {
         Console.WriteLine($"{kvp.Value}");
+    }
+}
+
+
+
+
+//2
+class Student
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<Student> students = new List<Student>(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] elements = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = elements[0];
+            int age = int.Parse(elements[1]);
+
+            students.Add(new Student()
+            {
+                Name = name,
+                Age = age
+            });
+        }
+
+        string commandFilter = Console.ReadLine();
+        int ageFilter = int.Parse(Console.ReadLine());
+        string[] filter = Console.ReadLine().Split();
+
+        if (commandFilter == "older")
+        {
+            students = students.Where(x => x.Age >= ageFilter).ToList();
+        }
+        else
+        {
+            students = students.Where(x => x.Age < ageFilter).ToList();
+        }
+
+        if (filter.Length > 1)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name} - {student.Age}");
+            }
+        }
+        else if (filter[0] == "name")
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name}");
+            }
+        }
+        else
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Age}");
+            }
+        }
     }
 }
