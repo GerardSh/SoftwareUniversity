67a39aa7a8f80ae7f94b69a746af4015a4fdef46
Gerard <gerardshah@gmail.com>
2024-04-27 16:38:06 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/06 Reverse And Exclude/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+int divisibleNumber = int.Parse(Console.ReadLine());
+
+Predicate<int> remove = numb => numb % divisibleNumber == 0;
+
+numbers.RemoveAll(remove);
+
+Action<List<int>> reverseList = list => list.Reverse();
+
+reverseList(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
