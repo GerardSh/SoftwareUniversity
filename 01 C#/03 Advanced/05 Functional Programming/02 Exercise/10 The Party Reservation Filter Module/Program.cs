8176561e1da36d5c88461ae4ea9f852742b90308
Gerard <gerardshah@gmail.com>
2024-04-27 22:14:55 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/10 The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Reflection;
+
+List<string> partyGuests = Console.ReadLine()
+    .Split()
+    .ToList();
+
+List<string> filtersToAdd = new List<string>();
+List<string> filtersToRemove = new List<string>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Print")
+{
+    string[] elements = input
+        .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+
+    if (command.Contains("Add"))
+    {
+        filtersToAdd.Add(elements[1] + " " + elements[2]);
+    }
+    else
+    {
+        filtersToRemove.Add(elements[1] + " " + elements[2]);
+    }
+}
+
+for (int i = 0; i < filtersToAdd.Count; i++)
+{
+    for (int j = 0; j < filtersToRemove.Count; j++)
+    {
+        if (filtersToAdd[i] == filtersToRemove[j])
+        {
+            filtersToAdd.RemoveAt(i--);
+            filtersToRemove.RemoveAt(j);
+
+            break;
+        }
+    }
+}
+
+List<Predicate<string>> predicates = new List<Predicate<string>>(filtersToAdd.Count);
+
+foreach (string filter in filtersToAdd)
+{
+    string[] filterElements = filter.Split();
+
+    if (filterElements[0].Contains("Starts"))
+    {
+        predicates.Add(new Predicate<string>(name => name.StartsWith(filterElements[2])));
+    }
+    else if (filterElements[0].Contains("Ends"))
+    {
+        predicates.Add(new Predicate<string>(name => name.EndsWith(filterElements[2])));
+    }
+    else if (filterElements[0].Contains("Length"))
+    {
+        predicates.Add(new Predicate<string>(name => name.Length == int.Parse(filterElements[1])));
+    }
+    else
+    {
+        predicates.Add(new Predicate<string>(name => name.Contains(filterElements[1])));
+    }
+}
+
+foreach (Predicate<string> predicate in predicates)
+{
+    partyGuests = partyGuests.Where(x => !predicate(x)).ToList();
+}
+
+Console.WriteLine(string.Join(" ", partyGuests));
