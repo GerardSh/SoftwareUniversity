6dc637f65d2f552ae5670b02281f79414a5887b4
Gerard <gerardshah@gmail.com>
2024-04-27 19:05:12 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/08 List Of Predicates/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var numbers = Enumerable.Range(1, n).ToArray();
+
+var divisors = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Func<int[], int, bool> isDivisible = (divisors, number) =>
+{
+    foreach (int divisor in divisors)
+    {
+        if (number % divisor != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+Action<int> printAction = number => Console.Write(number + " ");
+
+numbers.Where(number => isDivisible(divisors, number)).ToList().ForEach(printAction);
