b11886debc05a53f814a5b3f584f7659a12950e8
Gerard <gerardshah@gmail.com>
2024-04-27 12:34:25 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/04 Find Evens or Odds/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] intRange = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+string OddOrEven = Console.ReadLine();
+
+Predicate<int> predicate = x => true;
+
+switch (OddOrEven)
+{
+    case "odd":
+        predicate = x => x % 2 != 0;
+        break;
+
+    default:
+        predicate = x => x % 2 == 0;
+        break;
+}
+
+int lowerRange = intRange[0];
+int upperRange = intRange[1];
+
+for (int i = lowerRange; i <= upperRange; i++)
+{
+    if (predicate(i))
+    {
+        Console.Write(i + " ");
+    }
+}
