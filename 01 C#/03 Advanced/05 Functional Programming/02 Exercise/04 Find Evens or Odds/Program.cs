6222b75011ff576baf43ab466fff4b46f786a5d4
Gerard <gerardshah@gmail.com>
2024-04-27 12:34:59 +0300

    Advanced - Functional Programming Exercise added

--- 01 C#/03 Advanced/05 Functional Programming/02 Exercise/04 Find Evens or Odds/Program.cs
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/04 Find Evens or Odds/Program.cs
@@ -28,3 +28,37 @@ for (int i = lowerRange; i <= upperRange; i++)
         Console.Write(i + " ");
     }
 }
+
+
+
+
+//2
+int[] intRange = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] numbers = null;
+
+numbers = Enumerable.Range(intRange[0], intRange[1] - intRange[0] + 1).ToArray();
+
+Predicate<int> predicate = num => true;
+
+string filterWord = Console.ReadLine();
+
+if (filterWord == "odd")
+{
+    predicate = num => num % 2 != 0;
+}
+else
+{
+    predicate = num => num % 2 == 0;
+}
+
+//Option1
+//numbers = numbers.Where(num => predicate(num)).ToArray();
+
+//Option2
+numbers = numbers.Where(predicate.Invoke).ToArray();
+
+Console.WriteLine(String.Join(" ", numbers));
