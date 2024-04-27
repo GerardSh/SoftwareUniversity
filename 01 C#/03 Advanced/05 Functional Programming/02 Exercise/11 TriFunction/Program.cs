73d12f8e5bbe6394b7eed5282b90e28d5ae7bfb1
Gerard <gerardshah@gmail.com>
2024-04-27 22:53:11 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/11 TriFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split();
+
+Func<int, string, bool> func = (number, name) =>
+{
+    int sum = 0;
+
+    foreach (var c in name)
+    {
+        sum += (int)c;
+    }
+
+    return sum >= number;
+};
+
+Func<string[], Func<int, string, bool>, string> func2 = (names, predicate) =>
+{
+    foreach (var name in names)
+    {
+        if (predicate(number, name))
+        {
+            return name;
+        }
+    }
+
+    return "no name found";
+};
+
+Console.WriteLine(func2(names, func));
