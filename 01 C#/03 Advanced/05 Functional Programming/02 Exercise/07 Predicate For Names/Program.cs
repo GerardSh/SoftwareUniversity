46ce5bb2c36865885c610a0ba398cba08128c6b1
Gerard <gerardshah@gmail.com>
2024-04-27 17:44:32 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/07 Predicate For Names/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split();
+
+Predicate<string> predicate = name => name.Length <= n;
+Action<string> printName = name => Console.WriteLine(name);
+
+names.Where(name => predicate(name))
+    .ToList()
+    .ForEach(printName);
