0869e402068f46c888cebcef00ad9df062273567
Gerard <gerardshah@gmail.com>
2024-04-27 16:26:41 +0300

    Advanced - Functional Programming Exercise added

---
+++ 01 C#/03 Advanced/05 Functional Programming/02 Exercise/05 Applied Arithmetics/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string input;
+
+Action<int[]> printCollection = numbers => Console.WriteLine(string.Join(" ", numbers));
+
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] elements = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = elements[0];
+
+    Func<int, int> arithmeticFunc = null;
+
+    if (command == "add")
+    {
+        arithmeticFunc = numb => numb + 1;
+    }
+    else if (command == "multiply")
+    {
+        arithmeticFunc = numb => numb * 2;
+    }
+    else if (command == "subtract")
+    {
+        arithmeticFunc = (numb) => numb - 1;
+    }
+    else if (command == "print")
+    {
+        printCollection(numbers);
+        continue;
+    }
+
+    numbers = numbers.Select(arithmeticFunc).ToArray();
+}
