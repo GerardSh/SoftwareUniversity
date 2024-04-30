491c47ad530ef77a955c9db26fa6dd43cfac16a6
Gerard <gerardshah@gmail.com>
2024-04-30 22:44:03 +0300

    Create Dog.cs

---
+++ 01 C#/03 Advanced/06 Defining Classes/01 Lab/00 Hero/Dog.cs
@@ -0,0 +1,50 @@
+﻿namespace Animals
+{
+    internal class Dog
+    {
+        public Dog()
+        {
+
+        }
+
+        public Dog(int age, string name, string breed, string color)
+        {
+            Name = name;
+            Breed = breed;
+            this.age = age;
+            this.color = color;
+        }
+
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public int Size { get; set; }     
+
+        internal static string Country { get; set; }
+
+        private readonly string color;
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        /// <summary>
+        /// The dog will bark.
+        /// </summary>
+        public void Bark()
+        {
+            Console.WriteLine("Bau");
+        }
+    }
+}
