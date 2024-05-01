f8f4ae29db62abd65213cc1b60fe57ffe6ec6eb7
Gerard <gerardshah@gmail.com>
2024-05-01 23:24:50 +0300

    Advanced - Defining Classes Exercise added

---
+++ 01 C#/03 Advanced/06 Defining Classes/02 Exercise/03 Oldest Family Member/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] memberData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = memberData[0];
+                int age = int.Parse(memberData[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            Console.WriteLine(oldestMember.Name + " " + oldestMember.Age);
+        }
+    }
+}
