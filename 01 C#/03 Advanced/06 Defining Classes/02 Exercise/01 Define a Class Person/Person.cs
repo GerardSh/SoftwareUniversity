8219998fa2f6ef2b5fd1ff56ffe21eb5762e3cfa
Gerard <gerardshah@gmail.com>
2024-05-01 22:33:32 +0300

    Advanced - Defining Classes Exercise added

---
+++ 01 C#/03 Advanced/06 Defining Classes/02 Exercise/01 Define a Class Person/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }    
+    }
+}
