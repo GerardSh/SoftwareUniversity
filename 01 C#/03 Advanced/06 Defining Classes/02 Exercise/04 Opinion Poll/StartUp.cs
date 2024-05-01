014c4cffb0f068d4059f3ad11b9ace5098077a8c
Gerard <gerardshah@gmail.com>
2024-05-01 23:32:21 +0300

    Advanced - Defining Classes Exercise added

---
+++ 01 C#/03 Advanced/06 Defining Classes/02 Exercise/04 Opinion Poll/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                people.Add(new Person(name, age));
+            }
+
+            foreach (Person person in people.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
