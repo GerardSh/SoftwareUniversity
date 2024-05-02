9f0177fa90627f9da75801706abad9d0c600351b
Gerard <gerardshah@gmail.com>
2024-05-02 08:59:43 +0300

    Advanced - Defining Classes Exercise added

---
+++ 01 C#/03 Advanced/06 Defining Classes/02 Exercise/05 Date Modifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿namespace DefiningClasses
+{
+    internal class DateModifier
+    {
+        public int DateDifference { get; set; }
+
+        public void GetDateDifference(string[] datesData)
+        {
+              DateTime dateOne = DateTime.Parse(datesData[0]);
+              DateTime dateTwo = DateTime.Parse(datesData[1]);
+
+            var difference = (dateOne - dateTwo).Days;
+
+            DateDifference = Math.Abs(difference);
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/06 Defining Classes/02 Exercise/05 Date Modifier/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] dates = new string[2];
+
+            dates[0] = Console.ReadLine();
+            dates[1] = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+
+            dateModifier.GetDateDifference(dates);
+
+            Console.WriteLine(dateModifier.DateDifference);
+        }
+    }
+}
