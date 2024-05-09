dc0681e1178496a4b13d444fffb6863b20fe8b0b
Gerard <gerardshah@gmail.com>
2024-05-09 18:12:13 +0300

    Advanced - Implementing generic List

---
+++ 01 C#/03 Advanced/07 Implementing Linked List, List,  Stack and Queue - Workshop/02 List/02 Generic/CustomList.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace ImplementingCustomList
+{
+    public class CustomList<T>
+    {
+        private const int DefaultCapacity = 2;
+
+        public T this[int i]
+        {
+            get
+            {
+                if (ValidIndex(i))
+                {
+                    return items[i];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (ValidIndex(i))
+                {
+                    items[i] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        private int capacity => items.Length;
+
+        private T[] items = new T[DefaultCapacity];
+
+        public int Count { get; private set; }
+
+        public void Add(T element)
+        {
+            if (Count == capacity)
+            {
+                Resize();
+            }
+
+            items[Count++] = element;
+        }
+
+        public void Print()
+        {
+            T[] tempArr = new T[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                tempArr[i] = items[i];
+            }
+
+            Console.WriteLine(string.Join(", ", tempArr));
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (!ValidIndex(index))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            T removedValue = items[index];
+
+            ShiftToLeft(index);
+
+            Count--;
+
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+
+            return removedValue;
+        }
+
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (!ValidIndex(firstIndex) || !ValidIndex(secondIndex))
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            T temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+
+        public void InsertAt(int index, T item)
+        {
+            if (!ValidIndex(index))
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (capacity == Count)
+            {
+                Resize();
+            }
+
+            ShiftToRight(index);
+
+            items[index] = item;
+
+            Count++;
+        }
+
+        private void ShiftToLeft(int index)
+        {
+            //Option 1
+            //int[] tempArr = new int[items.Length];
+
+            //for (int i = 0; i < Count - 1; i++)
+            //{
+            //    if (i < index)
+            //    {
+            //        tempArr[i] = items[i];
+            //    }
+            //    else if (i >= index)
+            //    {
+            //        tempArr[i] = items[i + 1];
+            //    }
+            //}
+
+            //items = tempArr;
+
+            //Option 2
+            for (int i = index; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            items[Count - 1] = default;
+        }
+
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                items[i + 1] = items[i];
+            }
+        }
+
+        private void Resize()
+        {
+            T[] tempArr = new T[capacity * 2];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                tempArr[i] = items[i];
+            }
+
+            items = tempArr;
+        }
+
+        private void Shrink()
+        {
+            T[] tempArr = new T[capacity / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                tempArr[i] = items[i];
+            }
+
+            items = tempArr;
+        }
+
+        private bool ValidIndex(int index)
+        {
+            return index >= 0 && index < Count;
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/07 Implementing Linked List, List,  Stack and Queue - Workshop/02 List/02 Generic/StartUp.cs
@@ -0,0 +1,82 @@
+﻿namespace ImplementingCustomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(4);
+
+            intList.Print();
+
+            Console.WriteLine(intList.Contains(2));
+            Console.WriteLine(intList.Contains(5));
+
+            intList.InsertAt(0, 0);
+            intList.InsertAt(3, 3);
+
+            intList.Print();
+
+            intList.RemoveAt(0);
+            intList.RemoveAt(3);
+
+            intList.Print();
+
+            intList.RemoveAt(1);
+
+            intList.Print();
+
+            intList.Swap(0, 1);
+
+            intList.Print();
+
+            intList.Swap(1, 0);
+
+            intList.Print();
+
+            intList.Swap(0, 0);
+
+            intList.Print();
+
+            CustomList<string> stringList = new CustomList<string>();
+
+            stringList.Add("1");
+            stringList.Add("2");
+            stringList.Add("4");
+
+            stringList.Print();
+
+            Console.WriteLine(intList.Contains(2));
+            Console.WriteLine(intList.Contains(5));
+
+            stringList.InsertAt(0, "0");
+            stringList.InsertAt(3, "3");
+
+            stringList.Print();
+
+            stringList.RemoveAt(0);
+            stringList.RemoveAt(3);
+
+            stringList.Print();
+
+            stringList.RemoveAt(1);
+
+            stringList.Print();
+
+            stringList.Swap(0, 1);
+
+            stringList.Print();
+
+            stringList.Swap(1, 0);
+
+            stringList.Print();
+
+            stringList.Swap(0, 0);
+
+            stringList.Print();
+        }
+    }
+}
