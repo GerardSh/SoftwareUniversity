5551672d2465cfb95d789ef3d508277407c0f022
Gerard <gerardshah@gmail.com>
2024-05-10 09:59:17 +0300

    Advanced - Implementing generic Stack

---
+++ 01 C#/03 Advanced/07 Implementing Linked List, List,  Stack and Queue - Workshop/02 Exercise/02 Stack/02 Generic/CustomStack.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+
+namespace ImplementingCustomStack
+{
+    public class CustomStack<T> 
+        : IEnumerable<T>
+    {
+        const int InitialCapacity = 4;
+
+        private T[] items { get; set; } = new T[InitialCapacity];
+
+        public int Count { get; private set; }
+
+        private int Capacity => items.Length;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return items[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void Push(T item)
+        {
+            if (Count == Capacity)
+            {
+                Resize();
+            }
+
+            items[Count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack empty.");
+            }
+
+            T lastItem = items[Count - 1];
+            items[Count-- - 1] = default;
+
+            if (Count <= Capacity / 4)
+            {
+                Shrink();
+            }
+
+            return lastItem;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack empty.");
+            }
+
+            return items[Count - 1];
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        private void Shrink()
+        {
+            T[] tempArr = new T[Capacity / 4];
+
+            for (int i = 0; i < Count; i++)
+            {
+                tempArr[i] = items[i];
+            }
+
+            items = tempArr;
+        }
+
+        private void Resize()
+        {
+            T[] tempArr = new T[Capacity * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                tempArr[i] = items[i];
+            }
+
+            items = tempArr;
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/07 Implementing Linked List, List,  Stack and Queue - Workshop/02 Exercise/02 Stack/02 Generic/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace ImplementingCustomStack
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            CustomStack<int> intStack = new CustomStack<int>();
+
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+            intStack.Push(4);
+            intStack.Push(5);
+
+            var intStack2 = new CustomStack<int>();
+
+            intStack.ForEach(x =>
+            {
+                intStack2.Push(x);
+            });
+
+            Console.WriteLine(intStack.Pop());
+            Console.WriteLine(intStack.Pop());
+            Console.WriteLine(intStack.Pop());
+            Console.WriteLine(intStack.Pop());
+
+            Console.WriteLine(intStack.Peek());
+            Console.WriteLine(intStack.Peek());
+
+            CustomStack<string> stringStack = new CustomStack<string>();
+
+            stringStack.Push("1");
+            stringStack.Push("2");
+            stringStack.Push("3");
+            stringStack.Push("4");
+            stringStack.Push("5");
+
+            var stringStack2 = new CustomStack<string>();
+
+            stringStack.ForEach(x =>
+            {
+                stringStack2.Push(x);
+            });
+
+            Console.WriteLine(stringStack.Pop());
+            Console.WriteLine(stringStack.Pop());
+            Console.WriteLine(stringStack.Pop());
+            Console.WriteLine(stringStack.Pop());
+
+            Console.WriteLine(stringStack.Peek());
+            Console.WriteLine(stringStack.Peek());
+        }
+    }
+}
