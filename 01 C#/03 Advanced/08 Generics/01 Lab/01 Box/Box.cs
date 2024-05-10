40604941ab7655df9aca94f11c1c277fbff58562
Gerard <gerardshah@gmail.com>
2024-05-10 21:08:18 +0300

    Advanced - Generics Lab exercise added

---
+++ 01 C#/03 Advanced/08 Generics/01 Lab/01 Box/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        Stack<T> items = new Stack<T>();
+
+        public int Count => items.Count;
+
+        public void Add(T element)
+        {
+            items.Push(element);
+        }
+
+        public T Remove()
+        {
+            return items.Pop();
+        }
+    }
+}
