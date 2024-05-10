6181e91981144c1b73e129afcdc040f8356e21fe
Gerard <gerardshah@gmail.com>
2024-05-10 12:00:16 +0300

    Advanced - Implementing generic Queue

---
+++ 01 C#/03 Advanced/07 Implementing Linked List, List,  Stack and Queue - Workshop/02 Exercise/03 Queue/02 Generic/CustomQueue.cs
@@ -0,0 +1,110 @@
+﻿namespace ImplementingCustomQueue
+{
+    internal class CustomQueue<T>
+    {
+        private const int InitialCapacity = 4;
+
+        private const int FirstItemIndex = 0;
+
+        public int Count { get; private set; }
+
+        private T[] items { get; set; } = new T[InitialCapacity];
+
+        private int Capacity => items.Length;
+
+        public void Enqueue(T item)
+        {
+            if (Count == Capacity)
+            {
+                Resize();
+            }
+
+            items[Count++] = item;
+        }
+
+        public T Dequeue()
+        {
+            IsEmpty();
+
+            T firstItem = items[FirstItemIndex];
+
+            ShiftToLeft();
+
+            items[Count-- - 1] = default;
+
+            if (Count <= Capacity / 4)
+            {
+                Shrink();
+            }
+
+            return firstItem;
+        }
+
+        public T Peek()
+        {
+            IsEmpty();
+
+            return items[FirstItemIndex];
+        }
+
+        public void Clear()
+        {
+            IsEmpty();
+
+            items = new T[InitialCapacity];
+            Count = 0;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        private void IsEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Queue empty.");
+            }
+        }
+
+        private void ShiftToLeft()
+        {
+            for (int i = 0; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+        }
+
+        private void Shrink()
+        {
+            T[] tempArr = new T[Capacity / 4];
+
+            for (int i = 0; i < Count; i++)
+            {
+                tempArr[i] = items[i];
+            }
+
+            items = tempArr;
+        }
+
+        private void Resize()
+        {
+            T[] tempArr = new T[Capacity * 2];
+
+            //Option 1
+            //for (int i = 0; i < Count; i++)
+            //{
+            //    tempArr[i] = items[i];
+            //}
+
+            //Option 2
+            items.CopyTo(tempArr, 0);
+
+            items = tempArr;
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/07 Implementing Linked List, List,  Stack and Queue - Workshop/02 Exercise/03 Queue/02 Generic/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace ImplementingCustomQueue
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            CustomQueue<string> queue2 = new CustomQueue<string>();
+
+            int count = queue.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                queue2.Enqueue(queue.Dequeue().ToString());
+            }
+
+            queue2.ForEach(x => Console.WriteLine(x));
+
+            Console.WriteLine(queue2.Dequeue());
+            Console.WriteLine(queue2.Dequeue());
+            Console.WriteLine(queue2.Dequeue());
+            Console.WriteLine(queue2.Dequeue());
+
+            Console.WriteLine(queue2.Peek());
+            Console.WriteLine(queue2.Peek());
+
+            queue2.Clear();
+
+            Console.WriteLine(queue2.Count);
+        }
+    }
+}
