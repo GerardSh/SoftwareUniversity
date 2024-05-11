c323449ad597f89224c1f47c3835ea5d7f739f43
Gerard <gerardshah@gmail.com>
2024-05-11 22:11:00 +0300

    Advanced - Generics Exercise added

---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/03 Generic Swap Method String/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            SwapElements(list, indexes);
+        }
+
+        static void SwapElements<T>(List<T> list, int[] indexes)
+        {
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+
+            foreach (T t in list)
+            {
+                Console.WriteLine($"{typeof(T)}: {t}");
+            }
+        }
+    }
+}
