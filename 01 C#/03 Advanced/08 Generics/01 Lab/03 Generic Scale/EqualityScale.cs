b2a8152a6e0d76a799b6f4fc6faea6a551f5be5b
Gerard <gerardshah@gmail.com>
2024-05-11 12:04:28 +0300

    Advanced - Generics Lab exercise added

---
+++ 01 C#/03 Advanced/08 Generics/01 Lab/03 Generic Scale/EqualityScale.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+        where T : struct
+    {
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        internal T Left { get; set; }
+
+        internal T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+    }
+}
