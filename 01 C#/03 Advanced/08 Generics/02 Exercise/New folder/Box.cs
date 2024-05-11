52f8c3d89123b601e8e003895b85946116f1e8b4
Gerard <gerardshah@gmail.com>
2024-05-11 21:47:25 +0300

    Advanced - Generics Exercise added

---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/New folder/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp
+{
+    public class Box<T>
+    {
+        public Box(T value) 
+        { 
+        this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {value}";
+        }
+
+        T value;
+    }
+}
