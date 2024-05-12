a2a1533a82007cb60873de6c49e04a3b5bd58a72
Gerard <gerardshah@gmail.com>
2024-05-12 13:32:10 +0300

    Advanced - Generics Exercise added

---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/05 Generic Count Method String/02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            string valueToCompare = Console.ReadLine();
+
+            Console.WriteLine(CompareElements(list, valueToCompare));
+        }
+
+        static int CompareElements<T>(List<T> list, T valueToCompare)
+            where T : IComparable<T>
+        {
+            return list.Count(x => x.CompareTo(valueToCompare) > 0);
+        }
+    }
+}
