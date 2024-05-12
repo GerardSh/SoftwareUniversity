db389301c1ba3dda0139f9a9025bcf809635f47f
Gerard <gerardshah@gmail.com>
2024-05-12 17:34:48 +0300

    Advanced - Generics Exercise added

---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/07 Tuple/CustomTuple.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp
+{
+    public class CustomTuple<T,T2>
+    {
+        public override string ToString()
+        {
+            return $"{ItemOne} -> {ItemTwo}";
+        }
+
+        public T ItemOne { get; set; }
+
+        public T2 ItemTwo { get; set; }
+    }
+}
---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/07 Tuple/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] elements = Console.ReadLine()
+                .Split();
+
+            CustomTuple<string, string> tupleOne = new CustomTuple<string, string>();
+
+            tupleOne.ItemOne = $"{elements[0]} {elements[1]}";
+            tupleOne.ItemTwo = elements[2];
+
+            elements = Console.ReadLine()
+                .Split();
+
+            CustomTuple<string, int> tupleTwo = new CustomTuple<string, int>();
+
+            tupleTwo.ItemOne = $"{elements[0]}";
+            tupleTwo.ItemTwo = int.Parse(elements[1]);
+
+            elements = Console.ReadLine()
+               .Split();
+
+            CustomTuple<int, double> tupleThree = new CustomTuple<int, double>();
+
+            tupleThree.ItemOne = int.Parse(elements[0]);
+            tupleThree.ItemTwo = double.Parse(elements[1]);
+
+            Console.WriteLine(tupleOne);
+            Console.WriteLine(tupleTwo);
+            Console.WriteLine(tupleThree);
+        }
+    }
+}
