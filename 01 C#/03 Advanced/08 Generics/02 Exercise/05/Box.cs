b39ef6c009644d608d35115f04de63cb03296ae2
Gerard <gerardshah@gmail.com>
2024-05-12 13:21:57 +0300

    Advanced - Generics Exercise added

---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/05/Box.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp
+{
+    public class Box<T>
+        where T : IComparable<T>
+    {
+        public List<T> list = new List<T>();
+
+        private T value;
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public void AddValue(T value)
+        {
+            this.value = value;
+        }
+
+        public int CountElements()
+        {
+            return list.Count(x => x.CompareTo(value) > 0);
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/08 Generics/02 Exercise/05/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+
+            box.AddValue(Console.ReadLine());
+
+            Console.WriteLine(box.CountElements());
+        }
+    }
+}
