ed0b4a65b9886f1398595533b4d2e01113c8e2b8
Gerard <gerardshah@gmail.com>
2024-05-14 22:26:29 +0300

    Advanced - Iterators and Comparators Lab exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/01 Lab/02 LibraryIterator/Library.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+
+            private int index;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                index = -1;
+            }
+
+            public Book Current => books[index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+
+                if (index < books.Count)
+                {               
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
