7e5c3a741d1321693dc497c34558923371b4f4e7
Gerard <gerardshah@gmail.com>
2024-05-14 23:35:53 +0300

    Advanced - Iterators and Comparators Lab exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/01 Lab/04 BookComparer/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+    }
+}
