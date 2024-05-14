4203376ecb86a7af53a3c9ebf8d405f65741b757
Gerard <gerardshah@gmail.com>
2024-05-14 22:26:52 +0300

    Advanced - Iterators and Comparators Lab exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/01 Lab/03 ComparableBook/Book.cs
@@ -0,0 +1,39 @@
+﻿namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book? other)
+        {
+            if (Year > other.Year)
+            {
+                return 1;
+            }
+            else if (Year < other.Year)
+            {
+                return -1;
+            }
+            else
+            {
+                return Title.CompareTo(other.Title);
+            }
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/01 Lab/03 ComparableBook/Library.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+            this.books.Sort();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return books[i];
+            }
+           
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
