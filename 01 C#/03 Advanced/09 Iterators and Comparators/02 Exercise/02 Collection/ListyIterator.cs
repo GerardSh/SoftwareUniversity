f6dde712ec94add3e08ecfcfcc1443999bfdad0e
Gerard <gerardshah@gmail.com>
2024-05-16 20:32:40 +0300

    Advanced - Iterators and Comparators Exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/02 Collection/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+
+namespace ConsoleApp
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        List<T> list;
+
+        int index = 0;
+
+        public ListyIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public bool Move()
+        {
+            if (index < list.Count - 1)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < list.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (list.Count > 0)
+            {
+                Console.WriteLine(list[index]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                yield return list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/02 Collection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ListyIterator<string> iterator;
+
+            List<string> input = Console.ReadLine().Split().Skip(1).ToList();
+
+            iterator = new ListyIterator<string>(input);
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(iterator.Move());
+                }
+                else if (command == "Print")
+                {
+                    iterator.Print();
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(iterator.HasNext());
+                }
+                else if (command == "PrintAll")
+                {
+                    var sb = new StringBuilder();
+
+                    foreach (string item in iterator)
+                    {
+                        sb.Append(item + " ");
+                    }
+
+                    Console.WriteLine(sb.ToString().TrimEnd());
+                }
+            }
+        }
+    }
+}
