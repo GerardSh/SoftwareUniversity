504608e17a3a80cf365d76e29d614773ebadc1fb
Gerard <gerardshah@gmail.com>
2024-05-17 00:38:06 +0300

    Advanced - Iterators and Comparators Exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/03 Stack/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Stack<string> stack = new Stack<string>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] elements = input
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string command = elements[0];
+
+                if (command == "Push")
+                {
+                    stack.Push(elements.Skip(1).ToArray());
+                }
+                else if (command == "Pop")
+                {
+                    if (stack.HasElements())
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (string number in stack)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/03 Stack/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace ConsoleApp
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        List<T> list = new List<T>();
+
+        public void Push(params T[] elements)
+        {
+            list.AddRange(elements);
+        }
+
+        public bool HasElements()
+        {
+            return list.Count > 0;
+        }
+
+        public T Pop()
+        {
+            T temp = list[list.Count - 1];
+
+            list.RemoveAt(list.Count -1);
+
+            return temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                yield return list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
