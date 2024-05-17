52a5c82030e1cbe9595003fcc5fb99bddb53f61c
Gerard <gerardshah@gmail.com>
2024-05-17 13:26:21 +0300

    Advanced - Iterators and Comparators Exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/07 Custom Comparator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp
+{
+    public class Comparator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+            else if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+
+            return x.CompareTo(y);
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            numbers.Sort(new Comparator());
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
