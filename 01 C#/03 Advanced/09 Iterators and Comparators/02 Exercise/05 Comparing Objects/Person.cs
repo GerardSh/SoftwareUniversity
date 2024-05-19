f30d15bce22a4db60419313187d9a8aebfb3b5eb
Gerard <gerardshah@gmail.com>
2024-05-19 11:20:31 +0300

    Advanced - Iterators and Comparators Exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/05 Comparing Objects/Person.cs
@@ -0,0 +1,59 @@
+﻿namespace ConsoleApp
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person? other)
+        {
+            //Option 1
+            int result = Name.CompareTo(other.Name);
+
+            if (result == 1)
+            {
+                return 1;
+            }
+            else if (result == -1)
+            {
+                return -1;
+            }
+
+            if (Age > other.Age)
+            {
+                return 1;
+            }
+            else if (Age < other.Age)
+            {
+                return -1;
+            }
+
+            return Town.CompareTo(other.Town);
+
+            //Option 2
+            int result = Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age)
+            }
+
+            if (result == 0)
+            {
+                result = Town.CompareTo(other.Town);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/05 Comparing Objects/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] elements = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = elements[0];
+                int age = int.Parse(elements[1]);
+                string town = elements[2];
+
+                Person person = new Person(name, age, town);
+
+                people.Add(person);
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            int equalPeople = 0;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[n - 1].CompareTo(people[i]) == 0) 
+                {
+                equalPeople++;
+                }
+            }
+
+            if (equalPeople == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalPeople} {people.Count - equalPeople} {people.Count}");
+            }
+        }
+    }
+}
