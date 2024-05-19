d2bdb2e82f0085c893c49de079fe2900a8d91f77
Gerard <gerardshah@gmail.com>
2024-05-19 11:21:08 +0300

    Advanced - Iterators and Comparators Exercise added

---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/06 Equality Logic/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CompareTo(Person? other)
+        {
+            int result = Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Age.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            //Option 1
+            Person person = (Person)obj;
+            return Name == person.Name && Age == person.Age;
+
+            //Option2
+            return this.GetHashCode() == obj.GetHashCode();
+        }
+    }
+}
---
+++ 01 C#/03 Advanced/09 Iterators and Comparators/02 Exercise/06 Equality Logic/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> set = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+
+                Person person = new Person(personData[0], int.Parse(personData[1]));
+
+                sortedSet.Add(person);
+                set.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(set.Count);
+        }
+    }
+}
