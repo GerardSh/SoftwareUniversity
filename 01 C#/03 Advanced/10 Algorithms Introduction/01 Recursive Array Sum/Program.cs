77141ea4e5aba501205c826c5f61cb2f0ce6ba07
Gerard <gerardshah@gmail.com>
2024-05-20 00:08:13 +0300

    Advanced - Algorithms Introduction exercise added

---
+++ 01 C#/03 Advanced/10 Algorithms Introduction/01 Recursive Array Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int result = GetArraySum(0, Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            Console.WriteLine(result);
+        }
+
+        static int GetArraySum(int index, params int[] numbers)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + GetArraySum(index + 1, numbers);
+        }
+    }
+}
