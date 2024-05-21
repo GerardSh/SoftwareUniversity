68674783ca60648b6c1980abec45063d735546a3
Gerard <gerardshah@gmail.com>
2024-05-21 20:56:43 +0300

    Advanced - Algorithms Introduction exercise added

---
+++ 01 C#/03 Advanced/10 Algorithms Introduction/02 Recursive Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            long result = GetFactorial(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(result);
+        }
+
+        static long GetFactorial(int number)
+        {
+            if (number == 1)
+            {
+                return number;
+            }
+
+            return number * GetFactorial(number - 1);
+        }
+    }
+}
