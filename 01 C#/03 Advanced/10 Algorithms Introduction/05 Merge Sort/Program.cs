589098c89cf7469b415ee98f4b495f08a86ed48a
Gerard <gerardshah@gmail.com>
2024-05-22 12:26:23 +0300

    Advanced - Algorithms Introduction exercise added

---
+++ 01 C#/03 Advanced/10 Algorithms Introduction/05 Merge Sort/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Console.WriteLine(string.Join(" ", Split(list, 0, list.Count)));
+        }
+
+        static List<int> Split(List<int> list, int start, int end)
+        {
+            if (end - start == 1)
+            {
+                return new List<int>() { list[start] };
+            }
+
+            int middle = (start + end) / 2;
+
+            List<int> sortedLeftPart = Split(list, start, middle);
+
+            List<int> sortedRightPart = Split(list, middle, end);
+
+            return Merge(sortedLeftPart, sortedRightPart);
+        }
+
+        static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> sortedList = new List<int>(left.Count + right.Count);
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+    
+            for (int i = 0; i < left.Count + right.Count; i++)
+            {
+                if (leftIndex >= left.Count)
+                {
+                    sortedList.Add(right[rightIndex++]);
+                    continue;
+                }
+
+                if (rightIndex >= right.Count)
+                {
+                    sortedList.Add(left[leftIndex++]);
+                    continue;
+                }
+
+                if (left[leftIndex] <= right[rightIndex])
+                {
+                    sortedList.Add(left[leftIndex++]);
+                }
+                else
+                {
+                    sortedList.Add(right[rightIndex++]);
+                }
+            }
+
+            return sortedList;
+        }
+    }
+}
