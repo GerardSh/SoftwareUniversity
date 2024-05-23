e1deedd6658c0b0f2d57d957b9c7fb7329c92879
Gerard <gerardshah@gmail.com>
2024-05-23 23:25:16 +0300

    Advanced - Algorithms Introduction exercise added

---
+++ 01 C#/03 Advanced/10 Algorithms Introduction/07 Binary Search/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp
+{
+    public class BinarySearch
+    {
+        public static int IndexOf(int[] arr, int key)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low <= high)
+            {
+                int midIndex = low + (high - low) / 2;
+
+                if (key > arr[midIndex])
+                {
+                    low = midIndex + 1;
+
+                }
+                else if (key < arr[midIndex])
+                {
+                    high = midIndex - 1;
+                }
+                else
+                {
+                    return midIndex;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int key = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch.IndexOf(numbers, key));
+        }
+    }
+}
