a932afd399962da8b53c8465521909ea4cf864e6
Gerard <gerardshah@gmail.com>
2024-05-23 23:24:25 +0300

    Advanced - Algorithms Introduction exercise added

---
+++ 01 C#/03 Advanced/10 Algorithms Introduction/06 Quick Sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            QuickSort(array, 0, array.Length - 1);
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+
+        public static int Part(int[] array, int low, int high, int pivot)
+        {
+            while (low <= high)
+            {
+                while (array[low] < pivot)
+                {
+                    low++;
+                }
+
+                while (array[high] > pivot)
+                {
+                    high--;
+                }
+
+                if (low <= high)
+                {
+                    int temp = array[low];
+                    array[low] = array[high];
+                    array[high] = temp;
+                    low++;
+                    high--;
+                }
+            }
+
+            return low;
+        }
+
+        public static void QuickSort(int[] array, int lo, int high)
+        {
+            if (lo >= high) return;
+
+            int pivot = array[(lo + high) / 2];
+            int index = Part(array, lo, high, pivot);
+
+            QuickSort(array, lo, index - 1);
+
+            QuickSort(array, index, high);
+        }
+    }
+}
