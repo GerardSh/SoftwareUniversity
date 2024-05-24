6e2e25eabb692e6004e7477ae26d313e8a548a71
Gerard <gerardshah@gmail.com>
2024-05-24 20:04:38 +0300

    Advanced - Exam preparation exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/01 C# Advanced Retake Exam - 10 April 2024/02 Escape the Maze/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static char[,] maze;
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            maze = new char[n, n];
+
+            int playersRow = 0;
+            int playersCol = 0;
+            int playersHealth = 100;
+
+            for (int row = 0; row < n; row++)
+            {
+                string currentRow = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    maze[row, col] = currentRow[col];
+
+                    if (currentRow[col] == 'P')
+                    {
+                        playersRow = row;
+                        playersCol = col;
+                    }
+                }
+            }
+
+            bool gameOver = false;
+
+            while (!gameOver)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "up")
+                {
+                    if (!ValidIndex(playersRow - 1, playersCol))
+                    {
+                        continue;
+                    }
+
+                    maze[playersRow--, playersCol] = '-';
+                }
+                else if (command == "down")
+                {
+                    if (!ValidIndex(playersRow + 1, playersCol))
+                    {
+                        continue;
+                    }
+
+                    maze[playersRow++, playersCol] = '-';
+                }
+                else if (command == "left")
+                {
+                    if (!ValidIndex(playersRow, playersCol - 1))
+                    {
+                        continue;
+                    }
+
+                    maze[playersRow, playersCol--] = '-';
+                }
+                else if (command == "right")
+                {
+                    if (!ValidIndex(playersRow, playersCol + 1))
+                    {
+                        continue;
+                    }
+
+                    maze[playersRow, playersCol++] = '-';
+                }
+
+                char currentChar = maze[playersRow, playersCol];
+
+                if (currentChar == 'M')
+                {
+                    playersHealth -= 40;
+
+                    if (playersHealth <= 0)
+                    {
+                        gameOver = true;
+                    }
+                }
+                else if (currentChar == 'H')
+                {
+                    playersHealth += 15;
+
+                    if (playersHealth > 100)
+                    {
+                        playersHealth = 100;
+                    }        
+                }
+                else if (currentChar == 'X')
+                {
+                    gameOver = true;
+                }
+
+                maze[playersRow, playersCol] = 'P';
+            }
+
+            if (playersHealth > 0)
+            {
+                Console.WriteLine("Player escaped the maze. Danger passed!");               
+            }
+            else
+            {
+                Console.WriteLine("Player is dead. Maze over!");
+
+                playersHealth = 0;
+            }
+
+            Console.WriteLine($"Player's health: {playersHealth} units");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(maze[row,col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool ValidIndex(int row, int col)
+        {
+            return row >= 0 && row < maze.GetLength(0) && col >= 0 && col < maze.GetLength(1);
+        }
+    }
+}
