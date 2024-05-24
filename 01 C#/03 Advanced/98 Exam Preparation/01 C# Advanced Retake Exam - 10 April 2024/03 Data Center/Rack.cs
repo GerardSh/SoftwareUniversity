cec60640db674a1dcc5852e59ffd03751de1eca1
Gerard <gerardshah@gmail.com>
2024-05-24 22:37:01 +0300

    Advanced - Exam preparation exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/01 C# Advanced Retake Exam - 10 April 2024/03 Data Center/Rack.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+
+namespace DataCenter
+{
+    public class Rack
+    {
+        public Rack(int slots)
+        {
+            Slots = slots;
+            Servers = new List<Server>();
+        }
+
+        public int Slots { get; set; }
+
+        public List<Server> Servers { get; set; }
+
+        public int GetCount => Servers.Count;
+
+        public void AddServer(Server server)
+        {
+            if (GetCount >= Slots || Servers.FirstOrDefault(x => x.SerialNumber == server.SerialNumber) != null)
+            {
+                return;
+            }
+
+            Servers.Add(server);
+        }
+
+        public bool RemoveServer(string serialNumber)
+        {
+            Server server = Servers.FirstOrDefault(x => x.SerialNumber == serialNumber);
+
+            return Servers.Remove(server);
+        }
+
+        public string GetHighestPowerUsage()
+        {
+            //Option 1
+            Server bestServer = Servers.OrderByDescending(x => x.PowerUsage).FirstOrDefault();
+
+            //Option 2
+            // Server bestServer = null;
+            //int maxPower = 0;
+
+            //foreach (var server in Servers)
+            //{
+            //    if (server.PowerUsage > maxPower)
+            //    {
+            //        bestServer = server;
+            //        maxPower = server.PowerUsage;
+            //    }
+            //}
+
+            return bestServer.ToString();
+        }
+
+        public int GetTotalCapacity()
+        {
+            //Option 1
+            int totalCapacity = Servers.Sum(x => x.Capacity);
+
+            //Option 2
+            //int totalCapacity = 0;
+
+            //foreach (var server in Servers)
+            //{
+            //    totalCapacity += server.Capacity;
+            //}
+
+            return totalCapacity;
+        }
+
+        public string DeviceManager()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{GetCount} servers operating:");
+
+            foreach (var server in Servers)
+            {
+                sb.AppendLine(server.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
