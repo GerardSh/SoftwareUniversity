bae1d03be0d4d2f6bdde209fa63a3e81b06791ac
Gerard <gerardshah@gmail.com>
2024-05-25 17:27:23 +0300

    Advanced - Exam preparation exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/03 C# Advanced Retake Exam - 13 December 2023/03 Groceries Management/GroceriesStore.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace GroceriesManagement
+{
+    public class GroceriesStore
+    {
+        public GroceriesStore(int capacity)
+        {
+            Capacity = capacity;
+            Turnover = 0;
+            Stall = new List<Product>(capacity);
+        }
+
+        public int Capacity { get; set; }
+
+        public double Turnover { get; set; }
+
+        public List<Product> Stall { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            if (Stall.Count < Capacity && !Stall.Contains(product))
+            {
+                Stall.Add(product);
+            }
+        }
+
+        public bool RemoveProduct(string name) => Stall.Remove(Stall.FirstOrDefault(x => x.Name == name));
+
+        public string SellProduct(string name, double quantity)
+        {
+            Product product = Stall.FirstOrDefault(x => x.Name == name);
+
+            if (product == null)
+            {
+                return "Product not found";
+            }
+
+            double price = Math.Round(product.Price * quantity, 2);
+            Turnover += price;
+
+            return $"{name} - {price:F2}$";
+        }
+
+        public string GetMostExpensive()
+        {
+            return Stall.OrderByDescending(x => x.Price).FirstOrDefault().ToString();
+        }
+
+        public string CashReport()
+        {   
+            return $"Total Turnover: {Turnover:F2}$";
+        }
+
+        public string PriceList()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Groceries Price List:");
+
+            foreach (var item in Stall)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
