0596ebd595007ac4372d17cc695b737f93668304
Gerard <gerardshah@gmail.com>
2024-05-26 12:03:00 +0300

    Advanced - Exam preparation exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/06 C# Advanced Regular Exam - 17 June 2023/01 Temple of Doom/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var tools = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            var substances = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            var chalanges = new List<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+
+            while (tools.Any() && substances.Any())
+            {
+                int tool = tools.Dequeue();
+                int substance = substances.Pop();
+
+                int result = tool * substance;
+
+                if (chalanges.Contains(result))
+                {
+                    chalanges.Remove(result);
+                }
+                else
+                {
+                    tools.Enqueue(tool + 1);
+
+                    if (substance - 1 > 0)
+                    {
+                        substances.Push(substance - 1);
+                    }
+                }
+            }
+
+            if (chalanges.Any())
+            {
+                Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+            }
+            else
+            {
+                Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+            }
+
+            if (tools.Any())
+            {
+                Console.WriteLine("Tools: " + string.Join(", ", tools));
+            }
+
+            if (substances.Any())
+            {
+                Console.WriteLine("Substances: " + string.Join(", ", substances));
+            }
+
+            if (chalanges.Any())
+            {
+                Console.WriteLine("Chalanges: " + string.Join(", ", chalanges));
+            }
+        }
+    }
+}
