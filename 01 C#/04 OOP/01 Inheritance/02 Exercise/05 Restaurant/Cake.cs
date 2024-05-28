da625c971aa551f0d1b1a81f86292568515dc34e
Gerard <gerardshah@gmail.com>
2024-05-28 21:16:59 +0300

    OOP - Inheritance - Exercise - exercise added

---
+++ 01 C#/04 OOP/01 Inheritance/02 Exercise/05 Restaurant/Cake.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        const decimal CakePrice = 5m;
+        const double CakeGrams = 250;
+        const double CakeCalories = 1000;
+
+        public Cake(string name)
+            :base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+
+        }
+    }
+}
---
+++ 01 C#/04 OOP/01 Inheritance/02 Exercise/05 Restaurant/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        const decimal CoffeePrice = 3.5m;
+        const double CoffeeMilliliters = 50;
+
+        public Coffee(string name, double caffeine)
+           : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+       public double Caffeine { get; set; }
+    }
+}
