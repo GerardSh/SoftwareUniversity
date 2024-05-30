5e27e81143caff38ac3c8f06c9da18bd71eb360b
Gerard <gerardshah@gmail.com>
2024-05-30 14:05:00 +0300

    OOP - Encapsulation - Lab - exercise added

---
+++ 01 C#/04 OOP/02 Encapsulation/01 Lab/01 Persons/Program.cs
@@ -0,0 +1,27 @@
+﻿//namespace ConsoleApp
+//{
+
+//    class Person2 : Person
+//    {
+//        public Person2(string name) : base(name)
+//        {
+//            Name = "test";
+//        }
+//    }
+
+//    public class Program 
+//    {
+//        static void Main()
+//        {
+
+
+
+//            List<Person> list = new List<Person>();
+
+//            Person person = new Person2("test2");
+
+//            person.Name;
+         
+//        }
+//    }
+//}
