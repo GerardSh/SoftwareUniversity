44822de4b21ac341a8202201da415c6b4e10cb90
Gerard <gerardshah@gmail.com>
2024-06-05 12:05:16 +0300

    OOP - Interfaces and Abstraction - Lab - exercise added

---
+++ 01 C#/04 OOP/03 Interfaces and Abstraction/01 Lab/02 Cars/Tesla.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    internal class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {     
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public int Battery { get ; set ; }
+
+        public string Model { get ; set ; }
+
+        public string Color { get ; set ; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString().Trim();
+        }
+    }
+}
