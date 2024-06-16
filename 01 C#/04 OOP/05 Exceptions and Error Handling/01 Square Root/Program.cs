47ab54be7b8415d258adce45828ffa1dcabd7ac8
Gerard <gerardshah@gmail.com>
2024-06-16 22:42:44 +0300

    OOP - Exceptions and Error Handling - exercise added

---
+++ 01 C#/04 OOP/05 Exceptions and Error Handling/01 Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            number = Math.Sqrt(number);
+
+            Console.WriteLine(number);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye.");
+        }
+    }
+}
