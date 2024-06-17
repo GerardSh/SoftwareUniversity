bc20c0ac37b8f7a881baf2876fb322547bf53161
Gerard <gerardshah@gmail.com>
2024-06-17 16:55:23 +0300

    Advanced - Exam preparation - exercise added

--- 01 C#/03 Advanced/98 Exam Preparation/06 C# Advanced Regular Exam - 17 June 2023/02 Mouse In The Kitchen/Program.cs
+++ 01 C#/03 Advanced/98 Exam Preparation/06 C# Advanced Regular Exam - 17 June 2023/02 Mouse In The Kitchen/Program.cs
@@ -1,5 +1,168 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+            char[,] cupboard = new char[dimensions[0], dimensions[1]];
+
+            int mouseRow = 0;
+            int mouseCol = 0;
+            int cheeseCount = 0;
+            int cheeseEaten = 0;
+
+            for (int row = 0; row < cupboard.GetLength(0); row++)
+            {
+                string currentRow = Console.ReadLine();
+
+                for (int col = 0; col < cupboard.GetLength(1); col++)
+                {
+                    cupboard[row, col] = currentRow[col];
+
+                    if (currentRow[col] == 'M')
+                    {
+                        mouseRow = row;
+                        mouseCol = col;
+                    }
+
+                    if (currentRow[col] == 'C')
+                    {
+                        cheeseCount++;
+                    }
+                }
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "danger")
+            {
+                char symbol = ' ';
+
+                bool isOutside = false;
+
+                if (command == "up")
+                {
+                    if (IsValid(cupboard, mouseRow - 1, mouseCol))
+                    {
+                        if (cupboard[mouseRow - 1, mouseCol] == '@')
+                        {
+                            continue;
+                        }
+
+                        cupboard[mouseRow, mouseCol] = '*';
+                        symbol = cupboard[--mouseRow, mouseCol];
+                        cupboard[mouseRow, mouseCol] = 'M';
+                    }
+                    else
+                    {
+                        isOutside = true;
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (IsValid(cupboard, mouseRow + 1, mouseCol))
+                    {
+                        if (cupboard[mouseRow + 1, mouseCol] == '@')
+                        {
+                            continue;
+                        }
+                        cupboard[mouseRow, mouseCol] = '*';
+                        symbol = cupboard[++mouseRow, mouseCol];
+                        cupboard[mouseRow, mouseCol] = 'M';
+                    }
+                    else
+                    {
+                        isOutside = true;
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (IsValid(cupboard, mouseRow, mouseCol - 1))
+                    {
+                        if (cupboard[mouseRow, mouseCol - 1] == '@')
+                        {
+                            continue;
+                        }
+                        cupboard[mouseRow, mouseCol] = '*';
+                        symbol = cupboard[mouseRow, --mouseCol];
+                        cupboard[mouseRow, mouseCol] = 'M';
+                    }
+                    else
+                    {
+                        isOutside = true;
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (IsValid(cupboard, mouseRow, mouseCol + 1))
+                    {
+                        if (cupboard[mouseRow, mouseCol + 1] == '@')
+                        {
+                            continue;
+                        }
+
+                        cupboard[mouseRow, mouseCol] = '*';
+                        symbol = cupboard[mouseRow, ++mouseCol];
+                        cupboard[mouseRow, mouseCol] = 'M';
+                    }
+                    else
+                    {
+                        isOutside = true;
+                    }
+                }
+
+                if (isOutside)
+                {
+                    Console.WriteLine("No more cheese for tonight!");
+                    break;
+                }
+
+                if (symbol == 'C')
+                {
+                    cheeseEaten++;
+                }
+                else if (symbol == 'T')
+                {
+                    Console.WriteLine("Mouse is trapped!");
+                    break;
+                }
+
+                if (cheeseCount == cheeseEaten)
+                {
+                    Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+                    break;
+                }
+            }
+
+            if (command == "danger")
+            {
+                Console.WriteLine("Mouse will come back later!");
+            }
+
+            for (int row = 0; row < cupboard.GetLength(0); row++)
+            {
+                for (int col = 0; col < cupboard.GetLength(1); col++)
+                {
+                    Console.Write(cupboard[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsValid(char[,] cupboard, int row, int col) => row >= 0 && row < cupboard.GetLength(0) && col >= 0 && col < cupboard.GetLength(1);
+    }
+}
+
+
+
+
+//2
 namespace ConsoleApp
 {
     public class Program
