c47928ca876e461d82aebbdc7f97d9088d393a28
Gerard <gerardshah@gmail.com>
2024-06-18 18:55:39 +0300

    Advanced - Exam preparation - exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/08 C# Advanced Regular Exam - 18 February 2023/03 SoftUni Kindergarten/Kindergarten.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Child> Registry { get; set; }
+
+        public int ChildrenCount => Registry.Count;
+
+        public bool AddChild(Child child)
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveChild(string childFullName) => Registry.Remove(Registry.FirstOrDefault(x => $"{x.FirstName} {x.LastName}" == childFullName));
+
+        public Child GetChild(string childFullName) => Registry.FirstOrDefault(x => $"{x.FirstName} {x.LastName}" == childFullName);
+
+        public string RegistryReport()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Registered children in {Name}:");
+
+            foreach (var child in Registry.OrderByDescending(x=>x.Age).ThenBy(x=>x.LastName).ThenBy(x=>x.FirstName))
+            {
+                sb.AppendLine(child.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
