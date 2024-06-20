f57d26d00fd87299fdeaa998c66a282117a1f435
Gerard <gerardshah@gmail.com>
2024-06-20 22:48:46 +0300

    Advanced - Exam preparation - exercise added

--- 01 C#/03 Advanced/98 Exam Preparation/C# Advanced Regular Exam - 22 February 2020/02 Re-Volt/Program.cs
+++ 01 C#/03 Advanced/98 Exam Preparation/C# Advanced Regular Exam - 22 February 2020/02 Re-Volt/Program.cs
@@ -85,7 +85,6 @@
 
         private static void ChangePosition(char[,] matrix, string command)
         {
-
             if (command == "up")
             {
                 playerRow--;
---
+++ 01 C#/03 Advanced/98 Exam Preparation/C# Advanced Regular Exam - 22 February 2020/03 Guild/Guild.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            roster = new List<Player>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => roster.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count < Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name) => roster.Remove(roster.FirstOrDefault(x => x.Name == name));
+
+        public void PromotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+
+            if (player != null && player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+
+            if (player != null && player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] arrar = roster.Where(x => x.Class == @class).ToArray();
+
+            roster.RemoveAll(x => x.Class == @class);
+
+            return arrar;
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Players in the guild: {Name}");
+
+            foreach (Player player in roster)
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
