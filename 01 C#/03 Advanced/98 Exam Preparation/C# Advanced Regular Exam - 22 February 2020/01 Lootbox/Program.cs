287267a7afe4118ae10906d5028aa48e554c7c7a
Gerard <gerardshah@gmail.com>
2024-06-20 21:21:36 +0300

    Advanced - Exam preparation - exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/C# Advanced Regular Exam - 22 February 2020/01 Lootbox/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var firstLootBox = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var secondLootBox = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int sumOfItems = 0;
+
+            while (firstLootBox.Any() && secondLootBox.Any())
+            {
+                int firstItem = firstLootBox.Peek();
+                int secondItem = secondLootBox.Pop();
+
+                int sum = firstItem + secondItem;
+
+                if (sum % 2 == 0)
+                {
+                    sumOfItems += sum;
+                    firstLootBox.Dequeue();
+                }
+                else
+                {
+                    firstLootBox.Enqueue(secondItem);
+                }
+            }
+
+            if (!firstLootBox.Any())
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (sumOfItems >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {sumOfItems}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {sumOfItems}");
+            }
+        }
+    }
+}
