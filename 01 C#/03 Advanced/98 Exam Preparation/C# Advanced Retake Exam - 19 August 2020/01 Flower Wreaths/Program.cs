2c868923185ad692a90da50825082eb059f2b90f
Gerard <gerardshah@gmail.com>
2024-06-20 14:22:04 +0300

    Advanced - Exam preparation - exercise added

---
+++ 01 C#/03 Advanced/98 Exam Preparation/C# Advanced Retake Exam - 19 August 2020/01 Flower Wreaths/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int wreathCount = 0;
+            int storedSum = 0;
+
+            while (roses.Any() && lilies.Any())
+            {
+                int rose = roses.Dequeue();
+                int lily = lilies.Pop();
+
+                int sum = rose + lily;
+
+                while (sum >= 15)
+                {
+                    if (sum == 15)
+                    {
+                        wreathCount++;
+                        break;
+                    }
+
+                    sum -= 2;
+                }
+
+                if (sum < 15)
+                {
+                    storedSum += sum;
+                }
+            }
+
+            wreathCount += storedSum / 15;
+
+            if (wreathCount >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreathCount} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreathCount} wreaths more!");
+            }
+        }
+    }
+}
