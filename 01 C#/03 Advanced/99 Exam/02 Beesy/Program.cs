283d7f28652a5add010bb104ff1a8c63139b4b64
Gerard <gerardshah@gmail.com>
2024-06-22 10:19:42 +0300

    Advanced - Exam - exercise added

---
+++ 01 C#/03 Advanced/99 Exam/02 Beesy/Program.cs
@@ -0,0 +1,141 @@
+﻿//09:12
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static int beeRow = -1;
+        static int beeCol = -1;
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[n, n];
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                string currentRow = Console.ReadLine();
+
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    field[row, col] = currentRow[col];
+
+                    if (currentRow[col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                }
+            }
+
+            int energy = 15;
+            int unitsOfNecktar = 0;
+            bool restoreEnergy = true;
+            bool hiveReached = false;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                field[beeRow, beeCol] = '-';
+                energy--;
+
+                if (command == "up")
+                {
+                    if (beeRow == 0)
+                    {
+                        beeRow = field.GetLength(0) - 1;
+                    }
+                    else
+                    {
+                        beeRow--;
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (beeRow == field.GetLength(0) - 1)
+                    {
+                        beeRow = 0;
+                    }
+                    else
+                    {
+                        beeRow++;
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (beeCol == 0)
+                    {
+                        beeCol = field.GetLength(1) - 1;
+                    }
+                    else
+                    {
+                        beeCol--;
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (beeCol == field.GetLength(1) - 1)
+                    {
+                        beeCol = 0;
+                    }
+                    else
+                    {
+                        beeCol++;
+                    }
+                }
+
+                if (char.IsDigit(field[beeRow, beeCol]))
+                {
+                    unitsOfNecktar += field[beeRow, beeCol] - 48;
+                }
+                else if (field[beeRow, beeCol] == 'H')
+                {
+                    hiveReached = true;
+                    field[beeRow, beeCol] = 'B';
+                    break;
+                }
+
+                field[beeRow, beeCol] = 'B';
+
+                if (energy == 0)
+                {
+                    if (restoreEnergy && unitsOfNecktar >= 30)
+                    {
+                        restoreEnergy = false;
+                        energy = unitsOfNecktar - 30;
+                    }
+
+                    if (energy == 0)
+                    {
+                        Console.WriteLine("This is the end! Beesy ran out of energy.");
+                        break;
+                    }
+                }
+            }
+
+            if (hiveReached && unitsOfNecktar >= 30)
+            {
+                Console.WriteLine($"Great job, Beesy! The hive is full. Energy left: {energy}");
+            }
+            else if (hiveReached && unitsOfNecktar < 30)
+            {
+                Console.WriteLine("Beesy did not manage to collect enough nectar.");
+            }
+
+            PrintMatrix(field);
+        }
+
+        private static void PrintMatrix(char[,] wall)
+        {
+            for (int row = 0; row < wall.GetLength(0); row++)
+            {
+                for (int col = 0; col < wall.GetLength(1); col++)
+                {
+                    Console.Write(wall[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+//09:33:14 22.06.2024
