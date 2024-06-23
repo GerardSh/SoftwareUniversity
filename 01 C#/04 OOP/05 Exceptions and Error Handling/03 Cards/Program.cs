bfb084237f156816ab95c6374947331a64fff2fe
Gerard <gerardshah@gmail.com>
2024-06-23 11:44:13 +0300

    OOP - Exceptions and Error Handling - exercise added

--- 01 C#/04 OOP/05 Exceptions and Error Handling/03 Cards/Program.cs
+++ 01 C#/04 OOP/05 Exceptions and Error Handling/03 Cards/Program.cs
@@ -57,3 +57,67 @@
         }
     }
 }
+
+
+
+
+//2
+namespace ConsoleApp
+{
+    public class Card
+    {
+        public Card(string face, string suit)
+        {
+            Face = face;
+            Suit = suit;
+        }
+
+        public string Face { get; set; }
+
+        public string Suit { get; set; }
+
+        public override string ToString()
+        {
+            {
+                return $"[{Face}{(Suit == "S" ? "\u2660" : Suit == "H" ? "\u2665" : Suit == "D" ? "\u2666" : "\u2663")}]";
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<Card> cards = new List<Card>(input.Length);
+
+            foreach (string line in input)
+            {
+                string[] cardElements = line.Split();
+                try
+                {
+                    Card card = CreateCard(cardElements[0], cardElements[1]);
+                    cards.Add(card);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", cards));
+        }
+
+        private static Card CreateCard(string face, string suit)
+        {
+            if (!((face == "10" || face == "2" || face == "3" || face == "4" || face == "5" || face == "6" || face == "7" || face == "8" || face == "9" || face == "J" || face == "Q" || face == "K" || face == "A") &&
+               (suit == "S" || suit == "H" || suit == "D" || suit == "C")))
+            {
+                throw new ArgumentException("Invalid card!");
+            }
+
+            return new Card(face, suit);
+        }
+    }
+}
