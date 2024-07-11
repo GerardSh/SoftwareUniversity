4b5c0f318d04b4368dd8a2f470b319cbcbb6a6a9
Gerard <gerardshah@gmail.com>
2024-07-11 22:15:10 +0300

    OOP - Reflection and Attributes - Lab - exercise added

---
+++ 01 C#/04 OOP/08 Reflection and Attributes/01 Lab/01 Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] nameOfFieldsToInvestigate)
+        {
+            var sb = new StringBuilder();
+
+            Type type = Type.GetType(classToInvestigate);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Instance);
+
+            var classInstance = Activator.CreateInstance(type);
+
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+
+            foreach (var field in fields.Where(x => nameOfFieldsToInvestigate.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 01 C#/04 OOP/08 Reflection and Attributes/01 Lab/01 Stealer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Stealer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
