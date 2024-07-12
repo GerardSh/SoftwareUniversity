0accf2c59e9bf27a7695a23a99fec9ac908a88d9
Gerard <gerardshah@gmail.com>
2024-07-12 22:54:30 +0300

    OOP - Reflection and Attributes - Lab - exercise added

---
+++ 01 C#/04 OOP/08 Reflection and Attributes/01 Lab/04 Collector/Spy.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] nameOfFieldsToInvestigate)
+        {
+            var sb = new StringBuilder();
+
+            Type type = Type.GetType(classToInvestigate);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            var classInstance = Activator.CreateInstance(type);
+
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+
+            foreach (var field in fields.Where(x => nameOfFieldsToInvestigate.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            var sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+
+            var fields = type.GetFields();
+
+            foreach (var field in fields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            var nonPublicMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            foreach (var method in nonPublicMethods)
+            {
+                if (method.Name.Contains("get_"))
+                {
+                    sb.AppendLine($"{method.Name} have to be public!");
+                }
+            }
+
+            var publicMethods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+
+            foreach (var method in publicMethods)
+            {
+                if (method.Name.Contains("set_"))
+                {
+                    sb.AppendLine($"{method.Name} have to be private!");
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            var sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+
+            sb.AppendLine($"All Private Methods of Class: {type.FullName}");
+            sb.AppendLine($"Base Class: {type.BaseType.Name}");
+
+            var privateMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            foreach (var method in privateMethods)
+            {
+                sb.AppendLine($"{method.Name}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string CollectGettersAndSetters(string className)
+        {
+            var sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            var getterMethods = methods.Where(x => x.Name.StartsWith("get_"));
+            var setterMethods = methods.Where(x => x.Name.StartsWith("set_"));
+
+            foreach (var method in getterMethods)
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (var method in setterMethods)
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().FirstOrDefault().ParameterType}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 01 C#/04 OOP/08 Reflection and Attributes/01 Lab/04 Collector/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Stealer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
