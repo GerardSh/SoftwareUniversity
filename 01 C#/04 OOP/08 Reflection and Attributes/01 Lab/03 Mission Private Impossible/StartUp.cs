0574bc34018bc10f409e9ad3488aed529204fdc2
Gerard <gerardshah@gmail.com>
2024-07-12 10:40:30 +0300

    OOP - Reflection and Attributes - Lab - exercise added

---
+++ 01 C#/04 OOP/08 Reflection and Attributes/01 Lab/03 Mission Private Impossible/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Stealer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
