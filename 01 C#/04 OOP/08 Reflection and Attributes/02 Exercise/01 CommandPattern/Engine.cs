ef674d31538b2f18f4615255604a8c78cacd0cb3
Gerard <gerardshah@gmail.com>
2024-07-14 23:11:47 +0300

    OOP - Reflection and Attributes - Exercise - exercise added

---
+++ 01 C#/04 OOP/08 Reflection and Attributes/02 Exercise/01 CommandPattern/Engine.cs
@@ -0,0 +1,19 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ 01 C#/04 OOP/08 Reflection and Attributes/02 Exercise/01 CommandPattern/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    public abstract class Test
+    {
+        public abstract void Tess();
+    }
+
+   public abstract class Test2 : Test
+    {
+        public override void Tess()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+            engine.Run();
+        }
+    }
+}
