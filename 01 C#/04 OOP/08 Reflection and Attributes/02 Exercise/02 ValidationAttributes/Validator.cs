634d00ab04924ea904afa8e0a2c65d08eb725a23
Gerard <gerardshah@gmail.com>
2024-07-15 20:43:14 +0300

    OOP - Reflection and Attributes - Exercise - exercise added

---
+++ 01 C#/04 OOP/08 Reflection and Attributes/02 Exercise/02 ValidationAttributes/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var customAttributes = property.GetCustomAttributes().Where(x => x.GetType().IsSubclassOf(typeof(MyValidationAttribute))).Cast<MyValidationAttribute>().ToArray();
+
+                foreach (var customAttribute in customAttributes)
+                {
+                    bool isValid = customAttribute.IsValid(property.GetValue(obj));
+
+                    if (!isValid)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
