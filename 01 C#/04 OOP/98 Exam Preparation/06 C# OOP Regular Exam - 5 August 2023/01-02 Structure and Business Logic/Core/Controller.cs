a3e3f39418cfac14d210c6efb408f6dcf09b4bee
Gerard <gerardshah@gmail.com>
2024-08-01 20:01:28 +0300

    OOP - Exam preparation - exercise added

---
+++ 01 C#/04 OOP/98 Exam Preparation/06 C# OOP Regular Exam - 5 August 2023/01-02 Structure and Business Logic/Core/Controller.cs
@@ -0,0 +1,141 @@
+﻿using BankLoan.Core.Contracts;
+using BankLoan.Models;
+using BankLoan.Models.Contracts;
+using BankLoan.Repositories;
+using BankLoan.Repositories.Contracts;
+using BankLoan.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Core
+{
+    public class Controller : IController
+    {
+        IRepository<ILoan> loans;
+        IRepository<IBank> banks;
+
+        public Controller()
+        {
+            loans = new LoanRepository();
+            banks = new BankRepository();
+        }
+
+        public string AddBank(string bankTypeName, string name)
+        {
+            IBank bank = null;
+
+            if (bankTypeName == nameof(CentralBank))
+            {
+                bank = new CentralBank(name);
+            }
+            else if (bankTypeName == nameof(BranchBank))
+            {
+                bank = new BranchBank(name);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.BankTypeInvalid);
+            }
+
+            banks.AddModel(bank);
+
+            return string.Format(OutputMessages.BankSuccessfullyAdded, bank.GetType().Name);
+        }
+
+        public string AddClient(string bankName, string clientTypeName, string clientName, string id, double income)
+        {
+            IClient client = null;
+
+            if (clientTypeName == nameof(Student))
+            {
+                client = new Student(clientName, id, income);
+            }
+            else if (clientTypeName == nameof(Adult))
+            {
+                client = new Adult(clientName, id, income);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.ClientTypeInvalid);
+            }
+
+            IBank bank = banks.FirstModel(bankName);
+
+            string bankType = bank.GetType().Name;
+
+            if (clientTypeName == nameof(Student) && bankType == nameof(CentralBank)
+                || clientTypeName == nameof(Adult) && bankType == nameof(BranchBank))
+            {
+                return OutputMessages.UnsuitableBank;
+            }
+
+            bank.AddClient(client);
+
+            return string.Format(OutputMessages.ClientAddedSuccessfully, clientTypeName, bankName);
+        }
+
+        public string AddLoan(string loanTypeName)
+        {
+            ILoan loan = null;
+
+            if (loanTypeName == nameof(StudentLoan))
+            {
+                loan = new StudentLoan();
+            }
+            else if (loanTypeName == nameof(MortgageLoan))
+            {
+                loan = new MortgageLoan();
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.LoanTypeInvalid);
+            }
+
+            loans.AddModel(loan);
+
+            return string.Format(OutputMessages.LoanSuccessfullyAdded, loan.GetType().Name);
+        }
+
+        public string FinalCalculation(string bankName)
+        {
+            IBank bank = banks.FirstModel(bankName);
+
+            double funds = bank.Clients.Sum(x => x.Income) + bank.Loans.Sum(x => x.Amount);
+          
+            return string.Format(OutputMessages.BankFundsCalculated, bankName, funds.ToString("F2"));
+        }
+
+        public string ReturnLoan(string bankName, string loanTypeName)
+        {
+            ILoan loan = loans.FirstModel(loanTypeName);
+
+            if (loan == null)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.MissingLoanFromType, loanTypeName));
+            }
+
+            loans.RemoveModel(loan);
+
+            IBank bank = banks.FirstModel(bankName);
+
+            bank.AddLoan(loan);
+
+            return string.Format(OutputMessages.LoanReturnedSuccessfully, loanTypeName, bankName);
+        }
+
+        public string Statistics()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var bank in banks.Models)
+            {
+                sb.AppendLine(bank.GetStatistics());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
