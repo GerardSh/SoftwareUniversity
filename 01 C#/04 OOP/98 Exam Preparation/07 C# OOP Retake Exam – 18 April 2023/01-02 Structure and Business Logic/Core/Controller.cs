032a8e83eb29e9c49d7ebe032f3e0bced67184a4
Gerard <gerardshah@gmail.com>
2024-08-02 13:36:48 +0300

    OOP - Exam preparation - exercise added

---
+++ 01 C#/04 OOP/98 Exam Preparation/07 C# OOP Retake Exam – 18 April 2023/01-02 Structure and Business Logic/Core/Controller.cs
@@ -0,0 +1,171 @@
+﻿using EDriveRent.Core.Contracts;
+using EDriveRent.Models;
+using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories;
+using EDriveRent.Repositories.Contracts;
+using EDriveRent.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Core
+{
+    public class Controller : IController
+    {
+        IRepository<IUser> users;
+        IRepository<IVehicle> vehicles;
+        IRepository<IRoute> routes;
+
+        public Controller()
+        {
+            users = new UserRepository();
+            vehicles = new VehicleRepository();
+            routes = new RouteRepository();
+        }
+
+        public string AllowRoute(string startPoint, string endPoint, double length)
+        {
+            int routeId = routes.GetAll().Count() + 1;
+
+            IRoute route = new Route(startPoint, endPoint, length, routeId);
+
+            List<IRoute> allRoutes = routes.GetAll().ToList();
+
+            if (allRoutes.Any(x => x.StartPoint == startPoint && x.EndPoint == endPoint && x.Length == length))
+            {
+                return string.Format(OutputMessages.RouteExisting, startPoint, endPoint, length);
+            }
+            else if (allRoutes.Any(x => x.StartPoint == startPoint && x.EndPoint == endPoint && x.Length < length))
+            {
+                return string.Format(OutputMessages.RouteIsTooLong, startPoint, endPoint);
+            }
+
+            routes.AddModel(route);
+
+            foreach (var item in allRoutes)
+            {
+                if (item.StartPoint == startPoint && item.EndPoint == endPoint && item.Length > length)
+                {
+                    item.LockRoute();
+                }
+            }
+
+            return string.Format(OutputMessages.NewRouteAdded, startPoint, endPoint, length);
+        }
+
+        public string MakeTrip(string drivingLicenseNumber, string licensePlateNumber, string routeId, bool isAccidentHappened)
+        {
+            IUser user = users.FindById(drivingLicenseNumber);
+            IVehicle vehicle = vehicles.FindById(licensePlateNumber);
+            IRoute route = routes.FindById(routeId);
+
+            if (user.IsBlocked)
+            {
+                return string.Format(OutputMessages.UserBlocked, drivingLicenseNumber);
+            }
+
+            if (vehicle.IsDamaged)
+            {
+                return string.Format(OutputMessages.VehicleDamaged, licensePlateNumber);
+            }
+
+            if (route.IsLocked)
+            {
+                return string.Format(OutputMessages.RouteLocked, routeId);
+            }
+
+            vehicle.Drive(route.Length);
+
+            if (isAccidentHappened)
+            {
+                vehicle.ChangeStatus();
+                user.DecreaseRating();
+            }
+            else
+            {
+                user.IncreaseRating();
+            }
+
+            return vehicle.ToString();
+        }
+
+        public string RegisterUser(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            IUser user = users.FindById(drivingLicenseNumber);
+
+            if (user != null)
+            {
+                return string.Format(OutputMessages.UserWithSameLicenseAlreadyAdded, drivingLicenseNumber);
+            }
+
+            user = new User(firstName, lastName, drivingLicenseNumber);
+
+            users.AddModel(user);
+
+            return string.Format(OutputMessages.UserSuccessfullyAdded, firstName, lastName, drivingLicenseNumber);
+        }
+
+        public string RepairVehicles(int count)
+        {
+            List<IVehicle> damagedVehicles = vehicles.GetAll().Where(x => x.IsDamaged).OrderBy(x => x.Brand).ThenBy(x => x.Model).ToList();
+
+            if (damagedVehicles.Count < count)
+            {
+                count = damagedVehicles.Count;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                damagedVehicles[i].ChangeStatus();
+                damagedVehicles[i].Recharge();
+            }
+
+            return string.Format(OutputMessages.RepairedVehicles, count);
+        }
+
+        public string UploadVehicle(string vehicleType, string brand, string model, string licensePlateNumber)
+        {
+            IVehicle vehicle = null;
+
+            if (vehicleType == nameof(PassengerCar))
+            {
+                vehicle = new PassengerCar(brand, model, licensePlateNumber);
+            }
+            else if (vehicleType == nameof(CargoVan))
+            {
+                vehicle = new CargoVan(brand, model, licensePlateNumber);
+            }
+            else
+            {
+                return string.Format(OutputMessages.VehicleTypeNotAccessible, vehicleType);
+            }
+
+            if (vehicles.FindById(licensePlateNumber) != null)
+            {
+                return string.Format(OutputMessages.LicensePlateExists, licensePlateNumber);
+            }
+
+            vehicles.AddModel(vehicle);
+
+            return string.Format(OutputMessages.VehicleAddedSuccessfully, brand, model, licensePlateNumber);
+        }
+
+        public string UsersReport()
+        {
+            var sb = new StringBuilder();
+
+            List<IUser> sortedUsers = users.GetAll().OrderByDescending(x => x.Rating).ThenBy(x => x.LastName).ThenBy(x => x.FirstName).ToList();
+
+            sb.AppendLine("*** E-Drive-Rent ***");
+
+            foreach (var user in sortedUsers)
+            {
+                sb.AppendLine(user.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
