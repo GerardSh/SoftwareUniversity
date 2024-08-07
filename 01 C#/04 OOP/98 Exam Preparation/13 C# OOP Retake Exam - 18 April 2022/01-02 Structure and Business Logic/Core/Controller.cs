f77385ae32e56dcd989337a47e236c16ff3c6084
Gerard <gerardshah@gmail.com>
2024-08-07 13:01:08 +0300

    OOP - Exam preparation - exercise added

---
+++ 01 C#/04 OOP/98 Exam Preparation/13 C# OOP Retake Exam - 18 April 2022/01-02 Structure and Business Logic/Core/Controller.cs
@@ -0,0 +1,136 @@
+﻿using Heroes.Core.Contracts;
+using Heroes.Models.Contracts;
+using Heroes.Models.Heroes;
+using Heroes.Repositories;
+using Heroes.Repositories.Contracts;
+using Heroes.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Heroes.Core
+{
+    public class Controller : IController
+    {
+        IRepository<IHero> heroes;
+        IRepository<IWeapon> weapons;
+
+        public Controller()
+        {
+            heroes = new HeroRepository();
+            weapons = new WeaponRepository();
+        }
+
+        public string AddWeaponToHero(string weaponName, string heroName)
+        {
+            IHero hero = heroes.FindByName(heroName);
+
+            if (hero == null)
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.HeroDoesNotExist, heroName));
+            }
+
+            IWeapon weapon = weapons.FindByName(weaponName);
+
+            if (weapon == null)
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.WeaponDoesNotExist, weaponName));
+            }
+
+            if (hero.Weapon != null)
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.HeroAlreadyHasWeapon, heroName));
+            }
+
+            hero.AddWeapon(weapon);
+
+            return string.Format(OutputMessages.WeaponAddedToHero, heroName, weapon.GetType().Name.ToLower());
+        }
+
+        public string CreateHero(string type, string name, int health, int armour)
+        {
+            if (heroes.FindByName(name) != null)
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.HeroAlreadyExist, name));
+            }
+
+            IHero hero = null;
+
+            if (type == nameof(Knight))
+            {
+                hero = new Knight(name, health, armour);
+            }
+            else if (type == nameof(Barbarian))
+            {
+                hero = new Barbarian(name, health, armour);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.HeroTypeIsInvalid));
+            }
+
+            heroes.Add(hero);
+
+            if (type == nameof(Knight))
+            {
+                return string.Format(OutputMessages.SuccessfullyAddedKnight, name);
+            }
+
+            return string.Format(OutputMessages.SuccessfullyAddedBarbarian, name);
+        }
+
+
+        public string CreateWeapon(string type, string name, int durability)
+        {
+            if (weapons.FindByName(name) != null)
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.WeaponAlreadyExists, name));
+            }
+
+            IWeapon weapon = null;
+
+            if (type == nameof(Mace))
+            {
+                weapon = new Mace(name, durability);
+            }
+            else if (type == nameof(Claymore))
+            {
+                weapon = new Claymore(name, durability);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(OutputMessages.WeaponTypeIsInvalid));
+            }
+
+            weapons.Add(weapon);
+
+            return string.Format(OutputMessages.WeaponAddedSuccessfully, type.ToLower(), name);
+        }
+
+        public string HeroReport()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var hero in heroes.Models.OrderBy(x => x.GetType().Name).ThenByDescending(x => x.Health).ThenBy(x => x.Name))
+            {
+                sb.AppendLine($"{hero.GetType().Name}: {hero.Name}");
+                sb.AppendLine($"--Health: {hero.Health}");
+                sb.AppendLine($"--Armour: {hero.Armour}");
+                sb.AppendLine($"--Weapon: {(hero.Weapon != null ? hero.Weapon.Name : "Unarmed")}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string StartBattle()
+        {
+            Map map = new Map();
+
+            List<IHero> heroesWithWeapons = heroes.Models.Where(x => x.Weapon != null).ToList();
+
+            return map.Fight(heroesWithWeapons);
+        }
+    }
+}
