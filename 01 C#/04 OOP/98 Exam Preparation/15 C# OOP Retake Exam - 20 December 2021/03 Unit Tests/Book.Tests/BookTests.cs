cfb1a2638529bb5a41acc02a02a8210a17680f75
Gerard <gerardshah@gmail.com>
2024-08-08 22:30:50 +0300

    OOP - Exam preparation - exercise added

---
+++ 01 C#/04 OOP/98 Exam Preparation/15 C# OOP Retake Exam - 20 December 2021/03 Unit Tests/Book.Tests/BookTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Book.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public class BookTests
+    {
+        Book book;
+
+        [SetUp]
+        public void Setup()
+        {
+            book = new Book("name", "author");
+        }
+
+        [Test]
+        public void ConstructorShouldCreateObject()
+        {
+            Assert.IsNotNull(book);
+            Assert.Throws<ArgumentException>(() => new Book(string.Empty, "author"));
+            Assert.Throws<ArgumentException>(() => new Book(null, "author"));
+            Assert.That(book.BookName, Is.EqualTo("name"));
+
+            Assert.Throws<ArgumentException>(() => new Book("name", string.Empty));
+            Assert.Throws<ArgumentException>(() => new Book("name", null));
+            Assert.That(book.Author, Is.EqualTo("author"));
+
+            Assert.That(book.FootnoteCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AddFootnoteMethodShouldWorkCorrectly()
+        {
+            Assert.That(book.FootnoteCount, Is.EqualTo(0));
+
+            book.AddFootnote(1, "text");
+
+            Assert.That(book.FootnoteCount, Is.EqualTo(1));
+
+            Assert.Throws<InvalidOperationException>(() => book.AddFootnote(1, "text"));
+
+            book.AddFootnote(2, "text");
+
+            Assert.That(book.FootnoteCount, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void FindFootnoteMethodShouldWorkCorrectly()
+        {
+            book.AddFootnote(1, "text");
+
+            Assert.Throws<InvalidOperationException>(() => book.FindFootnote(2));
+
+            string result = book.FindFootnote(1);
+
+            Assert.That(result, Is.EqualTo($"Footnote #1: text"));
+        }
+
+        [Test]
+        public void AlterFootnoteMethodShouldWorkCorrectly()
+        {
+            book.AddFootnote(1, "text");
+
+            Assert.Throws<InvalidOperationException>(() => book.AlterFootnote(2, "new text"));
+
+            book.AlterFootnote(1, "new text");
+
+            string result = book.FindFootnote(1);
+
+            Assert.That(result, Is.EqualTo($"Footnote #1: new text"));
+        }
+    }
+}
