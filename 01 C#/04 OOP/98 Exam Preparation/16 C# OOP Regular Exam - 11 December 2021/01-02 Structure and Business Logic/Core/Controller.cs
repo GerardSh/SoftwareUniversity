be49c5346d87d34fdd7c2dd58473195c23429d33
Gerard <gerardshah@gmail.com>
2024-08-21 10:01:53 +0300

    OOP - Exam preparation - exercise added

---
+++ 01 C#/04 OOP/98 Exam Preparation/16 C# OOP Regular Exam - 11 December 2021/01-02 Structure and Business Logic/Core/Controller.cs
@@ -0,0 +1,150 @@
+﻿using Gym.Core.Contracts;
+using Gym.Models.Athletes;
+using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms;
+using Gym.Models.Gyms.Contracts;
+using Gym.Repositories;
+using Gym.Repositories.Contracts;
+using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym.Core
+{
+    public class Controller : IController
+    {
+        IRepository<IEquipment> equipment;
+        List<IGym> gym;
+
+        public Controller()
+        {
+            equipment = new EquipmentRepository();
+            gym = new List<IGym>();
+        }
+
+        public string AddAthlete(string gymName, string athleteType, string athleteName, string motivation, int numberOfMedals)
+        {
+            IAthlete athlete = null;
+
+            if (athleteType == nameof(Boxer))
+            {
+                athlete = new Boxer(athleteName, motivation, numberOfMedals);
+            }
+            else if (athleteType == nameof(Weightlifter))
+            {
+                athlete = new Weightlifter(athleteName, motivation, numberOfMedals);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidAthleteType);
+            }
+
+            IGym gym = this.gym.FirstOrDefault(x => x.Name == gymName);
+
+
+            if (athleteType == nameof(Boxer) && gym.GetType().Name == nameof(WeightliftingGym)
+                || athleteType == nameof(Weightlifter) && gym.GetType().Name == nameof(BoxingGym))
+            {
+                return string.Format(OutputMessages.InappropriateGym);
+            }
+
+            gym.AddAthlete(athlete);
+
+            return string.Format(OutputMessages.EntityAddedToGym, athleteType, gymName);
+        }
+
+        public string AddEquipment(string equipmentType)
+        {
+            IEquipment equipment = null;
+
+            if (equipmentType == nameof(BoxingGloves))
+            {
+                equipment = new BoxingGloves();
+            }
+            else if (equipmentType == nameof(Kettlebell))
+            {
+                equipment = new Kettlebell();
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidEquipmentType);
+            }
+
+            this.equipment.Add(equipment);
+
+            return string.Format(OutputMessages.SuccessfullyAdded, equipmentType);
+        }
+
+        public string AddGym(string gymType, string gymName)
+        {
+            IGym gym = null;
+
+            if (gymType == nameof(BoxingGym))
+            {
+                gym = new BoxingGym(gymName);
+            }
+            else if (gymType == nameof(WeightliftingGym))
+            {
+                gym = new WeightliftingGym(gymName);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidGymType);
+            }
+
+            this.gym.Add(gym);
+
+            return string.Format(OutputMessages.SuccessfullyAdded, gymType);
+        }
+
+        public string EquipmentWeight(string gymName)
+        {
+            double totalWeight = this.gym.FirstOrDefault(x => x.Name == gymName).EquipmentWeight;
+
+            return string.Format(OutputMessages.EquipmentTotalWeight, gymName, totalWeight);
+        }
+
+        public string InsertEquipment(string gymName, string equipmentType)
+        {
+            IEquipment equipment = this.equipment.FindByType(equipmentType);
+
+            if (equipment == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentEquipment, equipmentType));
+            }
+
+            this.equipment.Remove(equipment);
+
+            IGym gym = this.gym.FirstOrDefault(x => x.Name == gymName);
+
+            gym.AddEquipment(equipment);
+
+            return string.Format(OutputMessages.EntityAddedToGym, equipmentType, gymName);
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var gym in this.gym)
+            {
+                sb.AppendLine(gym.GymInfo());
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string TrainAthletes(string gymName)
+        {
+            IGym gym = this.gym.FirstOrDefault(x => x.Name == gymName);
+
+            gym.Exercise();
+
+            return string.Format(OutputMessages.AthleteExercise, gym.Athletes.Count);
+        }
+    }
+}
--- 01 C#/04 OOP/98 Exam Preparation/16 C# OOP Regular Exam - 11 December 2021/03 Unit Tests/Gyms.Tests/GymsTests.cs
+++ 01 C#/04 OOP/98 Exam Preparation/16 C# OOP Regular Exam - 11 December 2021/03 Unit Tests/Gyms.Tests/GymsTests.cs
@@ -1,7 +1,118 @@
-﻿namespace Gyms.Tests
+﻿using NUnit.Framework;
+using System;
+
+namespace Gyms.Tests
 {
     public class GymsTests
     {
-        // Implement unit tests here
+        Athlete athlete;
+        Gym gym;
+
+        [SetUp]
+        public void Setup()
+        {
+            athlete = new Athlete("name");
+            gym = new Gym("name", 10);
+        }
+
+        [Test]
+        public void ConstructorsShouldCreateObjectsCorrectly()
+        {
+            Assert.NotNull(athlete);
+            Assert.That(athlete.FullName, Is.EqualTo("name"));
+            Assert.That(athlete.IsInjured, Is.EqualTo(false));
+
+            Assert.NotNull(gym);
+            Assert.That(gym.Name, Is.EqualTo("name"));
+            Assert.That(gym.Capacity, Is.EqualTo(10));
+            Assert.That(gym.Count, Is.EqualTo(0));
+
+            Assert.Throws<ArgumentNullException>(() => new Gym(null, 10));
+            Assert.Throws<ArgumentNullException>(() => new Gym("", 10));
+            Assert.Throws<ArgumentException>(() => new Gym("name", -1));
+        }
+
+        [Test]
+        public void AddMethodShouldWorkCorrectly()
+        {
+            Athlete athlete2 = new Athlete("name2");
+            Athlete athlete3 = new Athlete("name3");
+            gym = new Gym("name", 2);
+
+            Assert.That(gym.Count, Is.EqualTo(0));
+
+            gym.AddAthlete(athlete);
+            gym.AddAthlete(athlete2);
+
+            Assert.That(gym.Count, Is.EqualTo(2));
+
+            Assert.Throws<InvalidOperationException>(() => gym.AddAthlete(athlete3));
+
+            Assert.That(gym.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void RemoveMethodShouldWorkCorrectly()
+        {
+            Athlete athlete2 = new Athlete("name2");
+            Athlete athlete3 = new Athlete("name3");
+
+            gym.AddAthlete(athlete);
+            gym.AddAthlete(athlete2);
+            gym.AddAthlete(athlete3);
+
+            Assert.That(gym.Count, Is.EqualTo(3));
+
+            Assert.Throws<InvalidOperationException>(() => gym.RemoveAthlete("name4"));
+
+            Assert.That(gym.Count, Is.EqualTo(3));
+
+            gym.RemoveAthlete("name3");
+
+            Assert.That(gym.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void InjureAthleteMethodShouldWorkCorrectly()
+        {
+            Athlete athlete2 = new Athlete("name2");
+            Athlete athlete3 = new Athlete("name3");
+
+            gym.AddAthlete(athlete);
+            gym.AddAthlete(athlete2);
+            gym.AddAthlete(athlete3);
+
+            Assert.Throws<InvalidOperationException>(() => gym.InjureAthlete("name4"));
+
+            Assert.That(athlete2.IsInjured, Is.False);
+
+            Athlete result = gym.InjureAthlete("name2");
+
+            Assert.That(athlete2.IsInjured, Is.True);
+            Assert.That(result.IsInjured, Is.True);
+            Assert.That(result, Is.EqualTo(athlete2));
+        }
+
+        [Test]
+        public void ReportMethodShouldWorkCorrectly()
+        {
+            Athlete athlete2 = new Athlete("name2");
+            Athlete athlete3 = new Athlete("name3");
+            
+            gym.AddAthlete(athlete);
+            gym.AddAthlete(athlete2);
+            gym.AddAthlete(athlete3);
+            gym.InjureAthlete("name3");
+
+            string result = gym.Report();
+
+            Assert.That(result, Is.EqualTo($"Active athletes at name: name, name2"));
+
+            gym = new Gym("name", 10);
+
+            result = gym.Report();
+
+            Assert.That(result, Is.EqualTo($"Active athletes at name: "));
+        }
     }
 }
