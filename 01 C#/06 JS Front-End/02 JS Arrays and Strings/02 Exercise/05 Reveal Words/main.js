7332fa7f6fe8c6149bd25ed80f6b85f6f14bc889
Gerard <gerardshah@gmail.com>
2024-11-02 01:34:34 +0200

    JS Front-End - JS Arrays and Strings - Exercise - exercise added

---
+++ 01 C#/06 JS Front-End/02 JS Arrays and Strings/02 Exercise/05 Reveal Words/main.js
@@ -0,0 +1,30 @@
+function solve(words, text) {
+    let wordsArray = words.split(', ');
+
+    for (let word of wordsArray) {
+        text = text.replaceAll(new RegExp(`(^|\\s)\\*{${word.length}}(?=\\s|$)`, 'g'), `$1${word}`);
+    }
+
+    console.log(text);
+}
+
+
+
+
+//2
+function solve(words, text) {
+    let wordsArray = words.split(', ');
+    let textArray = text.split(' ');
+
+    for (let word of wordsArray) {
+        for (let i = 0; i < textArray.length; i++) {
+            if (textArray[i] == '*'.repeat(word.length)) {
+                textArray[i] = word;
+            }
+        }
+    }
+
+    console.log(textArray.join(' '));
+}
+
+solve('great, learning', 'softuni is ***** place for ******** new programming languages');
