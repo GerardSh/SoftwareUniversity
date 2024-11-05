9f743fd8519cd341033eeaae43b4b009ff5a93fc
Gerard <gerardshah@gmail.com>
2024-11-05 23:54:54 +0200

    JS Front-End - Functions and Statements - Lab - exercise added

--- 01 C#/06 JS Front-End/03 Functions and Statements/01 Lab/02 Math Power/main.js
+++ 01 C#/06 JS Front-End/03 Functions and Statements/01 Lab/02 Math Power/main.js
@@ -1,45 +1,41 @@
-function solve(number, power) {
-    console.log(mathPower(number, power));
+function mathPower(number, power) {
+    let result = number;
 
-    function mathPower(number, power) {
-        let result = number;
-
-        for (let i = 1; i < power; i++) {
-            result *= number;
-        }
-
-        return result;
+    for (let i = 1; i < power; i++) {
+        result *= number;
     }
+
+    console.log(result);
 }
 
 
 
 
 //2
-function solve(base, power) {
-    console.log(mathPower(base, power));
-
-    function mathPower(base, power, result = 1, i = 0) {
-        if (i == power) {
-            return result;
-        }
-
-        return mathPower(base, power, result * base, ++i);
+function mathPower(base, power, result = 1, i = 0) {
+    if (i == power) {
+        return result;
     }
+
+    return mathPower(base, power, result * base, ++i);
 }
 
 
 
 
 //3
-function solve(base, power) {
-    console.log(mathPower(base, power));
-
-    function mathPower(base, power) {
-        if (power === 1) {
-            return base;
-        }
-
-        return base * mathPower(base, power - 1);
+function mathPower(base, power) {
+    if (power === 1) {
+        return base;
     }
+
+    return base * mathPower(base, power - 1);
+}
+
+
+
+
+//4
+function mathPower(base, power) {
+     console.log(base ** power);
 }
--- 01 C#/06 JS Front-End/03 Functions and Statements/01 Lab/03 Repeat String/main.js
+++ 01 C#/06 JS Front-End/03 Functions and Statements/01 Lab/03 Repeat String/main.js
@@ -1,41 +1,29 @@
-function solve(text, repeatCount) {
-    console.log(repeat(text, repeatCount));
+function repeat(text, repeatCount) {
+    let result = text;
 
-    function repeat(text, repeatCount) {
-        let result = text;
-
-        for (let i = 1; i < repeatCount; i++) {
-            result += text;
-        }
-
-        return result;
+    for (let i = 1; i < repeatCount; i++) {
+        result += text;
     }
+
+   console.log(result);      
 }
 
 
 
 
 //2
-function solve(text, repeatCount) {
-    console.log(repeat(text, repeatCount));
-
-    function repeat(text, repeatCount) {
-        return text.repeat(repeatCount);
-    }
+function repeat(text, repeatCount) {
+    console.log(text.repeat(repeatCount));
 }
 
 
 
 
 //3
-function solve(text, repeatCounter) {
-    console.log(repeat(text, repeatCounter));
-
-    function repeat(text, repeatCounter) {
-        if (repeatCounter == 1) {
-            return text
-        }
-
-        return text += repeat(text, --repeatCounter);
+function repeat(text, repeatCounter) {
+    if (repeatCounter == 1) {
+        return text
     }
+
+    return text += repeat(text, --repeatCounter);
 }
