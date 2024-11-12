d0a90d7801b5665cb907a59f8936f40ab79d269e
Gerard <gerardshah@gmail.com>
2024-11-12 22:11:00 +0200

    JS Front-End - Objects and Classes - Lab - exercise added

--- 01 C#/06 JS Front-End/04 Objects and Classes/01 Lab/02 City/main.js
+++ 01 C#/06 JS Front-End/04 Objects and Classes/01 Lab/02 City/main.js
@@ -5,3 +5,15 @@ function solve(city) {
         console.log(`${property[0]} -> ${property[1]}`);
     }
 }
+
+
+
+
+//2
+function solve(city) {
+    let cityKeys = Object.keys(city);
+
+    for (let key of cityKeys) {
+        console.log(`${key} -> ${city[key]}`);
+    }
+}
