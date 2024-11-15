a043352388d6c6abe467ff2cf10c77cbc00d7b2d
Gerard <gerardshah@gmail.com>
2024-11-15 19:44:08 +0200

    JS Front-End - Objects and Classes - Lab - exercise added

--- 01 C#/06 JS Front-End/04 Objects and Classes/01 Lab/08 Cats/main.js
+++ 01 C#/06 JS Front-End/04 Objects and Classes/01 Lab/08 Cats/main.js
@@ -15,3 +15,25 @@ function solve(cats) {
         new Cat(name, age).meow();
     }
 }
+
+
+
+
+//2
+function solve(cats) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    cats
+        .map(x => x.split(' '))
+        .map(([name, age]) => new Cat(name, age))
+        .forEach(x => x.meow())
+}
