7b87d14bd7917b47810d386caeb4ee9357d89c8b
Gerard <gerardshah@gmail.com>
2024-11-17 19:05:53 +0200

    JS Front-End - Objects and Classes - Exercise - exercise added

--- 01 C#/06 JS Front-End/04 Objects and Classes/02 Exercise/03 Store Provision/main.js
+++ 01 C#/06 JS Front-End/04 Objects and Classes/02 Exercise/03 Store Provision/main.js
@@ -32,3 +32,24 @@ function solve(firstInput, secondInput) {
         return products;
     }
 }
+
+
+
+
+//2
+function solve(firstInput, secondInput) {
+    let stock = [...firstInput, ...secondInput].reduce((stock, product, index, inputArray) => {
+        if (index % 2 == 0) {
+
+            if (!stock[product]) {
+                stock[product] = 0;
+            }
+
+            stock[product] += Number(inputArray[index + 1]);
+        }
+
+        return stock;
+    }, {});
+
+    Object.entries(stock).forEach(([key, value]) => console.log(`${key} -> ${value}`));
+}
