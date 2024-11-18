ab5417484e55789e9e21e4e82ba37ad7802d28aa
Gerard <gerardshah@gmail.com>
2024-11-18 14:25:25 +0200

    JS Front-End - Objects and Classes - More Exercise - exercise added

---
+++ 01 C#/06 JS Front-End/04 Objects and Classes/03 More Exercise/02 Class Storage/main.js
@@ -0,0 +1,24 @@
+function solve(input) {
+    let catalog = {};
+
+    for (product of input) {
+        const productDetails = product.split(' : ');
+        const productName = productDetails[0];
+        const productPrice = Number(productDetails[1]);
+        const productFirstLetter = productName[0];
+
+        if (!catalog[productFirstLetter]) {
+            catalog[productFirstLetter] = {};
+        }
+
+        catalog[productFirstLetter][productName] = productPrice;
+    }
+
+    for (let [key, value] of Object.entries(catalog).sort((a, b) => a[0].localeCompare(b[0]))) {
+        console.log(key);
+
+        for (let [name, price] of Object.entries(value).sort((a, b) => a[0].localeCompare(b[0]))) {
+            console.log(`  ${name}: ${price}`);
+        }
+    }
+}
