1306ef7f429684975d5ddf085984ad3f54cf2890
Gerard <gerardshah@gmail.com>
2024-11-22 23:41:51 +0200

    JS Front-End - DOM Introduction - Lab - demo added

---
+++ 01 C#/06 JS Front-End/05 DOM Introduction/01 Lab/00 Demo/script2.js
@@ -0,0 +1,6 @@
+const movieListElement = document.getElementById('movie-list');
+const headingElements = document.getElementsByClassName('heading');
+const movieListElements = document.getElementsByTagName('li');
+const favoriteMovieElement = document.querySelector('body > ul > li:first-child');
+const allMovieElements = document.querySelectorAll('#movie-list li')
+const test = document.querySelector('li[id =Ink]');
