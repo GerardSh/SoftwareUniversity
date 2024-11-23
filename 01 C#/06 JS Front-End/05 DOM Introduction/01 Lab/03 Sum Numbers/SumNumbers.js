4c04e7bf711621820f2d9fb1717acf012b39d5f8
Gerard <gerardshah@gmail.com>
2024-11-23 23:59:41 +0200

    JS Front-End - Objects and Classes - Exercise - exercise added

---
+++ 01 C#/06 JS Front-End/05 DOM Introduction/01 Lab/03 Sum Numbers/SumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+    const num1Element = document.getElementById('num1');
+    const num2Element = document.getElementById('num2');
+    let sum3SumElement = document.getElementById('sum');
+
+    sum3SumElement.value = Number(num1Element.value) + Number(num2Element.value);
+}
