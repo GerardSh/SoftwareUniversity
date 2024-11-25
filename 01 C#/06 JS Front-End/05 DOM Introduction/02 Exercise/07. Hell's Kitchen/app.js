c75710a08b93f15e043902850c2de9a0dc41c5ad
Gerard <gerardshah@gmail.com>
2024-11-25 11:51:30 +0200

    JS Front-End - DOM Introduction - Exercise - exercise added

--- 01 C#/06 JS Front-End/05 DOM Introduction/02 Exercise/07. Hell's Kitchen/app.js
+++ 01 C#/06 JS Front-End/05 DOM Introduction/02 Exercise/07. Hell's Kitchen/app.js
@@ -3,12 +3,46 @@ function solve() {
 
    function onClick() {
       let input = document.getElementsByTagName('textarea')[0].value;
+      const restaurantsInfo = JSON.parse(input);
 
+      const restaurants = [];
 
-      input = input.replaceAll('["', '');
-      input = input.replaceAll('"]', '');
+      for (const currentRestaurant of restaurantsInfo) {
+         const [restaurantName, workers] = currentRestaurant.split(' - ');
 
-      const arrayInput = input.split('","')
-      console.log(arrayInput);
+         let restaurant = restaurants.find(x => x.name == restaurantName);
+
+         if (!restaurant) {
+            restaurant = {
+               name: restaurantName,
+               workers: [],
+
+               averageSalary() {
+                  return (this.workers.reduce((aggr, currentWorker) => aggr + currentWorker.payment, 0) / restaurant.workers.length).toFixed(2);
+               },
+               highestSalary() {
+                  return this.workers.sort((a, b) => b.payment - a.payment)[0].payment.toFixed(2);
+               }
+            }
+
+            restaurants.push(restaurant);
+         }
+
+         for (const worker of workers.split(', ')) {
+            const [name, payment] = worker.split(' ');
+
+            restaurant.workers.push({ name, payment: Number(payment) });
+         }
+      }
+
+      const bestRestaurant = restaurants.sort((a, b) => b.averageSalary() - a.averageSalary())[0];
+
+      bestRestaurant.workers.sort((a, b) => b.payment - a.payment);
+
+      const bestRestaurantWorkers = bestRestaurant.workers.map(x => `Name: ${x.name} With Salary: ${x.payment}`);
+      const bestRestaurantInformation = `Name: ${bestRestaurant.name} Average Salary: ${bestRestaurant.averageSalary()} Best Salary: ${bestRestaurant.highestSalary()}`
+
+      document.getElementById('bestRestaurant').children[2].textContent = bestRestaurantInformation;
+      document.getElementById('workers').children[2].textContent = bestRestaurantWorkers.join(' ');
    }
 }
