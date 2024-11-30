562356f61d6ba7641d8d9b21f5b4cb67d0a40d3d
Gerard <gerardshah@gmail.com>
2024-11-30 23:59:31 +0200

    JS Front-End - DOM Manipulation and Events - Exercise - exercise added

---
+++ 01 C#/06 JS Front-End/06 DOM Manipulation and Events/02 Exercise/04 Fill-Dropdown/fill-dropdown.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', solve);
+
+function solve() {
+    document.querySelector('input[type="submit"').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const textInputEl = document.getElementById('newItemText');
+        const valueInputEl = document.getElementById('newItemValue');
+        const menuEl = document.getElementById('menu');
+
+        const optionEl = document.createElement('option');
+        optionEl.textContent = textInputEl.value + valueInputEl.value;
+
+        menuEl.append(optionEl);
+
+        textInputEl.value = '';
+        valueInputEl.value = '';
+    });
+}
