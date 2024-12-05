f46b4f7fe0ea76821f3616fbdb87a2e56e0a4b4f
Gerard <gerardshah@gmail.com>
2024-12-05 11:32:59 +0200

    Fundamentals - Retake Exam - exercise added

---
+++ 01 C#/97 Retakes/01 Fundamentals/02/02 Automobile Pairing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main()
+    {
+        string input;
+
+        while ((input = Console.ReadLine()) != "Done")
+        {
+            var match = Regex.Match(input, @"<([A-Z][a-zA-Z ]+)>.{0,}&&([A-Z]{2}[\d]{4}[A-Z]{2})&{2}");
+
+            if (!match.Success)
+            {
+                continue;
+            }
+
+            var carModel = match.Groups[1].Value;
+            var carNumer = match.Groups[2].Value;
+
+            Console.WriteLine($"Linked car {carModel} with number {carNumer}!");
+        }
+    }
+}
+
+// Created on: 05 / 12 / 2024 09:23:22
