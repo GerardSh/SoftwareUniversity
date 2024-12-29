b64990cab442639fa68a8ff720e05e798019e0e1
Gerard <gerardshah@gmail.com>
2024-12-29 23:59:00 +0200

    MS SQL - Databases Introduction - exercise added

--- 01 C#/07 MS SQL/01 Databases Introduction/01 Exercise/18 Basic Insert.sql
+++ 01 C#/07 MS SQL/01 Databases Introduction/01 Exercise/18 Basic Insert.sql
@@ -13,9 +13,6 @@ INSERT INTO Departments VALUES
 	('Software Development'),
 	('Quality Assurance')
 
-	INSERT INTO Addresses VALUES
-	('test1', 1)
-
 --•	Employees:
 --Name	                       Job Title	       Department           	Hire Date	   Salary
 --Ivan Ivanov Ivanov	   .NET Developer	   Software Development	        01/02/2013	   3500.00
@@ -25,10 +22,8 @@ INSERT INTO Departments VALUES
 --Peter Pan Pan	               Intern	          Marketing             	28/08/2016	   599.88
 
 INSERT INTO Employees VALUES
-	('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013/02/01', 3500.00, 1),
+	('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013/02/01', 3500.00, null),
 	('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004/03/02', 4000.00, null),
 	('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016/08/28', 525.25, null),
 	('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '2007/12/09', 3000.00, null),
 	('Peter', 'Pan', 'Pan', 'Intern', 3, '2016/08/28', 599.88, null)
-
-	select * from Addresses
