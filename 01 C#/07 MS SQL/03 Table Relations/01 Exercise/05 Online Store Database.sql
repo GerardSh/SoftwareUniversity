36a923230c44d0f5d138bda01daa7ebe4200e051
Gerard <gerardshah@gmail.com>
2025-01-08 23:57:32 +0200

    MS SQL - Table Relations - exercise added

---
+++ 01 C#/07 MS SQL/03 Table Relations/01 Exercise/05 Online Store Database.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Cities
+(
+	CityID INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(30)
+)
+
+CREATE TABLE Customers
+(
+	CustomerID INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(30),
+	Birthday DATE,
+	CityID INT REFERENCES Cities(CityID)
+)
+
+CREATE TABLE Orders
+(
+	OrderID INT IDENTITY PRIMARY KEY,
+	CustomerID INT REFERENCES Customers(CustomerID)
+)
+
+CREATE TABLE ItemTypes
+(
+	ItemTypeID INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(30)
+)
+
+CREATE TABLE Items
+(
+	ItemID INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(30),
+	ItemTypeID INT REFERENCES ItemTypes(ItemTypeID)
+)
+
+CREATE TABLE OrderItems
+(
+	OrderID INT REFERENCES Orders(OrderID),
+	ItemID INT REFERENCES Items(ItemID),
+
+	CONSTRAINT PK_OrderItem PRIMARY KEY(OrderID, ItemID)
+)
