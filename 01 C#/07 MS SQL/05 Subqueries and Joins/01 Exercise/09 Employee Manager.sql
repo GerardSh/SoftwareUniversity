24169003a6c55ae4e842892950f6a761face682d
Gerard <gerardshah@gmail.com>
2025-01-15 22:20:30 +0200

    MS SQL - Subqueries and Joins - exercise added

---
+++ 01 C#/07 MS SQL/05 Subqueries and Joins/01 Exercise/09 Employee Manager.sql
@@ -0,0 +1,8 @@
+SELECT
+e.EmployeeID,
+e.FirstName,
+e.ManagerID,
+em.FirstName AS ManagerName
+FROM Employees AS e
+JOIN Employees AS em ON e.ManagerID = em.EmployeeID AND e.ManagerID IN (3, 7)
+ORDER BY EmployeeID
