15a9d7928d41e6a056677b2c4a2019fd48cfb4cb
Gerard <gerardshah@gmail.com>
2025-01-15 22:20:36 +0200

    MS SQL - Subqueries and Joins - exercise added

---
+++ 01 C#/07 MS SQL/05 Subqueries and Joins/01 Exercise/11 Min Average Salary.sql
@@ -0,0 +1,17 @@
+SELECT
+Min(Temp.MinAverageSalary) AS MinAverageSalary
+FROM (
+SELECT 
+AVG(Salary) AS MinAverageSalary
+FROM Employees
+GROUP BY DepartmentID
+) AS Temp
+
+
+
+
+--2
+SELECT TOP (1) AVG(Salary) AS MinAverageSalary
+FROM Employees
+GROUP BY DepartmentID
+ORDER BY MinAverageSalary
