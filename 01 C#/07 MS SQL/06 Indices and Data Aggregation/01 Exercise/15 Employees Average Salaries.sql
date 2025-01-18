67454f1d1993befdafdad7d3f7d4e97044bbedef
Gerard <gerardshah@gmail.com>
2025-01-19 00:22:36 +0200

    MS SQL - Indices and Data Aggregation - exercise added

--- 01 C#/07 MS SQL/06 Indices and Data Aggregation/01 Exercise/15 Employees Average Salaries.sql
+++ 01 C#/07 MS SQL/06 Indices and Data Aggregation/01 Exercise/15 Employees Average Salaries.sql
@@ -1,15 +1,15 @@
 SELECT *
-INTO NewTable
-FROM Employees
-WHERE Salary > 30000
+  INTO NewTable
+  FROM Employees
+ WHERE Salary > 30000
 
 DELETE FROM NewTable
-WHERE ManagerID = 42
+ WHERE ManagerID = 42
 
 UPDATE NewTable
    SET Salary += 5000
  WHERE DepartmentID = 1
 
- SELECT DepartmentID, AVG(Salary) AS AverageSalary
- FROM Employees
+   SELECT DepartmentID, AVG(Salary) AS AverageSalary
+     FROM NewTable
  GROUP BY DepartmentID
