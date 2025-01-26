b1578fcafcb6cf5a7c101a17bf5d61598120fb5a
Gerard <gerardshah@gmail.com>
2025-01-26 20:01:41 +0200

    MS SQL - Functions and Stored Procedures - exercise added

---
+++ 01 C#/07 MS SQL/07 Functions and Stored Procedures/01 Exercise/11 Future Value Function.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION ufn_CalculateFutureValue (@sum DECIMAL(15,4),
+@yearlyInterestRate FLOAT, @numberOfYears INT)
+RETURNS DECIMAL(15,4)
+AS
+BEGIN
+	RETURN @sum * (POWER((1 + @yearlyInterestRate),@numberOfYears));
+END
