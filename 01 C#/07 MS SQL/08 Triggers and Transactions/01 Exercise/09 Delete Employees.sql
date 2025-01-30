794decd45d563d018bac6bc7b8d7c0815d3cf895
Gerard <gerardshah@gmail.com>
2025-01-31 00:28:23 +0200

    MS SQL - Triggers and Transactions - exercise added

--- 01 C#/07 MS SQL/08 Triggers and Transactions/01 Exercise/09 Delete Employees.sql
+++ 01 C#/07 MS SQL/08 Triggers and Transactions/01 Exercise/09 Delete Employees.sql
@@ -1,18 +1,23 @@
 CREATE TABLE Deleted_Employees
 (
-	Id INT PRIMARY KEY IDENTITY,
-	FirstName VARCHAR(50) NOT NULL, 
-	LastName VARCHAR(50) NOT NULL, 
-	MiddleName VARCHAR(50) NOT NULL, 
-	JobTitle VARCHAR(50) NOT NULL, 
-	DepartmentId INT NOT NULL, 
-	Salary MONEY NOT NULL
+EmployeeId INT IDENTITY PRIMARY KEY,
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+MiddleName VARCHAR(30),
+JobTitle VARCHAR(30) NOT NULL,
+DepartmentId INT,
+Salary MONEY NOT NULL
 )
-
-GO
 
 CREATE TRIGGER tr_DeletedEmployees
 ON Employees FOR DELETE
-AS 
-	INSERT INTO Deleted_Employees(FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
-	SELECT FirstName, LastName, MiddleName, JobTitle, DepartmentID, Salary FROM deleted;
+AS
+ INSERT INTO Deleted_Employees
+ SELECT 
+ FirstName,
+ LastName,
+ MiddleName,
+ JobTitle,
+ DepartmentID,
+ Salary
+ FROM deleted
