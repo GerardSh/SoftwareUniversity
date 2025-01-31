2709573457590681718b45fe93d99b61f51da38a
Gerard <gerardshah@gmail.com>
2025-01-31 23:50:59 +0200

    MS SQL - Additional Exercises - exercise added

---
+++ 01 C#/07 MS SQL/08 Triggers and Transactions/02 Additional Exercises/11 Count of Countries by Currency.sql
@@ -0,0 +1,9 @@
+SELECT crr.CurrencyCode,
+       crr.[Description] AS Currency,
+       COUNT(cntr.CountryCode) AS NumberOfCountries
+FROM Currencies AS crr
+     LEFT JOIN Countries AS cntr ON cntr.CurrencyCode = crr.CurrencyCode
+GROUP BY crr.CurrencyCode,
+         crr.[Description]
+ORDER BY NumberOfCountries DESC,
+         Currency;
