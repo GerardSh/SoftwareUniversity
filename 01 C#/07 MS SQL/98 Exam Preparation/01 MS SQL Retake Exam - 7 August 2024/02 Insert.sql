4eb55641f39ce6f11e9ef804b82c55129c49518f
Gerard <gerardshah@gmail.com>
2025-02-01 22:59:41 +0200

    MS SQL - Exam preparation - exercise added

---
+++ 01 C#/07 MS SQL/98 Exam Preparation/01 MS SQL Retake Exam - 7 August 2024/02 Insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO Brands 
+VALUES ('Timberland'),
+       ('Birkenstock')
+
+INSERT INTO Shoes
+VALUES ('Reaxion Pro', 150, 12),
+       ('Laurel Cort Lace-Up', 160, 12),
+       ('Perkins Row Sandal', 170, 12),
+       ('Arizona', 80, 13),
+       ('Ben Mid Dip', 85, 13),
+       ('Gizeh', 90, 13)
+
+INSERT INTO ShoesSizes
+VALUES (70, 1),
+	   (70, 2),
+	   (70, 3),
+	   (71, 2),
+	   (71, 3),
+	   (71, 4),
+	   (72, 4),
+	   (72, 5),
+	   (72, 6),
+	   (73, 1),
+	   (73, 3),
+	   (73, 5),
+	   (74, 2),
+	   (74, 4),
+	   (74, 6),
+	   (75, 1),
+	   (75, 2),
+	   (75, 3)
+       
+INSERT INTO Orders
+VALUES (70, 2, 15),
+	   (71, 3, 17),
+	   (72, 6, 18),
+	   (73, 5, 4),
+	   (74, 4, 7),
+	   (75, 1, 11)
