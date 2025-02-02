a7d4ea43f29f416387f65009cb4953579e6d66b1
Gerard <gerardshah@gmail.com>
2025-02-02 11:15:24 +0200

    MS SQL - Exam preparation - exercise added

--- 01 C#/07 MS SQL/98 Exam Preparation/01 MS SQL Retake Exam - 7 August 2024/10 Phone Including Digits 345.sql
+++ 01 C#/07 MS SQL/98 Exam Preparation/01 MS SQL Retake Exam - 7 August 2024/10 Phone Including Digits 345.sql
@@ -1,12 +1,12 @@
 SELECT FullName,
 	   PhoneNumber,
 	   Price AS OrderPrice,
-	   ss.ShoeId,
-	   BrandId
-FROM Orders as o
-	JOIN Users as u ON u.Id = o.UserId
-	JOIN Shoes as s ON s.Id = o.ShoeId
-	JOIN ShoesSizes AS ss ON ss.ShoeId = s.Id
-	JOIN Sizes AS sz ON sz.Id = ss.SizeId
+	   o.ShoeId,
+	   BrandId,
+	   CONCAT_WS('/', CONVERT(VARCHAR, sz.EU) + 'EU', CONVERT(VARCHAR, sz.US) + 'US', CONVERT(VARCHAR, sz.UK) + 'UK') AS ShoeSize
+FROM Orders AS o
+	JOIN Users AS u ON u.Id = o.UserId
+	JOIN Shoes AS s ON s.Id = o.ShoeId
+	JOIN Sizes AS sz ON sz.Id = o.SizeId
 WHERE PhoneNumber LIKE '%345%'
 ORDER BY s.Model
