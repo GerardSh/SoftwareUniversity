5dfff13c964508a555d802ab9f8c6e6e1116213c
Gerard <gerardshah@gmail.com>
2025-02-02 23:55:52 +0200

    MS SQL - Exam preparation - exercise added

---
+++ 01 C#/07 MS SQL/98 Exam Preparation/03 Databases MSSQL Server Retake Exam - 05 December 2023/01 DDL.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE RailwaysDb
+
+GO
+
+USE RailwaysDb
+
+CREATE TABLE Passengers
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(80) NOT NULL
+)
+
+CREATE TABLE Towns
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE RailwayStations
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	TownId INT REFERENCES Towns(Id) NOT NULL
+)
+
+CREATE TABLE Trains
+(
+	Id INT PRIMARY KEY IDENTITY,
+	HourOfDeparture VARCHAR(5) NOT NULL,
+	HourOfArrival VARCHAR(5) NOT NULL,
+	DepartureTownId INT REFERENCES Towns(Id) NOT NULL,
+	ArrivalTownId INT REFERENCES Towns(Id) NOT NULL
+)
+
+CREATE TABLE TrainsRailwayStations
+(
+	TrainId INT REFERENCES Trains(Id),
+	RailwayStationId INT REFERENCES RailwayStations(Id)
+	PRIMARY KEY (TrainId, RailwayStationId)
+)
+
+CREATE TABLE MaintenanceRecords
+(
+	Id INT PRIMARY KEY IDENTITY,
+	DateOfMaintenance DATE,
+	Details	VARCHAR(2000) NOT NULL,
+	TrainId INT REFERENCES Trains(Id) NOT NULL
+)
+
+CREATE TABLE Tickets
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Price DECIMAL(18, 2) NOT NULL,
+	DateOfDeparture DATE NOT NULL,
+	DateOfArrival DATE NOT NULL,
+	TrainId INT REFERENCES Trains(Id) NOT NULL,
+	PassengerId INT REFERENCES Passengers(Id) NOT NULL
+)
