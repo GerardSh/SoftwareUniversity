d371e32e0ab01ce548ba439cb7eda6a985b9a466
Gerard <gerardshah@gmail.com>
2025-02-02 23:57:42 +0200

    MS SQL - Exam preparation - exercise added

---
+++ 01 C#/07 MS SQL/98 Exam Preparation/03 Databases MSSQL Server Retake Exam - 05 December 2023/09 Count of Passengers Paid More Than Average.sql
@@ -0,0 +1,8 @@
+SELECT tns.[Name] AS TownName, COUNT(*) AS PassangerCount
+FROM Passengers AS p
+	JOIN Tickets AS ti ON ti.PassengerId = p.Id
+	JOIN Trains as t ON t.Id = ti.TrainId
+	JOIN Towns AS tns ON tns.Id = t.ArrivalTownId
+WHERE ti.Price > 76.99
+GROUP BY tns.[Name]
+ORDER BY tns.[Name]
