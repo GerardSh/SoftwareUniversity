beb07ef835d61118f3c5bf15baeefd1f9afcda35
Gerard <gerardshah@gmail.com>
2025-02-03 22:20:18 +0200

    MS SQL - Exam preparation - exercise added

---
+++ 01 C#/07 MS SQL/98 Exam Preparation/04 Databases MSSQL Server Regular Exam - 15 Oct 2023/05 Bookings by Price of Room and Arrival Date.sql
@@ -0,0 +1,6 @@
+SELECT FORMAT(ArrivalDate, 'yyyy-MM-dd') AS ArrivalDate,
+	   AdultsCount,
+	   ChildrenCount
+FROM Bookings AS b
+	JOIN Rooms AS r ON r.Id = b.RoomId 
+ORDER BY Price DESC, ArrivalDate
