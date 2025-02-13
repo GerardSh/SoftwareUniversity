82ab1e1678c20ed30c2d72a900b08e596164cb7c
Gerard <gerardshah@gmail.com>
2025-02-13 23:33:11 +0200

    Entity Framework Core - Entity Framework Introduction - demo added

---
+++ 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/00 Demo/DBFirstDemo/DBFirstDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using DBFirstDemo.Infrastructure.Data;
+using Microsoft.Extensions.Logging;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        using SoftUniDbContext context = new SoftUniDbContext();
+
+        var employees = context.Employees
+            .Select(e => new 
+            { 
+                e.FirstName,
+                e.LastName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToList();
+
+        foreach (var employee in employees)
+        {
+            Console.WriteLine(employee);
+        }
+    }
+}
