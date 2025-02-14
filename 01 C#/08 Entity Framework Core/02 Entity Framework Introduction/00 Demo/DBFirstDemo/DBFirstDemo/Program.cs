35daf5a03adbc9bba59e072cb49a26d8ace7fe21
Gerard <gerardshah@gmail.com>
2025-02-14 23:58:02 +0200

    Entity Framework Core - Entity Framework Introduction - demo added

--- 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/00 Demo/DBFirstDemo/DBFirstDemo/Program.cs
+++ 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/00 Demo/DBFirstDemo/DBFirstDemo/Program.cs
@@ -1,26 +1,48 @@
 ﻿using DBFirstDemo.Infrastructure.Data;
+using DBFirstDemo.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
         using SoftUniDbContext context = new SoftUniDbContext();
 
-        var employees = context.Employees
-            .Select(e => new 
-            { 
+        var employees = await context.Employees
+            .Select(e => new
+            {
                 e.FirstName,
                 e.LastName,
                 e.JobTitle,
                 e.Salary
             })
-            .ToList();
+            .ToListAsync();
 
         foreach (var employee in employees)
         {
-            Console.WriteLine(employee);
+            Console.WriteLine(employee.FirstName);
         }
+
+        Console.WriteLine();
+
+        var SQLCommand = context.Employees
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.JobTitle,
+                e.Salary
+            })
+            .Where(e => e.FirstName.StartsWith("a"))
+            .ToQueryString();
+
+        Console.WriteLine(SQLCommand);
+
+        // Output:
+        // SELECT[e].[FirstName], [e].[LastName], [e].[JobTitle], [e].[Salary]
+        // FROM[Employees] AS[e]
+        // WHERE[e].[FirstName] LIKE 'a%'
     }
 }
