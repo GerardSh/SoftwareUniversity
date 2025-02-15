6595163b1b087c9f41265ff27e75e1a3e840e390
Gerard <gerardshah@gmail.com>
2025-02-15 22:10:48 +0200

    Entity Framework Core - Entity Framework Introduction - exercise added

--- 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/00 Demo/DBFirstDemo.Infrastructure/Data/SoftUniDbContext.cs
+++ 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/00 Demo/DBFirstDemo.Infrastructure/Data/SoftUniDbContext.cs
@@ -28,7 +28,7 @@ namespace DBFirstDemo.Infrastructure.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;User Id=Username;Password=Password;TrustServerCertificate=true;");
+                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;User Id=sa;Password=r3F4iJbYas&#aRj^bmjj;TrustServerCertificate=true;");
             }
         }
 
---
+++ 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/01 All Exercises Project/SoftUni/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static StringBuilder sb = new StringBuilder();
+
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+
+           Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+               .Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}")
+               .ToList();
+
+            sb.Clear();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine(employee);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
