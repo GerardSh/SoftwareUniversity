8affe56b5ab9080cdd74cbf9ac50b9d59396ee8c
Gerard <gerardshah@gmail.com>
2025-02-15 22:22:15 +0200

    Entity Framework Core - Entity Framework Introduction - exercise added

--- 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/01 All Exercises Project/SoftUni/StartUp.cs
+++ 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/01 All Exercises Project/SoftUni/StartUp.cs
@@ -16,7 +16,10 @@ namespace SoftUni
         {
             var context = new SoftUniContext();
 
-           Console.WriteLine(GetEmployeesFullInformation(context));
+            // Console.WriteLine(GetEmployeesFullInformation(context));
+
+
+            // Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -30,6 +33,29 @@ namespace SoftUni
             foreach (var employee in employees)
             {
                 sb.AppendLine(employee);
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e=> e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                
+                .ToList();
+
+            sb.Clear();
+
+            foreach (var em in employees)
+            {
+                sb.AppendLine($"{em.FirstName} - {em.Salary:f2}");
             }
 
             return sb.ToString().Trim();
