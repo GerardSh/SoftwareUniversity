c9004f9e210adb0c49f86028e2c0ced7a3232cb6
Gerard <gerardshah@gmail.com>
2025-02-16 19:31:56 +0200

    Entity Framework Core - Entity Framework Introduction - exercise added

--- 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/01 All Exercises Project/SoftUni/StartUp.cs
+++ 01 C#/08 Entity Framework Core/02 Entity Framework Introduction/01 Exercise/01 All Exercises Project/SoftUni/StartUp.cs
@@ -157,5 +157,47 @@ namespace SoftUni
 
             return sb.ToString().Trim();
         }
+		
+		  public static string GetEmployeesInPeriod2(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Take(10)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    Projects = e.EmployeesProjects.Where(ep => ep.Project.StartDate.Year >= 2001 &&
+                                                         ep.Project.StartDate.Year <= 2003)
+                    .Select(ep => new
+                    {
+                        ProjectName = ep.Project.Name,
+                        StartDate = ep.Project.StartDate,
+                        EndDate = ep.Project.EndDate,
+                    })
+                })
+                .ToList();
+
+            sb.Clear();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} - Manager: {employee.ManagerFirstName} {employee.ManagerLastName}");
+
+                foreach (var project in employee.Projects)
+                {
+                    string startDate = project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                    string endDate = project.EndDate != null
+                                     ? project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                                     : "not finished";
+
+                    sb.AppendLine($"--{project.ProjectName} - {startDate} - {endDate}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
