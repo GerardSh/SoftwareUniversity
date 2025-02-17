57f9bdbb5289f8871cc333b5e15f0ef35ac285a7
Gerard <gerardshah@gmail.com>
2025-02-17 23:09:59 +0200

    Entity Framework Core - Entity Relations - demo added

---
+++ 01 C#/08 Entity Framework Core/03 Entity Relations/01 Exercise/00 Demo/Blog/BlogDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogDemo
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext()
+        {
+        }
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasKey(b => b.BlogId);
+
+            modelBuilder.Entity<Blog>()
+                .ToTable("Blogs", "blg"); // Optionally creating a new schema named "blg"
+
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Name)
+                .HasColumnName("BlogName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<Blog>()
+                 .Property(b => b.Description)
+                 .HasColumnType("NVARCHAR")
+                 .HasMaxLength(500);
+
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Created)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.LastUpdated)
+                .ValueGeneratedOnUpdate();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=BlogDb;User Id=sa;Password=r3F4iJbYas&#aRj^bmjj;TrustServerCertificate=true;");
+            }
+        }
+    }
+}
---
+++ 01 C#/08 Entity Framework Core/03 Entity Relations/01 Exercise/00 Demo/Blog/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Blog blog = new Blog();
+
+            blog.Name = null;
+
+            Console.WriteLine(blog.Name);
+        }
+    }
+}
