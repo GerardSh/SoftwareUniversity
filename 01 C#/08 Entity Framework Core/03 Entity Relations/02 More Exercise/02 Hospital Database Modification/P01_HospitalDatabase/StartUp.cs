755a76a8036f1e651afb2b8522df99d4cadf4ccf
Gerard <gerardshah@gmail.com>
2025-02-23 12:38:50 +0200

    Entity Framework Core - Entity Relations - exercise added

---
+++ 01 C#/08 Entity Framework Core/03 Entity Relations/02 More Exercise/02 Hospital Database Modification/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using P01_HospitalDatabase.Data;
+
+namespace P01_HospitalDatabase
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Db Creation Started...");
+
+            try
+            {
+                using HospitalContext dbContex = new HospitalContext();
+
+                dbContex.Database.EnsureDeleted();
+                dbContex.Database.EnsureCreated();
+
+                Console.WriteLine("Db Creation was successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Db Creation failed! - {e.Message}");
+            }
+        }
+    }
+}
