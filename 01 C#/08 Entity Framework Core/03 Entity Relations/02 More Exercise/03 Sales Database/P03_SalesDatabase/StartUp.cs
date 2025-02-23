2eb04ce2b1d4df55355022f54292652db9431209
Gerard <gerardshah@gmail.com>
2025-02-23 12:39:03 +0200

    Entity Framework Core - Entity Relations - exercise added

---
+++ 01 C#/08 Entity Framework Core/03 Entity Relations/02 More Exercise/03 Sales Database/P03_SalesDatabase/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using P03_SalesDatabase.Data;
+using System;
+
+namespace P03_SalesDatabase
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Db Creation Started...");
+
+            try
+            {
+                using SalesContext dbContex = new SalesContext();
+
+                dbContex.Database.EnsureDeleted();
+                dbContex.Database.EnsureCreated();
+
+                Console.WriteLine("Db Creation was successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Db Creation failed! - {e.Message}");
+            }
+        }
+    }
+}
