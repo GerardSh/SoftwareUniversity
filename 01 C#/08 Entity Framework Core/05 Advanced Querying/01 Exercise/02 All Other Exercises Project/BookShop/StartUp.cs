f00e911ed4e8e2f70063d2f0c837d58cb66a028a
Gerard <gerardshah@gmail.com>
2025-03-03 12:27:59 +0200

    Entity Framework Core - Advanced Querying - exercise added

---
+++ 01 C#/08 Entity Framework Core/05 Advanced Querying/01 Exercise/02 All Other Exercises Project/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            // DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, "miNor"));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            bool isEnumValid = Enum
+                .TryParse(command, true, out AgeRestriction ageRestriction);
+
+            if (!isEnumValid)
+            {
+                Environment.Exit(0);
+            }
+
+            var titles = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b =>  b.Title)
+                .OrderBy(b=> b)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, titles);
+        }
+    }
+}
