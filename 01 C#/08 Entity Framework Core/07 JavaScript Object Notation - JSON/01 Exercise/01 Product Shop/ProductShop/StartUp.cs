35106a54bde7f6ec80d2706119e45ab5c83ae398
Gerard <gerardshah@gmail.com>
2025-03-11 23:09:23 +0200

    Entity Framework Core - JSON - exercise added

---
+++ 01 C#/08 Entity Framework Core/07 JavaScript Object Notation - JSON/01 Exercise/01 Product Shop/ProductShop/StartUp.cs
@@ -0,0 +1,87 @@
+﻿namespace ProductShop
+{
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+
+    using Data;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+    using System.ComponentModel.DataAnnotations;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using ProductShopContext dbContext = new ProductShopContext();
+            dbContext.Database.Migrate();
+            // Console.WriteLine("Database migrated successfully!");
+
+            string jsonString = File.ReadAllText("../../../Datasets/users.json");
+            string result = ImportUsers(dbContext, jsonString);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            string result = string.Empty;
+
+            ImportUserDTO[]? userDtos = JsonConvert
+                .DeserializeObject<ImportUserDTO[]>(inputJson);
+
+            if (userDtos != null)
+            {
+                ICollection<User> usersToAdd = new List<User>();
+
+                foreach (var userDto in userDtos)
+                {
+                    if (!IsValid(userDto))
+                    {
+                        continue;
+                    }
+
+                    int? userAge = null;
+
+                    if (userDto.Age != null)
+                    {
+                        bool isAgeValid = int.TryParse(userDto.Age, out int parsedAge);
+
+                        if (!isAgeValid)
+                        {
+                            continue;
+                        }
+
+                        userAge = parsedAge;
+                    }
+
+                    User user = new User()
+                    {
+                        FirstName = userDto.FirstName,
+                        LastName = userDto.LastName,
+                        Age = userAge
+                    };
+
+                    usersToAdd.Add(user);
+                }
+
+                context.Users.AddRange(usersToAdd);
+                context.SaveChanges();
+
+                result = $"Successfully imported {usersToAdd.Count}";
+            }
+
+            return result;
+        }
+
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator
+                .TryValidateObject(dto, validateContext, validationResults);
+
+            return isValid;
+        }
+    }
+}
