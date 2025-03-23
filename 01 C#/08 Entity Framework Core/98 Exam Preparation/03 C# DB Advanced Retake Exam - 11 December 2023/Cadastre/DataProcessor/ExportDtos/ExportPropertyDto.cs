407b7f49d540d4cb242137d6e2800c4647fc53ee
Gerard <gerardshah@gmail.com>
2025-03-24 00:52:30 +0200

    Entity Framework Core - Exam preparation - exercise added

---
+++ 01 C#/08 Entity Framework Core/98 Exam Preparation/03 C# DB Advanced Retake Exam - 11 December 2023/Cadastre/DataProcessor/ExportDtos/ExportPropertyDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Cadastre.DataProcessor.ExportDtos
+{
+    [XmlType("Property")]
+    public class ExportPropertyDto
+    {
+        [XmlAttribute("postal-code")]
+        public string PostalCode { get; set; } = null!;
+
+        public string PropertyIdentifier { get; set; } = null!;
+
+        public int Area { get; set; }
+
+        public string DateOfAcquisition { get; set; } = null!;
+    }
+}
--- 01 C#/08 Entity Framework Core/98 Exam Preparation/03 C# DB Advanced Retake Exam - 11 December 2023/Cadastre/DataProcessor/Serializer.cs
+++ 01 C#/08 Entity Framework Core/98 Exam Preparation/03 C# DB Advanced Retake Exam - 11 December 2023/Cadastre/DataProcessor/Serializer.cs
@@ -1,4 +1,8 @@
 ﻿using Cadastre.Data;
+using Cadastre.Data.Enumerations;
+using Cadastre.DataProcessor.ExportDtos;
+using NetPay.Utilities;
+using Newtonsoft.Json;
 
 namespace Cadastre.DataProcessor
 {
@@ -6,12 +10,51 @@ namespace Cadastre.DataProcessor
     {
         public static string ExportPropertiesWithOwners(CadastreContext dbContext)
         {
-           throw new NotImplementedException();
+            var properties = dbContext.Properties
+                .Where(p => p.DateOfAcquisition >= new DateTime(2000, 1, 1))
+                .OrderByDescending(p => p.DateOfAcquisition)
+                .ThenBy(p => p.PropertyIdentifier)
+                .Select(p => new
+                {
+                    p.PropertyIdentifier,
+                    p.Area,
+                    p.Address,
+                    DateOfAcquisition = p.DateOfAcquisition.ToString("dd/MM/yyyy"),
+                    Owners = p.PropertiesCitizens
+                            .Select(pc => pc.Citizen)
+                            .Select(c => new
+                            {
+                                c.LastName,
+                                MaritalStatus = c.MaritalStatus.ToString()
+                            })
+                            .OrderBy(c => c.LastName)
+                            .ToArray()
+                })
+                .ToList();
+
+            var result = JsonConvert.SerializeObject(properties, Formatting.Indented);
+
+            return result.ToString().Trim();
         }
 
         public static string ExportFilteredPropertiesWithDistrict(CadastreContext dbContext)
         {
-            throw new NotImplementedException();
+            var properties = dbContext.Properties
+                .Where(p => p.Area >= 100)
+                .OrderByDescending(p => p.Area)
+                .ThenBy(p => p.DateOfAcquisition)
+                .Select(p => new ExportPropertyDto()
+                {
+                    PostalCode = p.District.PostalCode,
+                    PropertyIdentifier = p.PropertyIdentifier,
+                    Area = p.Area,
+                    DateOfAcquisition = p.DateOfAcquisition.ToString("dd/MM/yyyy")
+                })
+                .ToList();
+
+            string result = XmlHelper.Serialize(properties, "Properties");
+
+            return result.ToString().Trim();
         }
     }
 }
