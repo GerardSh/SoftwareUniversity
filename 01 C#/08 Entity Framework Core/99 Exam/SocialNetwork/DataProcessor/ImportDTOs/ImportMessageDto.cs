7f2e298e6c14eac492f9c6c8285cbcbefc0883e7
Gerard <gerardshah@gmail.com>
2025-03-30 12:38:30 +0300

    Entity Framework Core - Exam - exercise added

---
+++ 01 C#/08 Entity Framework Core/99 Exam/SocialNetwork/DataProcessor/ImportDTOs/ImportMessageDto.cs
@@ -0,0 +1,25 @@
+﻿using SocialNetwork.Data.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace SocialNetwork.DataProcessor.ImportDTOs
+{
+    [XmlType(nameof(Message))]
+    public class ImportMessageDto
+    {
+        [XmlAttribute("SentAt")]
+        public string SentAt { get; set; } = null!;
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public string Content { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+
+        public int ConversationId { get; set; }
+
+        public int SenderId { get; set; }
+    }
+}
---
+++ 01 C#/08 Entity Framework Core/99 Exam/SocialNetwork/DataProcessor/ImportDTOs/ImportPostDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.DataProcessor.ImportDTOs
+{
+    public class ImportPostDto
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(300)]
+        public string Content { get; set; } = null!;
+
+        public string CreatedAt { get; set; } = null!;
+
+        public int CreatorId { get; set; }
+    }
+}
