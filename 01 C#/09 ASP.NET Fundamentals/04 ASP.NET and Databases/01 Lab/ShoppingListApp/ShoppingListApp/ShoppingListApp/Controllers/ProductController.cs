55e8f0a69be3b0b79efa9d675ea448de159b9e83
Gerard <gerardshah@gmail.com>
2025-04-23 23:59:32 +0300

    ASP.NET - ASP.NET and Databases - Lab - Add ShoppingListApp project

---
+++ 01 C#/09 ASP.NET Fundamentals/04 ASP.NET and Databases/01 Lab/ShoppingListApp/ShoppingListApp/ShoppingListApp/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingListApp.Contracts;
+using ShoppingListApp.Models;
+
+namespace ShoppingListApp.Controllers
+{
+    public class ProductController(IProductService productService) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var model = await productService.GetAllAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new ProductViewModel(); //This is how where we create a new instance of the ProductViewModel
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductViewModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
+            await productService.AddProductAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await productService.GetByIdAsync(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, ProductViewModel model)
+        {
+            if (ModelState.IsValid == false || model.Id != id)
+            {
+                return View(model);
+            }
+
+            await productService.UpdateProductAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            // Check if the product exists
+            var existingProduct = await productService.GetByIdAsync(id);
+
+            if (existingProduct == null)
+            {
+                // Handle the case where the product with the given id is not found
+                return NotFound();
+            }
+
+            // Perform the deletion
+            await productService.DeleteProductAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ 01 C#/09 ASP.NET Fundamentals/04 ASP.NET and Databases/01 Lab/ShoppingListApp/ShoppingListApp/ShoppingListApp/Data/ShoppingListDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingListApp.Data.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ShoppingListApp.Data
+{
+    public class ShoppingListDbContext : DbContext
+    {
+        public ShoppingListDbContext(DbContextOptions<ShoppingListDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductNote> ProductNotes { get; set; }
+
+        //Here the information is seeded upon startup
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasData(
+                     new Product() { Id = 1, Name = "Cheese" },
+                     new Product() { Id = 2, Name = "Milk" });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
