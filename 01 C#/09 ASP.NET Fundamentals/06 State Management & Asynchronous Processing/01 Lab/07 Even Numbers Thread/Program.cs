dc208d9a72dd9804c53b7e00d22a95940dd38687
Gerard <gerardshah@gmail.com>
2025-05-01 23:38:14 +0300

    ASP.NET -  State Management & Asynchronous Processing - Lab - Add exercise

---
+++ 01 C#/09 ASP.NET Fundamentals/06 State Management & Asynchronous Processing/01 Lab/07 Even Numbers Thread/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int start = 0;
+        int end = 100;
+
+        Thread evens = new Thread(() => PrintEvenNumbers(start, end));
+
+        evens.Start();
+        evens.Join();
+
+        Console.WriteLine("Thread finished work");   
+    }
+
+    static void PrintEvenNumbers(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0) Console.WriteLine(i);
+        }
+    }
+}
