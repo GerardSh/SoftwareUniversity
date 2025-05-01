7332ed5854cdfb27a88a29bec63488b761fc797f
Gerard <gerardshah@gmail.com>
2025-05-01 23:51:05 +0300

    ASP.NET - State Management & Asynchronous Processing - Lab - Add exercise

---
+++ 01 C#/09 ASP.NET Fundamentals/06 State Management & Asynchronous Processing/01 Lab/08 Sum Evens in Range/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            var command = Console.ReadLine();
+
+            if (command == "show")
+            {
+                var result = SumAsync();
+                Console.WriteLine(result);
+            }
+        }
+    }
+
+    private static long SumAsync()
+    {
+        return Task.Run(() =>
+        {
+            long sum = 0;
+
+            for (int i = 1; i < 10000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+
+        }).Result;
+    }
+}
