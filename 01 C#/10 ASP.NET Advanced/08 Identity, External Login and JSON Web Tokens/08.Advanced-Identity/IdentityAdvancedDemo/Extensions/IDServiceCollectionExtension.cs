544c69329aac51ed790542b171d291b109dc8002
Gerard <gerardshah@gmail.com>
2025-06-01 23:50:54 +0300

    ASP.NET Advanced - Identity, External Login and JSON Web Tokens - Add demo

---
+++ 01 C#/10 ASP.NET Advanced/08 Identity, External Login and JSON Web Tokens/08.Advanced-Identity/IdentityAdvancedDemo/Extensions/IDServiceCollectionExtension.cs
@@ -0,0 +1,57 @@
+﻿using IdentityAdvancedDemo.Data;
+using IdentityAdvancedDemo.Data.IdentityModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IDServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationDatabase(this IServiceCollection services, IConfiguration config)
+        {
+            // Add services to the container.
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIDentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options => options.SignIn.RequireConfirmedAccount = true)
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User/Login";
+                options.LogoutPath = "/User/Logout";
+            });
+
+            services.AddAuthentication()
+                .AddFacebook(options =>
+                {
+                    options.AppId = "658708255927516";
+                    options.AppSecret = "795acf9f3041cb193e8305bc94dcd6d9";
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("MyPolicy", policy =>
+                {
+                    policy.RequireRole("Admin");
+                    policy.RequireClaim("EmployeeNumber");
+                });
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            return services;
+        }
+    }
+}
